7dc81d9c375ef231ce5f262d4b7a9647fe58904b
BestMod <71246119+BestMod@users.noreply.github.com>
2022-11-01 17:55:45 -0700

    add basic textures to cooling coils (wow so innovative)

--- src/main/java/supersymmetry/loaders/SuSyRecipeLoader.java
+++ src/main/java/supersymmetry/loaders/SuSyRecipeLoader.java
@@ -1,9 +1,12 @@
 package supersymmetry.loaders;
 
+import supersymmetry.api.recipe.FridgeRecipes;
+
 public class SuSyRecipeLoader {
 
     public static void init() {
         SuSyMetaTileEntityLoader.init();
+        FridgeRecipes.init();
         // make more loaders to categorize recipes and what is added
     }
 }
