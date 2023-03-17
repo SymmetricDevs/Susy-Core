13a1ff19286f95f28fd055415e778c4d7976481c
Zalgo239 <67443118+Zalgo239@users.noreply.github.com>
2023-03-17 11:08:05 +0100

    Added compile time deps

--- src/main/java/supersymmetry/api/recipe/SuSyRecipeMaps.java
+++ src/main/java/supersymmetry/api/recipe/SuSyRecipeMaps.java
@@ -3,13 +3,13 @@ package supersymmetry.api.recipe;
 import gregtech.api.recipes.RecipeMap;
 import gregtech.api.recipes.builders.PrimitiveRecipeBuilder;
 import gregtech.api.recipes.builders.SimpleRecipeBuilder;
-import gregtech.api.sound.GTSounds;
+import gregtech.core.sound.GTSoundEvents;
 import supersymmetry.api.recipe.builders.CoilingCoilRecipeBuilder;
 
 public class SuSyRecipeMaps {
 
     public static final RecipeMap<CoilingCoilRecipeBuilder> COOLING_RECIPES = new RecipeMap<>("magnetic_refrigerator", 1, 3, 0, 3, 0, 0, 0, 1, new CoilingCoilRecipeBuilder(), false)
-            .setSound(GTSounds.COOLING);
+            .setSound(GTSoundEvents.COOLING);
 
     public static final RecipeMap<PrimitiveRecipeBuilder> COAGULATION_RECIPES = new RecipeMap<>("coagulation_tank", 0, 2, 1, 1, 1, 2, 0, 0, new PrimitiveRecipeBuilder(), false);
 
