7b8562a04ef19283e3d829345230044fb7fc6cb8
Loxo <loxo.Minecraft@gmail.com>
2023-04-06 11:45:25 +0200

    Change textures and add electrical roaster tiers

--- src/main/java/supersymmetry/common/metatileentities/SuSyMetaTileEntities.java
+++ src/main/java/supersymmetry/common/metatileentities/SuSyMetaTileEntities.java
@@ -53,6 +53,20 @@ public class SuSyMetaTileEntities {
 
         ROASTER_BRONZE = registerMetaTileEntity(14522, new MetaTileEntitySteamRoaster(susyId("roaster.steam"), false));
 
+        ROASTER[0] = registerMetaTileEntity(14523, new SimpleMachineMetaTileEntity(susyId("roaster.lv"), SuSyRecipeMaps.ROASTER_RECIPES, SusyTextures.ROASTER_OVERLAY, 1, true));
+        ROASTER[1] = registerMetaTileEntity(14524, new SimpleMachineMetaTileEntity(susyId("roaster.mv"), SuSyRecipeMaps.ROASTER_RECIPES, SusyTextures.ROASTER_OVERLAY, 2, true));
+        ROASTER[2] = registerMetaTileEntity(14525, new SimpleMachineMetaTileEntity(susyId("roaster.hv"), SuSyRecipeMaps.ROASTER_RECIPES, SusyTextures.ROASTER_OVERLAY, 3, true));
+        ROASTER[3] = registerMetaTileEntity(14526, new SimpleMachineMetaTileEntity(susyId("roaster.ev"), SuSyRecipeMaps.ROASTER_RECIPES, SusyTextures.ROASTER_OVERLAY, 4, true));
+        ROASTER[4] = registerMetaTileEntity(14527, new SimpleMachineMetaTileEntity(susyId("roaster.iv"), SuSyRecipeMaps.ROASTER_RECIPES, SusyTextures.ROASTER_OVERLAY, 5, true));
+        ROASTER[5] = registerMetaTileEntity(14528, new SimpleMachineMetaTileEntity(susyId("roaster.luv"), SuSyRecipeMaps.ROASTER_RECIPES, SusyTextures.ROASTER_OVERLAY, 6, true));
+        ROASTER[6] = registerMetaTileEntity(14529, new SimpleMachineMetaTileEntity(susyId("roaster.zpm"), SuSyRecipeMaps.ROASTER_RECIPES, SusyTextures.ROASTER_OVERLAY, 7, true));
+        ROASTER[7] = registerMetaTileEntity(14530, new SimpleMachineMetaTileEntity(susyId("roaster.uv"), SuSyRecipeMaps.ROASTER_RECIPES, SusyTextures.ROASTER_OVERLAY, 8, true));
+        ROASTER[8] = registerMetaTileEntity(14531, new SimpleMachineMetaTileEntity(susyId("roaster.uhv"), SuSyRecipeMaps.ROASTER_RECIPES, SusyTextures.ROASTER_OVERLAY, 9, true));
+        ROASTER[9] = registerMetaTileEntity(14532, new SimpleMachineMetaTileEntity(susyId("roaster.uev"), SuSyRecipeMaps.ROASTER_RECIPES, SusyTextures.ROASTER_OVERLAY, 10, true));
+        ROASTER[10] = registerMetaTileEntity(14533, new SimpleMachineMetaTileEntity(susyId("roaster.uiv"), SuSyRecipeMaps.ROASTER_RECIPES, SusyTextures.ROASTER_OVERLAY, 11, true));
+        ROASTER[11] = registerMetaTileEntity(14534, new SimpleMachineMetaTileEntity(susyId("roaster.uxv"), SuSyRecipeMaps.ROASTER_RECIPES, SusyTextures.ROASTER_OVERLAY, 12, true));
+        ROASTER[12] = registerMetaTileEntity(14535, new SimpleMachineMetaTileEntity(susyId("roaster.opv"), SuSyRecipeMaps.ROASTER_RECIPES, SusyTextures.ROASTER_OVERLAY, 13, true));
+
     }
 
     private static @NotNull ResourceLocation susyId(@NotNull String name) {
@@ -62,5 +76,6 @@ public class SuSyMetaTileEntities {
     static{
         LATEX_COLLECTOR = new MetaTileEntityLatexCollector[GTValues.EV];
         VULCANIZING_PRESS = new SimpleMachineMetaTileEntity[GTValues.EV];
+        ROASTER = new SimpleMachineMetaTileEntity[GTValues.OpV];
     }
 }
