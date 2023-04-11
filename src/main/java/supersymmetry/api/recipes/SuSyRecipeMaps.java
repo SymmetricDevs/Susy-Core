7b485270a7db0d3099a747e0cda4b7a90ca19a14
LetterN <24603524+LetterN@users.noreply.github.com>
2023-04-11 09:22:47 +0800

    fix

--- src/main/java/supersymmetry/api/recipes/SuSyRecipeMaps.java
+++ src/main/java/supersymmetry/api/recipes/SuSyRecipeMaps.java
@@ -18,19 +18,13 @@ public class SuSyRecipeMaps {
     static {
         COOLING_RECIPES = (new RecipeMap("magnetic_refrigerator", 1, 3, 0, 3, 0, 0, 0, 1, new SimpleRecipeBuilder(), false))
             .setSound(GTSoundEvents.COOLING);
-        SINTERING_RECIPES = (new RecipeMap("magnetic_refrigerator", 1, 3, 0, 3, 0, 0, 0, 1, new SimpleRecipeBuilder(), false))
-            .setSound(GTSoundEvents.COOLING);
-
-    public static final RecipeMap<SinteringRecipeBuilder> SINTERING_RECIPES = new RecipeMap<>("sintering_oven", 1, 4, 1, 4, 1, 2, 0, 2, new SinteringRecipeBuilder(), false)
+        SINTERING_RECIPES = new RecipeMap<>("sintering_oven", 1, 4, 1, 4, 1, 2, 0, 2, new SinteringRecipeBuilder(), false)
             .setSound(GTSoundEvents.COMBUSTION);
-
-    public static final RecipeMap<PrimitiveRecipeBuilder> COAGULATION_RECIPES = new RecipeMap<>("coagulation_tank", 0, 2, 1, 1, 1, 2, 0, 0, new PrimitiveRecipeBuilder(), false);
-
-    public static final RecipeMap<SimpleRecipeBuilder> VULCANIZATION_RECIPES = new RecipeMap<>("vulcanizing_press", 1, 3, 1, 2, 0, 1, 0, 1, new SimpleRecipeBuilder(), false)
-        .setSlotOverlay(false, false, true, GuiTextures.MOLD_OVERLAY)        
-        .setSound(GTSoundEvents.COMBUSTION);
-
-    public static final RecipeMap<SimpleRecipeBuilder> ROASTER_RECIPES = new RecipeMap<>("roaster", 1, 2, 0, 2, 0, 0, 0, 3, new SimpleRecipeBuilder(), false)
+        COAGULATION_RECIPES = new RecipeMap<>("coagulation_tank", 0, 2, 1, 1, 1, 2, 0, 0, new PrimitiveRecipeBuilder(), false);
+        VULCANIZATION_RECIPES = new RecipeMap<>("vulcanizing_press", 1, 3, 1, 2, 0, 1, 0, 1, new SimpleRecipeBuilder(), false)
+            .setSlotOverlay(false, false, true, GuiTextures.MOLD_OVERLAY)        
             .setSound(GTSoundEvents.COMBUSTION);
-
+        ROASTER_RECIPES = new RecipeMap<>("roaster", 1, 2, 0, 2, 0, 0, 0, 3, new SimpleRecipeBuilder(), false)
+            .setSound(GTSoundEvents.COMBUSTION);
+    }
 }
