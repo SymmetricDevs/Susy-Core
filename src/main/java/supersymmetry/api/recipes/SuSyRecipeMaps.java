8cb49d329d71a4874dce5b3a76f037708f0ef61d
LetterN <24603524+LetterN@users.noreply.github.com>
2023-04-11 13:54:19 +0800

    revert regression + stop using depricated constructor

--- src/main/java/supersymmetry/api/recipes/SuSyRecipeMaps.java
+++ src/main/java/supersymmetry/api/recipes/SuSyRecipeMaps.java
@@ -5,25 +5,26 @@ import gregtech.api.recipes.RecipeMap;
 import gregtech.api.recipes.builders.PrimitiveRecipeBuilder;
 import gregtech.api.recipes.builders.SimpleRecipeBuilder;
 import gregtech.core.sound.GTSoundEvents;
+import supersymmetry.api.recipes.builders.CoilingCoilRecipeBuilder;
 import supersymmetry.api.recipes.builders.SinteringRecipeBuilder;
 
 public class SuSyRecipeMaps {
-    public static final RecipeMap<SimpleRecipeBuilder> COOLING_RECIPES;
+    public static final RecipeMap<CoilingCoilRecipeBuilder> COOLING_RECIPES;
     public static final RecipeMap<SinteringRecipeBuilder> SINTERING_RECIPES;
     public static final RecipeMap<PrimitiveRecipeBuilder> COAGULATION_RECIPES;
     public static final RecipeMap<SimpleRecipeBuilder> VULCANIZATION_RECIPES;
     public static final RecipeMap<SimpleRecipeBuilder> ROASTER_RECIPES;
 
     static {
-        COOLING_RECIPES = (new RecipeMap("magnetic_refrigerator", 1, 3, 0, 3, 0, 0, 0, 1, new SimpleRecipeBuilder(), false))
+        COOLING_RECIPES = (new RecipeMap<>("magnetic_refrigerator", 3, 3, 0, 1, new CoilingCoilRecipeBuilder(), false))
             .setSound(GTSoundEvents.COOLING);
-        SINTERING_RECIPES = new RecipeMap<>("sintering_oven", 1, 4, 1, 4, 1, 2, 0, 2, new SinteringRecipeBuilder(), false)
+        SINTERING_RECIPES = new RecipeMap<>("sintering_oven", 4, 4, 2, 2, new SinteringRecipeBuilder(), false)
             .setSound(GTSoundEvents.COMBUSTION);
-        COAGULATION_RECIPES = new RecipeMap<>("coagulation_tank", 0, 2, 1, 1, 1, 2, 0, 0, new PrimitiveRecipeBuilder(), false);
-        VULCANIZATION_RECIPES = new RecipeMap<>("vulcanizing_press", 1, 3, 1, 2, 0, 1, 0, 1, new SimpleRecipeBuilder(), false)
+        COAGULATION_RECIPES = new RecipeMap<>("coagulation_tank", 2,1,2, 0, new PrimitiveRecipeBuilder(), false);
+        VULCANIZATION_RECIPES = new RecipeMap<>("vulcanizing_press", 3, 2, 1, 1, new SimpleRecipeBuilder(), false)
             .setSlotOverlay(false, false, true, GuiTextures.MOLD_OVERLAY)        
             .setSound(GTSoundEvents.COMBUSTION);
-        ROASTER_RECIPES = new RecipeMap<>("roaster", 1, 2, 0, 2, 0, 0, 0, 3, new SimpleRecipeBuilder(), false)
+        ROASTER_RECIPES = new RecipeMap<>("roaster", 2, 2, 0, 3, new SimpleRecipeBuilder(), false)
             .setSound(GTSoundEvents.COMBUSTION);
     }
 }
