5e184bea166f3a08cc3aac9feba2c776f0e5c7d4
Loxo <loxo.Minecraft@gmail.com>
2023-04-12 21:16:26 +0200

    Lead Drum and Latex temperature fix

--- src/main/java/supersymmetry/common/materials/SuSyUnknownCompositionMaterials.java
+++ src/main/java/supersymmetry/common/materials/SuSyUnknownCompositionMaterials.java
@@ -1,5 +1,6 @@
 package supersymmetry.common.materials;
 
+import gregtech.api.fluids.fluidType.FluidTypes;
 import gregtech.api.unification.material.Material;
 
 import static supersymmetry.common.materials.SusyMaterials.*;
@@ -8,10 +9,8 @@ public class SuSyUnknownCompositionMaterials {
 
     public static void init() {
         Latex = new Material.Builder(27050, "latex")
+                .dust().fluid(FluidTypes.LIQUID)
                 .color(0xFFFADA)
-                .dust()
-                .fluid()
-                .fluidTemp(293)
                 .build();
     }
 }
