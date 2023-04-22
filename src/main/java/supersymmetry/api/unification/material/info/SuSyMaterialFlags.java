0eea4faa3bd46f7c417e22c4eb6e8b5b63388901
Loxo <loxo.Minecraft@gmail.com>
2023-04-22 23:14:28 +0200

    Trying to add a new material flag try 2

---
+++ src/main/java/supersymmetry/api/unification/material/info/SuSyMaterialFlags.java
@@ -0,0 +1,13 @@
+package supersymmetry.api.unification.material.info;
+
+import gregtech.api.unification.material.info.MaterialFlag;
+import gregtech.api.unification.material.properties.PropertyKey;
+
+public class SuSyMaterialFlags {
+    public static MaterialFlag GENERATE_CATALYST_BED = (new MaterialFlag.Builder("generate_catalyst_bed"))
+            .requireProps(new PropertyKey[]{PropertyKey.DUST})
+            .build();
+
+    public SuSyMaterialFlags(){
+    }
+}
