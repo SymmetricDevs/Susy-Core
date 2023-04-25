f9f8d6ac75d6266d2adfdbee5f04b13322737341
Loxo <loxo.Minecraft@gmail.com>
2023-04-25 20:29:57 +0200

    Add catalyst pellets, improve existing catalyst beds.

--- src/main/java/supersymmetry/common/item/SuSyMetaItems.java
+++ src/main/java/supersymmetry/common/item/SuSyMetaItems.java
@@ -1,14 +1,17 @@
 package supersymmetry.common.item;
 
+import gregtech.api.items.metaitem.MetaItem;
 import gregtech.api.items.metaitem.StandardMetaItem;
 
 public class SuSyMetaItems {
 
     private static StandardMetaItem metaItem;
 
+    public static MetaItem.MetaValueItem CATALYST_BED_SUPPORT_GRID;
+
     public static void initMetaItems() {
-        //metaItem = new StandardMetaItem();
-        //metaItem.setRegistryName("meta_item");
+        metaItem = new StandardMetaItem();
+        metaItem.setRegistryName("meta_item");
     }
 
     public static void initSubItems() {
@@ -17,6 +20,6 @@ public class SuSyMetaItems {
 
     private static void initMetaItem() {
         // initialize metaitems here
-        // ex: META_VALUE_ITEM = metaitem.addItem(id, "name");
+        CATALYST_BED_SUPPORT_GRID = metaItem.addItem(1, "catalyst_bed_support_grid");
     }
 }
