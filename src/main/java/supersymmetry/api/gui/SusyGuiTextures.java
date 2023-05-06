d00965b5c00c86c09e1c9aa96f6af89af8e9979f
igobylotsofnames <igblon@outlook.com>
2023-05-06 12:33:14 -0700

    Add Slot Textures
    As requested by MTBO
    
    (Textures by Monstro)

--- src/main/java/supersymmetry/api/gui/SusyGuiTextures.java
+++ src/main/java/supersymmetry/api/gui/SusyGuiTextures.java
@@ -1,9 +1,21 @@
 package supersymmetry.api.gui;
 
 import gregtech.api.gui.resources.SteamTexture;
+import gregtech.api.gui.resources.TextureArea;
 
 public class SusyGuiTextures {
     public static final SteamTexture PROGRESS_BAR_MIXER_STEAM = SteamTexture.fullImage("textures/gui/progress_bar/progress_bar_mixer_%s.png");
     public static final SteamTexture FLUID_SLOT_STEAM = SteamTexture.fullImage("textures/gui/base/fluid_slot_%s.png");
     public static final SteamTexture MOLD_OVERLAY_STEAM = SteamTexture.fullImage("textures/gui/overlay/mold_overlay_%s.png");
+
+    public static final TextureArea CATALYST_BED_OVERLAY  = TextureArea.fullImage("textures/gui/overlay/catalyst_bed_overlay.png");
+    public static final TextureArea CATALYST_PELLET_OVERLAY = TextureArea.fullImage("textures/gui/overlay/catalyst_pellet_overlay.png");
+    public static final TextureArea ELECTRODE_OVERLAY = TextureArea.fullImage("textures/gui/overlay/electrode_overlay.png");
+    public static final TextureArea ELECTROMAGNETIC_SEPARATOR_FLUID_OVERLAY = TextureArea.fullImage("textures/gui/overlay/electromagnetic_separator_fluid_overlay.png");
+    public static final TextureArea ELECTROMAGNETIC_SEPARATOR_ITEM_OVERLAY = TextureArea.fullImage("textures/gui/overlay/electromagnetic_separator_item_overlay.png");
+    public static final TextureArea LARGE_REACTOR_FLUID_OVERLAY = TextureArea.fullImage("textures/gui/overlay/large_reactor_fluid_overlay.png");
+    public static final TextureArea LARGE_REACTOR_ITEM_OVERLAY = TextureArea.fullImage("textures/gui/overlay/large_reactor_item_overlay.png");
+    public static final TextureArea SIFTER_FLUID_OVERLAY = TextureArea.fullImage("textures/gui/overlay/sifter_fluid_overlay.png");
+    public static final TextureArea SIFTER_ITEM_INPUT_OVERLAY = TextureArea.fullImage("textures/gui/overlay/sifter_item_input_overlay.png");
+    public static final TextureArea SIFTER_ITEM_OUTPUT_OVERLAY = TextureArea.fullImage("textures/gui/overlay/sifter_item_output_overlay.png");
 }
--- src/main/java/supersymmetry/api/recipes/SuSyRecipeMaps.java
+++ src/main/java/supersymmetry/api/recipes/SuSyRecipeMaps.java
@@ -8,9 +8,11 @@ import gregtech.core.sound.GTSoundEvents;
 import gregtech.api.recipes.builders.PrimitiveRecipeBuilder;
 import gregtech.api.recipes.builders.SimpleRecipeBuilder;
 import gregtechfoodoption.recipe.GTFORecipeMaps;
+import supersymmetry.api.gui.SusyGuiTextures;
 import supersymmetry.api.recipes.builders.CoilingCoilRecipeBuilder;
 import supersymmetry.api.recipes.builders.NoEnergyRecipeBuilder;
 import supersymmetry.api.recipes.builders.SinteringRecipeBuilder;
+import supersymmetry.client.renderer.textures.SusyTextures;
 
 
 public class SuSyRecipeMaps {
@@ -35,29 +37,54 @@ public class SuSyRecipeMaps {
             .setSound(GTSoundEvents.CENTRIFUGE);
 
     public static final RecipeMap<SimpleRecipeBuilder> CSTR_RECIPES = new RecipeMap<>("continuous_stirred_tank_reactor", 0, 0, 4, 2, new SimpleRecipeBuilder(), false)
+            .setSlotOverlay(false, true, false, GuiTextures.MOLECULAR_OVERLAY_3)
+            .setSlotOverlay(false, true, true, GuiTextures.MOLECULAR_OVERLAY_4)
+            .setSlotOverlay(true, true, GuiTextures.VIAL_OVERLAY_2)
             .setSound(GTSoundEvents.CHEMICAL_REACTOR);
 
     public static final RecipeMap<SimpleRecipeBuilder> FIXED_BED_REACTOR_RECIPES = new RecipeMap<>("fixed_bed_reactor", 1, 1, 3, 2, new SimpleRecipeBuilder(), false)
+            .setSlotOverlay(false, false, SusyGuiTextures.CATALYST_BED_OVERLAY)
+            .setSlotOverlay(false, true, GuiTextures.MOLECULAR_OVERLAY_3)
+            .setSlotOverlay(true, true, GuiTextures.MOLECULAR_OVERLAY_3)
             .setSound(GTSoundEvents.CHEMICAL_REACTOR);
 
     public static final RecipeMap<SimpleRecipeBuilder> TRICKLE_BED_REACTOR_RECIPES = new RecipeMap<>("trickle_bed_reactor", 1, 0, 3, 2, new SimpleRecipeBuilder(), false)
+            .setSlotOverlay(false, false, SusyGuiTextures.CATALYST_PELLET_OVERLAY)
+            .setSlotOverlay(false, true, GuiTextures.MOLECULAR_OVERLAY_3)
+            .setSlotOverlay(true, true, GuiTextures.MOLECULAR_OVERLAY_3)
             .setSound(GTSoundEvents.CHEMICAL_REACTOR);
 
-    public static final RecipeMap<SimpleRecipeBuilder> CRYSTALLIZER_RECIPES = new RecipeMap<>("crystallizer",1, 1, 3, 1, new SimpleRecipeBuilder(), false);
+    public static final RecipeMap<SimpleRecipeBuilder> CRYSTALLIZER_RECIPES = new RecipeMap<>("crystallizer",1, 1, 3, 1, new SimpleRecipeBuilder(), false)
+            .setSound(GTSoundEvents.CHEMICAL_REACTOR);
 
     public static final RecipeMap<SimpleRecipeBuilder> BUBBLE_COLUMN_REACTOR_RECIPES = new RecipeMap<>("bubble_column_reactor", 1, 0, 3, 2, new SimpleRecipeBuilder(), false)
+            .setSlotOverlay(false, false, SusyGuiTextures.LARGE_REACTOR_ITEM_OVERLAY)
+            .setSlotOverlay(false, true, GuiTextures.MOLECULAR_OVERLAY_3)
+            .setSlotOverlay(true, true, GuiTextures.MOLECULAR_OVERLAY_3)
             .setSound(GTSoundEvents.CHEMICAL_REACTOR);
 
     public static final RecipeMap<SimpleRecipeBuilder> DRYER = new RecipeMap<>("dryer", 1, 1, 1, 1, new SimpleRecipeBuilder(), false)
-            .setSound(GTSoundEvents.CHEMICAL_REACTOR);
+            .setSound(GTSoundEvents.COOLING);
 
     public static final RecipeMap<SimpleRecipeBuilder> FLUIDIZED_BED_REACTOR_RECIPES = new RecipeMap<>("fluidized_bed_reactor", 2, 1, 3, 2, new SimpleRecipeBuilder(), false)
+            .setSlotOverlay(false, true, SusyGuiTextures.LARGE_REACTOR_FLUID_OVERLAY)
+            .setSlotOverlay(true, true, SusyGuiTextures.LARGE_REACTOR_FLUID_OVERLAY)
+            .setSlotOverlay(false, false, SusyGuiTextures.LARGE_REACTOR_ITEM_OVERLAY)
+            .setSlotOverlay(true, false, SusyGuiTextures.LARGE_REACTOR_ITEM_OVERLAY)
             .setSound(GTSoundEvents.CHEMICAL_REACTOR);
 
     public static final RecipeMap<SimpleRecipeBuilder> POLYMERIZATION_RECIPES = new RecipeMap<>("polymerization_tank", 2, 1, 2, 1, new SimpleRecipeBuilder(), false)
+            .setSlotOverlay(false, true, GuiTextures.MOLECULAR_OVERLAY_3)
+            .setSlotOverlay(true, true, GuiTextures.MOLECULAR_OVERLAY_3)
+            .setSlotOverlay(false, false, GuiTextures.MOLECULAR_OVERLAY_1)
+            .setSlotOverlay(true, false, GuiTextures.MOLECULAR_OVERLAY_1)
             .setSound(GTSoundEvents.CHEMICAL_REACTOR);
 
     public static final RecipeMap<SimpleRecipeBuilder> ELECTROLYTIC_CELL_RECIPES = new RecipeMap<>("electrolytic_cell", 3, 3, 2, 4, new SimpleRecipeBuilder(), false)
+            .setSlotOverlay(false, true, GuiTextures.MOLECULAR_OVERLAY_3)
+            .setSlotOverlay(true, true, GuiTextures.MOLECULAR_OVERLAY_3)
+            .setSlotOverlay(false, false, SusyGuiTextures.ELECTRODE_OVERLAY)
+            .setSlotOverlay(true, false, GuiTextures.MOLECULAR_OVERLAY_1)
             .setSound(GTSoundEvents.ELECTROLYZER);
 
     public static final RecipeMap<SimpleRecipeBuilder> COKING_RECIPES = new RecipeMap<>("coking_tower", 1, 1, 3, 2, new SimpleRecipeBuilder(), false)
@@ -66,13 +93,15 @@ public class SuSyRecipeMaps {
     public static final RecipeMap<SimpleRecipeBuilder> VACUUM_DISTILLATION_RECIPES = new RecipeMap<>("vacuum_distillation", 0, 0, 1, 1, new SimpleRecipeBuilder(), false)
             .setSound(GTSoundEvents.CHEMICAL_REACTOR);
 
-    public static final RecipeMap<SimpleRecipeBuilder> CATALYTIC_REFORMER_RECIPES = new RecipeMap<>("catalytic_reformer_recipes", 1, 1, 0, 0, 1, 2, 1, 4, new SimpleRecipeBuilder(), false)
+    public static final RecipeMap<SimpleRecipeBuilder> CATALYTIC_REFORMER_RECIPES = new RecipeMap<>("catalytic_reformer_recipes", 1, 0, 2, 4, new SimpleRecipeBuilder(), false)
             .setProgressBar(GuiTextures.PROGRESS_BAR_CRACKING, ProgressWidget.MoveType.HORIZONTAL)
             .setSound(GTSoundEvents.FURNACE);
 
     public static final RecipeMap<NoEnergyRecipeBuilder> SMOKE_STACK = new RecipeMap<>("smoke_stack", 0, 0, 1, 0, new NoEnergyRecipeBuilder(), false);
 
     public static final RecipeMap<SimpleRecipeBuilder> FERMENTATION_VAT_RECIPES = new RecipeMap<>("vat_fermentation", 3, 3, 3, 3, new SimpleRecipeBuilder(), false)
+            .setSlotOverlay(false, false, true, GuiTextures.DUST_OVERLAY)
+            .setSlotOverlay(true, false, true, GuiTextures.DUST_OVERLAY)
             .setSound(GTSoundEvents.CHEMICAL_REACTOR);
 
     public static final RecipeMap<SimpleRecipeBuilder> UV_RECIPES = new RecipeMap<>("uv_light_box", 2, 1, 0, 0, new SimpleRecipeBuilder(), false)
@@ -94,5 +123,11 @@ public class SuSyRecipeMaps {
         RecipeMaps.ELECTROMAGNETIC_SEPARATOR_RECIPES.setMaxFluidInputs(1);
         GTFORecipeMaps.GREENHOUSE_RECIPES.setMaxFluidInputs(4);
         RecipeMaps.PYROLYSE_RECIPES.setMaxFluidOutputs(3);
+        RecipeMaps.ELECTROMAGNETIC_SEPARATOR_RECIPES.setSlotOverlay(false, false, SusyGuiTextures.ELECTROMAGNETIC_SEPARATOR_ITEM_OVERLAY);
+        RecipeMaps.ELECTROMAGNETIC_SEPARATOR_RECIPES.setSlotOverlay(false, true, SusyGuiTextures.ELECTROMAGNETIC_SEPARATOR_FLUID_OVERLAY);
+        RecipeMaps.SIFTER_RECIPES.setSlotOverlay(false, true, SusyGuiTextures.SIFTER_FLUID_OVERLAY);
+        RecipeMaps.SIFTER_RECIPES.setSlotOverlay(true, true, SusyGuiTextures.SIFTER_FLUID_OVERLAY);
+        RecipeMaps.SIFTER_RECIPES.setSlotOverlay(false, false, SusyGuiTextures.SIFTER_ITEM_INPUT_OVERLAY);
+        RecipeMaps.SIFTER_RECIPES.setSlotOverlay(true, false, SusyGuiTextures.SIFTER_ITEM_OUTPUT_OVERLAY);
     }
 }
