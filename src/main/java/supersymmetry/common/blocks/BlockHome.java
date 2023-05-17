a3657c5c661e6f7aa5a347108457681ea156c75a
Tech22 <37029404+TechLord22@users.noreply.github.com>
2023-05-17 12:57:02 -0400

    fix spawn location crash

--- src/main/java/supersymmetry/common/blocks/BlockHome.java
+++ src/main/java/supersymmetry/common/blocks/BlockHome.java
@@ -1,26 +1,21 @@
 package supersymmetry.common.blocks;
 
 import gregtech.api.block.VariantBlock;
-import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.properties.PropertyDirection;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.*;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public class BlockHome extends VariantBlock<BlockHome.HomeType> {
@@ -45,20 +40,57 @@ public class BlockHome extends VariantBlock<BlockHome.HomeType> {
     }
 
     @Override
-    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
-        if (worldIn.isRemote) {
-            return false;
-        } else {
+    public boolean onBlockActivated(@NotNull World worldIn, @NotNull BlockPos pos, @NotNull IBlockState state,
+                                    @NotNull EntityPlayer playerIn, @NotNull EnumHand hand, @NotNull EnumFacing facing,
+                                    float hitX, float hitY, float hitZ) {
+        if (!worldIn.isRemote) {
             if (worldIn.provider.canRespawnHere() && worldIn.getBiome(pos) != net.minecraft.init.Biomes.HELL) {
+                playerIn.setSpawnPoint(pos, true);
                 playerIn.sendStatusMessage(new TextComponentTranslation("tile.home.allowed"), true);
-                net.minecraftforge.event.ForgeEventFactory.onPlayerSpawnSet(playerIn, pos, true);
-                playerIn.bedLocation = pos;
-                playerIn.setSpawnPoint(playerIn.bedLocation, false);
             } else {
                 playerIn.sendStatusMessage(new TextComponentTranslation("tile.home.denied"), true);
             }
-            return true;
         }
+        return true;
+    }
+
+    @Override
+    @Nullable
+    public BlockPos getBedSpawnPosition(@NotNull IBlockState state, @NotNull IBlockAccess world, @NotNull BlockPos pos, @Nullable EntityPlayer player) {
+        final int posX = pos.getX();
+        final int posY = pos.getY();
+        final int posZ = pos.getZ();
+
+        BlockPos.MutableBlockPos mutablePos = new BlockPos.MutableBlockPos(pos);
+        for (int x = -1; x <= 1; x++) {
+            for (int z = -1; z <= 1; z++) {
+                mutablePos.setPos(posX + x, posY, posZ + z);
+                if (hasRoomForPlayer(world, mutablePos)) {
+                    return mutablePos;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @param world the world to check
+     * @param pos   the position of the block
+     * @return if there is room for the player to spawn at the block
+     */
+    private static boolean hasRoomForPlayer(@NotNull IBlockAccess world, @NotNull BlockPos pos) {
+        BlockPos.MutableBlockPos mutablePos = new BlockPos.MutableBlockPos(pos);
+        if (world.getBlockState(mutablePos).getMaterial().isSolid()) {
+            return false;
+        }
+
+        mutablePos.move(EnumFacing.UP);
+        if (world.getBlockState(mutablePos).getMaterial().isSolid()) {
+            return false;
+        }
+
+        mutablePos.move(EnumFacing.DOWN, 2);
+        return world.getBlockState(mutablePos).isSideSolid(world, mutablePos, EnumFacing.UP);
     }
 
     public enum HomeType implements IStringSerializable {
