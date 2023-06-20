12d32e34b891c9c8cb81f1e7908a05ed117b6b61
igobylotsofnames <igblon@outlook.com>
2023-06-20 05:37:49 -0700

    Update MetaTileEntitySUSYLargeTurbine.java

--- src/main/java/supersymmetry/common/metatileentities/multi/electric/MetaTileEntitySUSYLargeTurbine.java
+++ src/main/java/supersymmetry/common/metatileentities/multi/electric/MetaTileEntitySUSYLargeTurbine.java
@@ -14,6 +14,7 @@ import gregtech.api.recipes.RecipeMap;
 import gregtech.api.util.BlockInfo;
 import gregtech.client.renderer.ICubeRenderer;
 import gregtech.common.blocks.BlockMetalCasing;
+import gregtech.common.blocks.BlockTurbineCasing;
 import gregtech.common.blocks.MetaBlocks;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
