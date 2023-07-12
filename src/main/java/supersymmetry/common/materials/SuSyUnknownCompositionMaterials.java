81d4e512c5369a092d5f1e454e7005ae2a88bcca
igobylotsofnames <igblon@outlook.com>
2023-07-11 18:46:00 -0700

    gt 2.7 compat

--- src/main/java/supersymmetry/common/materials/SuSyUnknownCompositionMaterials.java
+++ src/main/java/supersymmetry/common/materials/SuSyUnknownCompositionMaterials.java
@@ -2,6 +2,7 @@ package supersymmetry.common.materials;
 
 import gregtech.api.fluids.fluidType.FluidTypes;
 import gregtech.api.unification.material.Material;
+import gregtech.api.util.GTUtility;
 
 import static supersymmetry.common.materials.SusyMaterials.*;
 
@@ -9,17 +10,17 @@ public class SuSyUnknownCompositionMaterials {
 
     public static void init() {
 
-        Latex = new Material.Builder(27050, "latex")
+        Latex = new Material.Builder(27050, GTUtility.gregtechId("latex"))
                 .dust().fluid(FluidTypes.LIQUID)
                 .color(0xFFFADA)
                 .build();
 
-        Mud = new Material.Builder(27051, "mud")
+        Mud = new Material.Builder(27051, GTUtility.gregtechId("mud"))
                 .fluid(FluidTypes.LIQUID)
                 .color(0x211b14)
                 .build();
 
-        Seawater = new Material.Builder(27052, "sea_water")
+        Seawater = new Material.Builder(27052, GTUtility.gregtechId("sea_water"))
                 .fluid()
                 .color(0x3c5bc2)
                 .build();
