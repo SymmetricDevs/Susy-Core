d69262cc3cd2b641d984b953024997291d91b0aa
Zalgo239 <67443118+Zalgo239@users.noreply.github.com>
2023-07-12 20:59:11 +0200

    Switch to using SusyCore's mod id

--- src/main/java/supersymmetry/common/materials/SuSyUnknownCompositionMaterials.java
+++ src/main/java/supersymmetry/common/materials/SuSyUnknownCompositionMaterials.java
@@ -3,6 +3,7 @@ package supersymmetry.common.materials;
 import gregtech.api.fluids.fluidType.FluidTypes;
 import gregtech.api.unification.material.Material;
 import gregtech.api.util.GTUtility;
+import supersymmetry.api.util.SusyUtility;
 
 import static supersymmetry.common.materials.SusyMaterials.*;
 
@@ -10,17 +11,17 @@ public class SuSyUnknownCompositionMaterials {
 
     public static void init() {
 
-        Latex = new Material.Builder(27050, GTUtility.gregtechId("latex"))
+        Latex = new Material.Builder(27050, SusyUtility.susyId("latex"))
                 .dust().fluid(FluidTypes.LIQUID)
                 .color(0xFFFADA)
                 .build();
 
-        Mud = new Material.Builder(27051, GTUtility.gregtechId("mud"))
+        Mud = new Material.Builder(27051, SusyUtility.susyId("mud"))
                 .fluid(FluidTypes.LIQUID)
                 .color(0x211b14)
                 .build();
 
-        Seawater = new Material.Builder(27052, GTUtility.gregtechId("sea_water"))
+        Seawater = new Material.Builder(27052, SusyUtility.susyId("sea_water"))
                 .fluid()
                 .color(0x3c5bc2)
                 .build();
