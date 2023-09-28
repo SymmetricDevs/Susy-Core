e23c2cb7ee3d23cfa4834731b7e6100cfafed9d4
ISurrealI <merhabalarexoria@gmail.com>
2023-09-28 20:14:08 +0300

    fix transformer and reflection

--- src/main/java/supersymmetry/asm/SusyTransformer.java
+++ src/main/java/supersymmetry/asm/SusyTransformer.java
@@ -4,32 +4,32 @@ import net.minecraft.launchwrapper.IClassTransformer;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.ClassNode;
 import supersymmetry.api.SusyLog;
+import supersymmetry.asm.visitors.StockLoaderVisitor;
 
 public class SusyTransformer implements IClassTransformer, Opcodes {
 
     @Override
     public byte[] transform(String name, String transformedName, byte[] basicClass) {
-        String internalName = transformedName.replace('.', '/');
-/*      This is where any visitors that need to be invoked are ran
-        if (internalName.equals(BlockStoneSmoothVisitor.TARGET_CLASS_NAME)) {
-            ClassReader classReader = new ClassReader(basicClass);
-            ClassWriter classWriter = new ClassWriter(0);
-            classReader.accept(new BlockStoneSmoothVisitor(classWriter), 0);
-            byte[] sus = classWriter.toByteArray();
-            SusyLog.logger.info(String.format("Transformed class %s", name));
-            return sus;
-        }
-        */
-        if(internalName.equals(DefinitionManagerVisitor.TARGET_CLASS_NAME)) {
-            ClassReader cr = new ClassReader(basicClass);
-            ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
-            cr.accept(new DefinitionManagerVisitor(cw), 0);
-            byte[] sus = cw.toByteArray();
-            SusyLog.logger.info(String.format("Transformed class %s", name));
-            return sus;
+
+        if (name.equals("cam72cam.immersiverailroading.registry.DefinitionManager")) {
+            SusyLog.logger.info("Transforming {}", name);
+            return writeStockLoader(basicClass);
         }
 
         return basicClass;
+    }
+
+    private byte[] writeStockLoader(byte[] basicClass) {
+        ClassReader reader = new ClassReader(basicClass);
+        ClassNode cls = new StockLoaderVisitor();
+
+        reader.accept(cls, 0);
+
+        ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+        cls.accept(writer);
+
+        return writer.toByteArray();
     }
 }
