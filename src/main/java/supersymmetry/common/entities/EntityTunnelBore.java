94b992495440bfa8f2b51e72d4a1ceef7fb7c594
Loxo <loxo.Minecraft@gmail.com>
2023-10-12 17:20:07 +0200

    Improve placement logic and reduce boring machine speed

--- src/main/java/supersymmetry/common/entities/EntityTunnelBore.java
+++ src/main/java/supersymmetry/common/entities/EntityTunnelBore.java
@@ -2,6 +2,7 @@ package supersymmetry.common.entities;
 
 import cam72cam.immersiverailroading.IRItems;
 import cam72cam.immersiverailroading.entity.Locomotive;
+import cam72cam.immersiverailroading.entity.physics.Simulation;
 import cam72cam.immersiverailroading.entity.physics.SimulationState;
 import cam72cam.immersiverailroading.items.nbt.RailSettings;
 import cam72cam.immersiverailroading.library.*;
@@ -17,6 +18,8 @@ import cam72cam.mod.math.Vec3i;
 import cam72cam.mod.world.World;
 import net.minecraft.nbt.NBTTagFloat;
 import net.minecraft.nbt.NBTTagInt;
+import net.minecraft.nbt.NBTTagString;
+import supersymmetry.api.SusyLog;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,35 +54,40 @@ public class EntityTunnelBore extends Locomotive {
     @Override
     public void onTick() {
         super.onTick();
-        SimulationState state = getCurrentState();
-        if(state != null) {
-            Vec3d positionFront = VecUtil.fromWrongYawPitch(state.config.offsetFront, state.yaw, state.pitch).add(state.position);
-            ITrack trackFront = MovementTrack.findTrack(state.config.world, positionFront, state.yawFront, state.config.gauge.value());
-            // We have reached the end of the track
-            if(trackFront == null) {
-                /*RailSettings settings = new RailSettings(
-                        Gauge.standard(),
-                        "immersiverailroading:track/bmtrack.json",
-                        TrackItems.STRAIGHT,
-                        10,
-                        90,
-                        1,
-                        TrackPositionType.FIXED,
-                        TrackSmoothing.BOTH,
-                        TrackDirection.valueOf("flexible"),
-                        ItemStack.EMPTY,
-                        ItemStack.EMPTY,
-                        false,
-                        false
-                );*/
-                ItemStack trackBlueprintStack = new ItemStack(IRItems.ITEM_TRACK_BLUEPRINT, 0);
-                trackBlueprintStack.internal.setTagInfo("length", new NBTTagInt(10));
-                trackBlueprintStack.internal.setTagInfo("degrees", new NBTTagFloat(0));
-                PlacementInfo placementInfo = new PlacementInfo(trackBlueprintStack, this.getRotationYaw(), new Vec3d(0.5,0.5,0.5));
-                RailInfo railInfo = new RailInfo(trackBlueprintStack, placementInfo, (PlacementInfo)null);
-                World irWorld = getWorld();
-                BuilderBase trackBuilder = railInfo.getBuilder(irWorld, new Vec3i(getPosition()));
-                trackBuilder.build();
+        if(!this.states.isEmpty()) {
+            SimulationState currentState = getCurrentState();
+            int idx = this.states.indexOf(currentState);
+            SimulationState nextState = this.states.get(idx + 1);
+            if(nextState != null) {
+                Vec3d positionFront = VecUtil.fromWrongYawPitch(nextState.config.offsetFront, nextState.yaw, nextState.pitch).add(nextState.position);
+                ITrack trackFront = MovementTrack.findTrack(nextState.config.world, positionFront, nextState.yawFront, nextState.config.gauge.value());
+                // We have reached the end of the track
+                if(trackFront == null) {
+                    RailSettings settings = new RailSettings(
+                            Gauge.standard(),
+                            "immersiverailroading:track/bmtrack.json",
+                            TrackItems.STRAIGHT,
+                            10,
+                            90,
+                            1,
+                            TrackPositionType.FIXED,
+                            TrackSmoothing.BOTH,
+                            TrackDirection.NONE,
+                            ItemStack.EMPTY,
+                            ItemStack.EMPTY,
+                            false,
+                            false
+                    );
+                    ItemStack trackBlueprintStack = new ItemStack(IRItems.ITEM_TRACK_BLUEPRINT, 0);
+                    settings.write(trackBlueprintStack);
+                    PlacementInfo placementInfo = new PlacementInfo(trackBlueprintStack, this.getRotationYaw(), new Vec3d(0.5,0.5,0.5));
+                    RailInfo railInfo = new RailInfo(trackBlueprintStack, placementInfo, (PlacementInfo)null);
+                    World irWorld = getWorld();
+                    BuilderBase trackBuilder = railInfo.getBuilder(irWorld, new Vec3i(getPosition()));
+                    trackBuilder.build();
+                    Simulation.forceQuickUpdates = true;
+                    this.states = new ArrayList<>();
+                }
             }
         }
     }
