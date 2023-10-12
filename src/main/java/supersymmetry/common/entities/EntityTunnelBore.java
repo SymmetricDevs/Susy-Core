3c4c52435477676042f15c7a7b852cc1e459d0b9
Loxo <loxo.Minecraft@gmail.com>
2023-10-12 20:58:22 +0200

    Start work on tracksegments which help building and manipulating tracks.

--- src/main/java/supersymmetry/common/entities/EntityTunnelBore.java
+++ src/main/java/supersymmetry/common/entities/EntityTunnelBore.java
@@ -16,17 +16,15 @@ import cam72cam.mod.item.ItemStack;
 import cam72cam.mod.math.Vec3d;
 import cam72cam.mod.math.Vec3i;
 import cam72cam.mod.world.World;
-import net.minecraft.nbt.NBTTagFloat;
-import net.minecraft.nbt.NBTTagInt;
-import net.minecraft.nbt.NBTTagString;
-import supersymmetry.api.SusyLog;
+import supersymmetry.integration.immersiverailroading.control.TunnelBoreControl;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class EntityTunnelBore extends Locomotive {
 
-    private int length = 50;
+    private int distanceToGo = 50;
+    private ArrayList<TunnelBoreControl> controlSequence = new ArrayList<>();
     public FluidQuantity getTankCapacity() {
         return FluidQuantity.ZERO;
     }
@@ -63,30 +61,34 @@ public class EntityTunnelBore extends Locomotive {
                 ITrack trackFront = MovementTrack.findTrack(nextState.config.world, positionFront, nextState.yawFront, nextState.config.gauge.value());
                 // We have reached the end of the track
                 if(trackFront == null) {
-                    RailSettings settings = new RailSettings(
-                            Gauge.standard(),
-                            "immersiverailroading:track/bmtrack.json",
-                            TrackItems.STRAIGHT,
-                            10,
-                            90,
-                            1,
-                            TrackPositionType.FIXED,
-                            TrackSmoothing.BOTH,
-                            TrackDirection.NONE,
-                            ItemStack.EMPTY,
-                            ItemStack.EMPTY,
-                            false,
-                            false
-                    );
-                    ItemStack trackBlueprintStack = new ItemStack(IRItems.ITEM_TRACK_BLUEPRINT, 0);
-                    settings.write(trackBlueprintStack);
-                    PlacementInfo placementInfo = new PlacementInfo(trackBlueprintStack, this.getRotationYaw(), new Vec3d(0.5,0.5,0.5));
-                    RailInfo railInfo = new RailInfo(trackBlueprintStack, placementInfo, (PlacementInfo)null);
-                    World irWorld = getWorld();
-                    BuilderBase trackBuilder = railInfo.getBuilder(irWorld, new Vec3i(getPosition()));
-                    trackBuilder.build();
-                    Simulation.forceQuickUpdates = true;
-                    this.states = new ArrayList<>();
+                    if(this.distanceToGo > 0) {
+                        RailSettings settings = new RailSettings(
+                                Gauge.standard(),
+                                "immersiverailroading:track/bmtrack.json",
+                                TrackItems.STRAIGHT,
+                                this.distanceToGo >= 10 ? 10 : this.distanceToGo,
+                                90,
+                                1,
+                                TrackPositionType.FIXED,
+                                TrackSmoothing.BOTH,
+                                TrackDirection.NONE,
+                                ItemStack.EMPTY,
+                                ItemStack.EMPTY,
+                                false,
+                                false
+                        );
+                        ItemStack trackBlueprintStack = new ItemStack(IRItems.ITEM_TRACK_BLUEPRINT, 0);
+                        settings.write(trackBlueprintStack);
+                        PlacementInfo placementInfo = new PlacementInfo(trackBlueprintStack, this.getRotationYaw(), new Vec3d(0.5, 0.5, 0.5));
+                        RailInfo railInfo = new RailInfo(trackBlueprintStack, placementInfo, (PlacementInfo) null);
+                        World irWorld = getWorld();
+                        BuilderBase trackBuilder = railInfo.getBuilder(irWorld, new Vec3i(getPosition()));
+                        List<TrackBase> tracks = trackBuilder.getTracksForRender();
+                        trackBuilder.build();
+                        Simulation.forceQuickUpdates = true;
+                        this.states = new ArrayList<>();
+                        this.distanceToGo = this.distanceToGo >= 10 ? this.distanceToGo - 10 : 0;
+                    }
                 }
             }
         }
