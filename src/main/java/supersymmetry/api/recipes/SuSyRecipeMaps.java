a6de656cce043f7c4467a7efd8e2e1f6df617417
planetme <87911459+planetme@users.noreply.github.com>
2023-10-20 18:51:58 -0500

    add an item slot to CSTR for heating elements

--- src/main/java/supersymmetry/api/recipes/SuSyRecipeMaps.java
+++ src/main/java/supersymmetry/api/recipes/SuSyRecipeMaps.java
@@ -37,7 +37,7 @@ public class SuSyRecipeMaps {
             .setProgressBar(GuiTextures.PROGRESS_BAR_COMPRESS, ProgressWidget.MoveType.HORIZONTAL)
             .setSound(GTSoundEvents.CENTRIFUGE);
 
-    public static final RecipeMap<CatalystRecipeBuilder> CSTR_RECIPES = new RecipeMap<>("continuous_stirred_tank_reactor", 0, 0, 4, 2, new CatalystRecipeBuilder(), false)
+    public static final RecipeMap<CatalystRecipeBuilder> CSTR_RECIPES = new RecipeMap<>("continuous_stirred_tank_reactor", 1, 0, 4, 2, new CatalystRecipeBuilder(), false)
             .setSlotOverlay(false, true, false, GuiTextures.MOLECULAR_OVERLAY_3)
             .setSlotOverlay(false, true, true, GuiTextures.MOLECULAR_OVERLAY_4)
             .setSlotOverlay(true, true, GuiTextures.VIAL_OVERLAY_2)
