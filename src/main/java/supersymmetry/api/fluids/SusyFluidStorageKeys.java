0344ab7430876262282cf8ce08cda6db5a7d3936
planetme <87911459+planetme@users.noreply.github.com>
2024-01-11 16:02:05 -0600

    le correctif au chocolat

--- src/main/java/supersymmetry/api/fluids/SusyFluidStorageKeys.java
+++ src/main/java/supersymmetry/api/fluids/SusyFluidStorageKeys.java
@@ -22,6 +22,6 @@ public final class SusyFluidStorageKeys {
         UnaryOperator.identity(),
         m -> "susy.fluid.impure_slurry");
 
-    private FluidStorageKeys() {}
+    private SusyFluidStorageKeys() {}
     
 }
