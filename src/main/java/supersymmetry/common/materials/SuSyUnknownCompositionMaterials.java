e41efd6a9d3c0ce2812338d5a3835638b4c39e42
planetme <87911459+planetme@users.noreply.github.com>
2024-01-11 07:36:38 -0600

    move susycore mats to new system

--- src/main/java/supersymmetry/common/materials/SuSyUnknownCompositionMaterials.java
+++ src/main/java/supersymmetry/common/materials/SuSyUnknownCompositionMaterials.java
@@ -1,6 +1,5 @@
 package supersymmetry.common.materials;
 
-import gregtech.api.fluids.fluidType.FluidTypes;
 import gregtech.api.unification.material.Material;
 import supersymmetry.api.util.SuSyUtility;
 
@@ -11,17 +10,17 @@ public class SuSyUnknownCompositionMaterials {
     public static void init() {
 
         Latex = new Material.Builder(27050, SuSyUtility.susyId("latex"))
-                .dust().fluid(FluidTypes.LIQUID)
+                .dust().liquid()
                 .color(0xFFFADA)
                 .build();
 
         Mud = new Material.Builder(27051, SuSyUtility.susyId("mud"))
-                .fluid(FluidTypes.LIQUID)
+                .liquid()
                 .color(0x211b14)
                 .build();
 
         Seawater = new Material.Builder(27052, SuSyUtility.susyId("sea_water"))
-                .fluid()
+                .liquid()
                 .color(0x3c5bc2)
                 .build();
     }
