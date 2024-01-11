0115f9eab376f601b644acab8b9da5dadc6172f1
TechLord22 <37029404+techlord22@users.noreply.github.com>
2024-01-11 17:49:44 -0500

    finish porting drums

--- src/main/java/supersymmetry/common/metatileentities/single/storage/MetaTileEntityCryoDrum.java
+++ src/main/java/supersymmetry/common/metatileentities/single/storage/MetaTileEntityCryoDrum.java
@@ -3,18 +3,16 @@ package supersymmetry.common.metatileentities.single.storage;
 import codechicken.lib.colour.ColourRGBA;
 import codechicken.lib.raytracer.CuboidRayTraceResult;
 import codechicken.lib.render.CCRenderState;
+import codechicken.lib.render.pipeline.ColourMultiplier;
 import codechicken.lib.render.pipeline.IVertexOperation;
 import codechicken.lib.vec.Matrix4;
+import gregtech.api.capability.IPropertyFluidFilter;
 import gregtech.api.capability.impl.FilteredFluidHandler;
-import gregtech.api.capability.impl.ThermalFluidHandlerItemStack;
-import gregtech.api.fluids.FluidState;
-import gregtech.api.fluids.attribute.AttributedFluid;
-import gregtech.api.fluids.attribute.FluidAttribute;
-import gregtech.api.fluids.attribute.FluidAttributes;
+import gregtech.api.capability.impl.GTFluidHandlerItemStack;
+import gregtech.api.capability.impl.PropertyFluidFilter;
 import gregtech.api.gui.ModularUI;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
-import gregtech.api.unification.material.Material;
 import gregtech.api.util.GTUtility;
 import gregtech.client.renderer.texture.Textures;
 import gregtech.client.utils.TooltipHelper;
@@ -27,291 +25,275 @@ import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidTank;
 import net.minecraftforge.fluids.FluidUtil;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.templates.FluidHandlerItemStack;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.tuple.Pair;
-import supersymmetry.client.renderer.textures.SusyTextures;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import javax.annotation.Nullable;
 import java.io.IOException;
 import java.util.List;
+
+import static gregtech.api.capability.GregtechDataCodes.UPDATE_AUTO_OUTPUT;
 
 public class MetaTileEntityCryoDrum extends MetaTileEntity {
 
     private final int tankSize;
-    private final Material material;
     private FilteredFluidHandler fluidTank;
     private boolean isAutoOutput = false;
 
+    private final int maxFluidTemp;
     private final boolean isAcidProof;
     private final boolean isPlasmaProof;
 
-    int maxFluidTemp;
+    private final int color;
 
-    public MetaTileEntityCryoDrum(ResourceLocation metaTileEntityId, Material material, int tankSize, int maxFluidTemp, boolean isAcidProof, boolean isPlasmaProof) {
+    public MetaTileEntityCryoDrum(ResourceLocation metaTileEntityId, int tankSize, int maxFluidTemp, boolean isAcidProof,
+                                  boolean isPlasmaProof, int color) {
         super(metaTileEntityId);
-        this.material = material;
         this.tankSize = tankSize;
         this.maxFluidTemp = maxFluidTemp;
         this.isAcidProof = isAcidProof;
         this.isPlasmaProof = isPlasmaProof;
+        this.color = color;
         this.initializeInventory();
     }
 
+    @Override
     public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
-        return new MetaTileEntityCryoDrum(this.metaTileEntityId, this.material, this.tankSize, this.maxFluidTemp, this.isAcidProof, this.isPlasmaProof);
+        return new MetaTileEntityCryoDrum(this.metaTileEntityId, this.tankSize, this.maxFluidTemp, this.isAcidProof,
+                this.isPlasmaProof, this.color);
     }
 
+    @Override
     public int getLightOpacity() {
         return 1;
     }
 
-    public int getActualComparatorValue() {
-        FluidTank fluidTank = this.fluidTank;
-        int fluidAmount = fluidTank.getFluidAmount();
-        int maxCapacity = fluidTank.getCapacity();
-        float f = (float)fluidAmount / ((float)maxCapacity * 1.0F);
-        return MathHelper.floor(f * 14.0F) + (fluidAmount > 0 ? 1 : 0);
-    }
-
+    @Override
     public boolean isOpaqueCube() {
         return false;
     }
 
-    public String getHarvestTool() {
-        return "wrench";
-    }
-
+    @Override
     public boolean hasFrontFacing() {
-        return true;
+        return false;
     }
 
+    @Override
     protected void initializeInventory() {
         super.initializeInventory();
-        this.fluidTank = (new FilteredFluidHandler(this.tankSize)).setFillPredicate((stack) -> {
-            if (stack != null && stack.getFluid() != null) {
-                Fluid fluid = stack.getFluid();
-
-                //I want to use materials for these drums that do not have fluid pipe properties, so these cases are handled with bools directly
-                //Additionally, if something is a cryo drum its assumed that it is gas and cryo proof
-                if (fluid.getTemperature() > maxFluidTemp) return false;
-                else {
-                    if (fluid instanceof AttributedFluid) {
-                        if (fluid.getState() == FluidState.PLASMA && !this.isPlasmaProof) return false;
-                        
-                        for (FluidAttributes attribute : fluid.getAttributes()) {
-                            if (attribute == FluidAttributes.ACID) {
-                                if (!this.isAcidProof) return false;
-                                break;
-                            }
-                        }
-                    }
-
-                    return true;
-                }
-
-            } else {
-                return false;
-            }
-        });
-
+        this.fluidTank = new FilteredFluidHandler(this.tankSize)
+                .setFilter(new PropertyFluidFilter(maxFluidTemp, true, isAcidProof, true, isPlasmaProof));
         this.fluidInventory = this.fluidTank;
     }
 
+    @Override
     public void initFromItemStackData(NBTTagCompound itemStack) {
         super.initFromItemStackData(itemStack);
-        if (itemStack.hasKey("Fluid", 10)) {
-            FluidStack fluidStack = FluidStack.loadFluidStackFromNBT(itemStack.getCompoundTag("Fluid"));
-            this.fluidTank.setFluid(fluidStack);
+        if (itemStack.hasKey(FluidHandlerItemStack.FLUID_NBT_KEY, Constants.NBT.TAG_COMPOUND)) {
+            FluidStack fluidStack = FluidStack
+                    .loadFluidStackFromNBT(itemStack.getCompoundTag(FluidHandlerItemStack.FLUID_NBT_KEY));
+            fluidTank.setFluid(fluidStack);
         }
-
     }
 
+    @Override
     public void writeItemStackData(NBTTagCompound itemStack) {
         super.writeItemStackData(itemStack);
-        FluidStack fluidStack = this.fluidTank.getFluid();
+        FluidStack fluidStack = fluidTank.getFluid();
         if (fluidStack != null && fluidStack.amount > 0) {
             NBTTagCompound tagCompound = new NBTTagCompound();
             fluidStack.writeToNBT(tagCompound);
-            itemStack.setTag("Fluid", tagCompound);
+            itemStack.setTag(FluidHandlerItemStack.FLUID_NBT_KEY, tagCompound);
         }
-
     }
 
+    @Override
     public ICapabilityProvider initItemStackCapabilities(ItemStack itemStack) {
-        return new ThermalFluidHandlerItemStack(itemStack, this.tankSize, this.maxFluidTemp, true, this.isAcidProof, true, this.isPlasmaProof);
+        return new GTFluidHandlerItemStack(itemStack, tankSize).setFilter(this.fluidTank.getFilter());
     }
 
-    public void writeInitialSyncData(PacketBuffer buf) {
+    @Override
+    public void writeInitialSyncData(@NotNull PacketBuffer buf) {
         super.writeInitialSyncData(buf);
-        FluidStack fluidStack = this.fluidTank.getFluid();
+        FluidStack fluidStack = fluidTank.getFluid();
         buf.writeBoolean(fluidStack != null);
         if (fluidStack != null) {
             NBTTagCompound tagCompound = new NBTTagCompound();
             fluidStack.writeToNBT(tagCompound);
             buf.writeCompoundTag(tagCompound);
         }
-
-        buf.writeBoolean(this.isAutoOutput);
+        buf.writeBoolean(isAutoOutput);
     }
 
-    public void receiveInitialSyncData(PacketBuffer buf) {
+    @Override
+    public void receiveInitialSyncData(@NotNull PacketBuffer buf) {
         super.receiveInitialSyncData(buf);
         FluidStack fluidStack = null;
         if (buf.readBoolean()) {
             try {
                 NBTTagCompound tagCompound = buf.readCompoundTag();
                 fluidStack = FluidStack.loadFluidStackFromNBT(tagCompound);
-            } catch (IOException var4) {
+            } catch (IOException ignored) {
             }
         }
-
-        this.fluidTank.setFluid(fluidStack);
-        this.isAutoOutput = buf.readBoolean();
+        fluidTank.setFluid(fluidStack);
+        isAutoOutput = buf.readBoolean();
     }
 
-    public void receiveCustomData(int dataId, PacketBuffer buf) {
+    @Override
+    public void receiveCustomData(int dataId, @NotNull PacketBuffer buf) {
         super.receiveCustomData(dataId, buf);
-        if (dataId == 560) {
+        if (dataId == UPDATE_AUTO_OUTPUT) {
             this.isAutoOutput = buf.readBoolean();
-            this.scheduleRenderUpdate();
+            scheduleRenderUpdate();
         }
-
     }
 
+    @Override
     public void update() {
         super.update();
-        if (!this.getWorld().isRemote && this.isAutoOutput && this.getOffsetTimer() % 5L == 0L) {
-            this.pushFluidsIntoNearbyHandlers(new EnumFacing[]{EnumFacing.DOWN});
-        }
-
-    }
-
-    public boolean onRightClick(EntityPlayer playerIn, EnumHand hand, EnumFacing facing, CuboidRayTraceResult hitResult) {
-        if (!playerIn.getHeldItem(hand).hasCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, (EnumFacing)null)) {
-            return false;
-        } else {
-            return this.getWorld().isRemote || !playerIn.isSneaking() && FluidUtil.interactWithFluidHandler(playerIn, hand, this.fluidTank);
+        if (!getWorld().isRemote) {
+            if (isAutoOutput && getOffsetTimer() % 5 == 0) {
+                pushFluidsIntoNearbyHandlers(EnumFacing.DOWN);
+            }
         }
     }
 
-    public boolean onScrewdriverClick(EntityPlayer playerIn, EnumHand hand, EnumFacing wrenchSide, CuboidRayTraceResult hitResult) {
+    @Override
+    public boolean onRightClick(EntityPlayer playerIn, EnumHand hand, EnumFacing facing,
+                                CuboidRayTraceResult hitResult) {
+        if (playerIn.getHeldItem(hand).hasCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null)) {
+            return getWorld().isRemote ||
+                    (!playerIn.isSneaking() && FluidUtil.interactWithFluidHandler(playerIn, hand, fluidTank));
+        }
+        return super.onRightClick(playerIn, hand, facing, hitResult);
+    }
+
+    @Override
+    public boolean onScrewdriverClick(@NotNull EntityPlayer playerIn, EnumHand hand, EnumFacing wrenchSide,
+                                      CuboidRayTraceResult hitResult) {
         if (!playerIn.isSneaking()) {
-            if (this.getWorld().isRemote) {
-                this.scheduleRenderUpdate();
-                return true;
-            } else {
-                playerIn.sendMessage(new TextComponentTranslation("gregtech.machine.drum." + (this.isAutoOutput ? "disable" : "enable") + "_output", new Object[0]));
-                this.toggleOutput();
+            if (getWorld().isRemote) {
+                scheduleRenderUpdate();
                 return true;
             }
-        } else {
-            return super.onScrewdriverClick(playerIn, hand, wrenchSide, hitResult);
+            playerIn.sendStatusMessage(new TextComponentTranslation(
+                    "gregtech.machine.drum." + (isAutoOutput ? "disable" : "enable") + "_output"), true);
+            toggleOutput();
+            return true;
         }
+        return super.onScrewdriverClick(playerIn, hand, wrenchSide, hitResult);
     }
 
     private void toggleOutput() {
-        this.isAutoOutput = !this.isAutoOutput;
-        if (!this.getWorld().isRemote) {
-            this.notifyBlockUpdate();
-            this.writeCustomData(560, (buf) -> {
-                buf.writeBoolean(this.isAutoOutput);
-            });
-            this.markDirty();
+        isAutoOutput = !isAutoOutput;
+        if (!getWorld().isRemote) {
+            notifyBlockUpdate();
+            writeCustomData(UPDATE_AUTO_OUTPUT, buf -> buf.writeBoolean(isAutoOutput));
+            markDirty();
         }
-
     }
 
-    //needed to change this as well, but wasn't worth digging enough to have it not just use brass's color
-    @Override @SideOnly(Side.CLIENT)
+    @Override
+    @SideOnly(Side.CLIENT)
     public Pair<TextureAtlasSprite, Integer> getParticleTexture() {
-        int color = ColourRGBA.multiply(GTUtility.convertRGBtoOpaqueRGBA_CL(this.material.getMaterialRGB()), GTUtility.convertRGBtoOpaqueRGBA_CL(this.getPaintingColorForRendering()));
-        color = GTUtility.convertOpaqueRGBA_CLtoRGB(color);
-        return Pair.of(SusyTextures.BRASS_DRUM.getParticleTexture(), color);
+        int particleColor = ColourRGBA.multiply(
+                GTUtility.convertRGBtoOpaqueRGBA_CL(this.color),
+                GTUtility.convertRGBtoOpaqueRGBA_CL(getPaintingColorForRendering()));
+        particleColor = GTUtility.convertOpaqueRGBA_CLtoRGB(particleColor);
+        return Pair.of(Textures.DRUM.getParticleTexture(), particleColor);
     }
 
-    //I wanted a custom colored texture
     @Override
     public void renderMetaTileEntity(CCRenderState renderState, Matrix4 translation, IVertexOperation[] pipeline) {
-        SusyTextures.BRASS_DRUM.render(renderState, translation, pipeline, this.getFrontFacing());
-        SusyTextures.BRASS_DRUM_OVERLAY.renderOrientedState(renderState, translation, pipeline, this.getFrontFacing(), false, false);
+        ColourMultiplier multiplier = new ColourMultiplier(
+                ColourRGBA.multiply(GTUtility.convertRGBtoOpaqueRGBA_CL(this.color),
+                        GTUtility.convertRGBtoOpaqueRGBA_CL(getPaintingColorForRendering())));
+        Textures.DRUM.render(renderState, translation, ArrayUtils.add(pipeline, multiplier), getFrontFacing());
+        Textures.DRUM_OVERLAY.render(renderState, translation, pipeline);
 
-        if (this.isAutoOutput) {
+        if (isAutoOutput) {
             Textures.STEAM_VENT_OVERLAY.renderSided(EnumFacing.DOWN, renderState, translation, pipeline);
         }
-
     }
 
+    @Override
     public int getDefaultPaintingColor() {
-        return 16777215;
+        return 0xFFFFFF;
     }
 
+    @Override
     @SideOnly(Side.CLIENT)
     public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, boolean advanced) {
-        tooltip.add(I18n.format("gregtech.universal.tooltip.fluid_storage_capacity", this.tankSize));
+        tooltip.add(I18n.format("gregtech.universal.tooltip.fluid_storage_capacity", tankSize));
+
+        // FluidPipeProperties#appendTooltips
         if (TooltipHelper.isShiftDown()) {
-
-            //always gas and cryo proof, so no condition needs to be checked
-            tooltip.add(I18n.format("gregtech.fluid_pipe.max_temperature", this.maxFluidTemp));
+            tooltip.add(I18n.format("gregtech.fluid_pipe.max_temperature", maxFluidTemp));
             tooltip.add(I18n.format("gregtech.fluid_pipe.gas_proof"));
-
-            if (this.isAcidProof)
-                tooltip.add(I18n.format("gregtech.fluid_pipe.acid_proof"));
-
+            if (isPlasmaProof) tooltip.add(I18n.format("gregtech.fluid_pipe.plasma_proof"));
             tooltip.add(I18n.format("gregtech.fluid_pipe.cryo_proof"));
-
-            if (this.isPlasmaProof)
-                tooltip.add(I18n.format("gregtech.fluid_pipe.plasma_proof"));
-
-            tooltip.add(I18n.format("gregtech.tool_action.screwdriver.access_covers"));
-            tooltip.add(I18n.format("gregtech.tool_action.screwdriver.auto_output_down"));
-            tooltip.add(I18n.format("gregtech.tool_action.crowbar"));
-        } else {
+            if (fluidTank.getFilter() instanceof IPropertyFluidFilter filter) {
+                filter.getContainedAttributes().forEach(a -> a.appendContainerTooltips(tooltip));
+            }
+        } else if (fluidTank.getFilter() instanceof IPropertyFluidFilter filter && !filter.getContainedAttributes().isEmpty()) {
             tooltip.add(I18n.format("gregtech.tooltip.tool_fluid_hold_shift"));
         }
 
-        NBTTagCompound tagCompound = stack.getTagCompound();
-        if (tagCompound != null && tagCompound.hasKey("Fluid", 10)) {
-            FluidStack fluidStack = FluidStack.loadFluidStackFromNBT(tagCompound.getCompoundTag("Fluid"));
-            if (fluidStack == null) {
-                return;
-            }
-
-            tooltip.add(I18n.format("gregtech.machine.fluid_tank.fluid", fluidStack.amount, I18n.format(fluidStack.getUnlocalizedName())));
+        if (TooltipHelper.isShiftDown()) {
+            tooltip.add(I18n.format("gregtech.tool_action.screwdriver.access_covers"));
+            tooltip.add(I18n.format("gregtech.tool_action.screwdriver.auto_output_down"));
+            tooltip.add(I18n.format("gregtech.tool_action.crowbar"));
         }
 
+        NBTTagCompound tagCompound = stack.getTagCompound();
+        if (tagCompound != null && tagCompound.hasKey("Fluid", Constants.NBT.TAG_COMPOUND)) {
+            FluidStack fluidStack = FluidStack.loadFluidStackFromNBT(tagCompound.getCompoundTag("Fluid"));
+            if (fluidStack == null) return;
+            tooltip.add(I18n.format("gregtech.machine.fluid_tank.fluid", fluidStack.amount,
+                    I18n.format(fluidStack.getUnlocalizedName())));
+        }
     }
 
+    // Override this so that we can control the "Hold SHIFT" tooltip manually
+    @Override
     public boolean showToolUsages() {
         return false;
     }
 
+    @Override
     protected ModularUI createUI(EntityPlayer entityPlayer) {
         return null;
     }
 
+    @Override
     public NBTTagCompound writeToNBT(NBTTagCompound data) {
         super.writeToNBT(data);
-        data.setTag("FluidInventory", ((FluidTank)this.fluidInventory).writeToNBT(new NBTTagCompound()));
-        data.setBoolean("AutoOutput", this.isAutoOutput);
+        data.setTag("FluidInventory", ((FluidTank) fluidInventory).writeToNBT(new NBTTagCompound()));
+        data.setBoolean("AutoOutput", isAutoOutput);
         return data;
     }
 
+    @Override
     public void readFromNBT(NBTTagCompound data) {
         super.readFromNBT(data);
-        ((FluidTank)this.fluidInventory).readFromNBT(data.getCompoundTag("FluidInventory"));
-        this.isAutoOutput = data.getBoolean("AutoOutput");
+        ((FluidTank) this.fluidInventory).readFromNBT(data.getCompoundTag("FluidInventory"));
+        isAutoOutput = data.getBoolean("AutoOutput");
     }
 
+    @Override
     protected boolean shouldSerializeInventories() {
         return false;
     }
