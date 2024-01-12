df4648beb84815c18ee0b140da51e8846d329f86
TechLord22 <37029404+techlord22@users.noreply.github.com>
2024-01-11 19:18:13 -0500

    fix fluid registry name operators

--- src/main/java/supersymmetry/api/fluids/SusyFluidStorageKeys.java
+++ src/main/java/supersymmetry/api/fluids/SusyFluidStorageKeys.java
@@ -4,24 +4,21 @@ import gregtech.api.fluids.FluidState;
 import gregtech.api.fluids.store.FluidStorageKey;
 import supersymmetry.api.unification.material.info.SuSyMaterialIconType;
 
-import java.util.function.UnaryOperator;
-
 import static supersymmetry.api.util.SuSyUtility.susyId;
 
 public final class SusyFluidStorageKeys {
 
     public static final FluidStorageKey SLURRY = new FluidStorageKey(susyId("slurry"),
             SuSyMaterialIconType.slurry,
-            UnaryOperator.identity(),
+            s -> s + "_slurry",
             m -> "susy.fluid.slurry",
             FluidState.LIQUID);
 
     public static final FluidStorageKey IMPURE_SLURRY = new FluidStorageKey(susyId("impure_slurry"),
             SuSyMaterialIconType.slurry,
-            UnaryOperator.identity(),
+            s -> "impure_" + s + "_slurry",
             m -> "susy.fluid.impure_slurry",
             FluidState.LIQUID);
 
     private SusyFluidStorageKeys() {}
-    
 }
