a4cf63f54c53cc3f48a62fa62e1fd7dc2d490bd8
TheScienceDemon <louisruppert0@gmail.com>
2024-02-24 19:02:43 +0100

    Implement IGhostSlotConfigurable in Coagulation Tank

--- src/main/java/supersymmetry/common/metatileentities/multi/primitive/MetaTileEntityCoagulationTank.java
+++ src/main/java/supersymmetry/common/metatileentities/multi/primitive/MetaTileEntityCoagulationTank.java
@@ -3,13 +3,12 @@ package supersymmetry.common.metatileentities.multi.primitive;
 import codechicken.lib.render.CCRenderState;
 import codechicken.lib.render.pipeline.IVertexOperation;
 import codechicken.lib.vec.Matrix4;
+import gregtech.api.capability.IGhostSlotConfigurable;
+import gregtech.api.capability.impl.GhostCircuitItemStackHandler;
 import gregtech.api.capability.impl.ItemHandlerList;
 import gregtech.api.gui.GuiTextures;
 import gregtech.api.gui.ModularUI;
-import gregtech.api.gui.widgets.ProgressWidget;
-import gregtech.api.gui.widgets.RecipeProgressWidget;
-import gregtech.api.gui.widgets.SlotWidget;
-import gregtech.api.gui.widgets.TankWidget;
+import gregtech.api.gui.widgets.*;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.IMultiblockPart;
@@ -18,26 +17,42 @@ import gregtech.api.metatileentity.multiblock.RecipeMapPrimitiveMultiblockContro
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
 import gregtech.api.pattern.PatternMatchContext;
+import gregtech.api.recipes.ingredients.IntCircuitIngredient;
 import gregtech.api.util.GTTransferUtils;
 import gregtech.client.renderer.ICubeRenderer;
 import gregtech.client.renderer.texture.Textures;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.IFluidTank;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.ItemStackHandler;
+import supersymmetry.api.gui.SusyGuiTextures;
 import supersymmetry.api.recipes.SuSyRecipeMaps;
 import supersymmetry.client.renderer.textures.SusyTextures;
 import supersymmetry.common.blocks.BlockCoagulationTankWall;
 import supersymmetry.common.blocks.SuSyBlocks;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Arrays;
 
-public class MetaTileEntityCoagulationTank extends RecipeMapPrimitiveMultiblockController {
+public class MetaTileEntityCoagulationTank extends RecipeMapPrimitiveMultiblockController implements IGhostSlotConfigurable {
+
+    @Nullable
+    protected GhostCircuitItemStackHandler circuitInventory;
+    private IItemHandlerModifiable actualImportItems;
+
     public MetaTileEntityCoagulationTank(ResourceLocation metaTileEntityId) {
         super(metaTileEntityId, SuSyRecipeMaps.COAGULATION_RECIPES);
+        circuitInventory = new GhostCircuitItemStackHandler(this);
+        circuitInventory.addNotifiableMetaTileEntity(this);
     }
 
     public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
@@ -84,6 +99,11 @@ public class MetaTileEntityCoagulationTank extends RecipeMapPrimitiveMultiblockC
         builder.widget((new SlotWidget(this.exportItems, 0, 106, 39, true, false)
                 .setBackgroundTexture(GuiTextures.PRIMITIVE_SLOT)));
 
+        SlotWidget circuitSlot = new GhostCircuitSlotWidget(circuitInventory, 0, 124, 62)
+                .setBackgroundTexture(GuiTextures.PRIMITIVE_SLOT, SusyGuiTextures.INT_CIRCUIT_OVERLAY_STEAM.get(true));
+        builder.widget(getCircuitSlotTooltip(circuitSlot))
+                .widget(new ClickButtonWidget(115, 62, 9, 9, "", click -> circuitInventory.addCircuitValue(click.isShiftClick ? 5 : 1)).setShouldClientCallback(true).setButtonTexture(GuiTextures.BUTTON_INT_CIRCUIT_PLUS).setDisplayFunction(() -> circuitInventory.hasCircuitValue() && circuitInventory.getCircuitValue() < IntCircuitIngredient.CIRCUIT_MAX))
+                .widget(new ClickButtonWidget(115, 71, 9, 9, "", click -> circuitInventory.addCircuitValue(click.isShiftClick ? -5 : -1)).setShouldClientCallback(true).setButtonTexture(GuiTextures.BUTTON_INT_CIRCUIT_MINUS).setDisplayFunction(() -> circuitInventory.hasCircuitValue() && circuitInventory.getCircuitValue() > IntCircuitIngredient.CIRCUIT_MIN));
 
         return builder.bindPlayerInventory(entityPlayer.inventory, GuiTextures.PRIMITIVE_SLOT, 0);
     }
@@ -136,5 +156,70 @@ public class MetaTileEntityCoagulationTank extends RecipeMapPrimitiveMultiblockC
 
     public boolean hasMaintenanceMechanics() {
         return false;
+    }
+
+    @Override
+    public boolean hasGhostCircuitInventory() {
+        return true;
+    }
+
+    @Override
+    public void setGhostCircuitConfig(int config) {
+        if (this.circuitInventory == null || this.circuitInventory.getCircuitValue() == config) {
+            return;
+        }
+        this.circuitInventory.setCircuitValue(config);
+        if (!getWorld().isRemote) {
+            markDirty();
+        }
+    }
+
+    protected SlotWidget getCircuitSlotTooltip(SlotWidget widget) {
+        String configString;
+
+        if (circuitInventory == null || circuitInventory.getCircuitValue() == GhostCircuitItemStackHandler.NO_CONFIG) {
+            configString = new TextComponentTranslation("gregtech.gui.configurator_slot.no_value").getFormattedText();
+        } else {
+            configString = String.valueOf(circuitInventory.getCircuitValue());
+        }
+
+        return widget.setTooltipText("gregtech.gui.configurator_slot.tooltip", configString);
+    }
+
+    @Override
+    protected void initializeInventory() {
+        super.initializeInventory();
+        actualImportItems = null;
+    }
+
+    @Override
+    public IItemHandlerModifiable getImportItems() {
+        if (actualImportItems == null) actualImportItems = circuitInventory == null ? super.getImportItems() : new ItemHandlerList(Arrays.asList(super.getImportItems(), circuitInventory));
+        return actualImportItems;
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound data) {
+        super.writeToNBT(data);
+        if (circuitInventory != null) circuitInventory.write(data);
+        return data;
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound data) {
+        super.readFromNBT(data);
+        if (circuitInventory != null) {
+            if (data.hasKey("CircuitInventory", Constants.NBT.TAG_COMPOUND)) {
+                ItemStackHandler legacyCircuitInventory = new ItemStackHandler();
+                for (int i =0; i < legacyCircuitInventory.getSlots(); i++) {
+                    ItemStack stack = legacyCircuitInventory.getStackInSlot(i);
+                    if (stack.isEmpty()) continue;
+                    stack = GTTransferUtils.insertItem(importItems, stack, false);
+                    circuitInventory.setCircuitValueFromStack(stack);
+                }
+            } else {
+                circuitInventory.read(data);
+            }
+        }
     }
 }
