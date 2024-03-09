de5a6cc17f47246bda6120f866e990a7288c24da
Loxo <loxo.Minecraft@gmail.com>
2024-03-09 17:27:40 +0100

    Make the bore do slopes upwards

--- src/main/java/supersymmetry/common/entities/EntityTunnelBore.java
+++ src/main/java/supersymmetry/common/entities/EntityTunnelBore.java
@@ -12,7 +12,6 @@ import cam72cam.immersiverailroading.library.TrackSmoothing;
 import cam72cam.immersiverailroading.physics.MovementTrack;
 import cam72cam.immersiverailroading.thirdparty.trackapi.ITrack;
 import cam72cam.immersiverailroading.track.BuilderBase;
-import cam72cam.immersiverailroading.track.TrackBase;
 import cam72cam.immersiverailroading.util.*;
 import cam72cam.mod.fluid.Fluid;
 import cam72cam.mod.item.ItemStack;
@@ -31,6 +30,8 @@ public class EntityTunnelBore extends Locomotive {
     private float borerAngle = 0;
 
     private int distanceToGo = 150;
+
+    private final int trackLength = 10;
 
     private ItemStack railBedFill = new ItemStack(new net.minecraft.item.ItemStack(Blocks.CONCRETE));
     private ArrayList<TunnelBoreControl> controlSequence = new ArrayList<>();
@@ -74,34 +75,7 @@ public class EntityTunnelBore extends Locomotive {
                 ITrack trackFront = MovementTrack.findTrack(nextState.config.world, positionFront, nextState.yawFront, nextState.config.gauge.value());
                 // We have reached the end of the track
                 if(trackFront == null) {
-                    if(this.distanceToGo > 0) {
-                        RailSettings settings = new RailSettings(
-                                this.gauge,
-                                "immersiverailroading:track/bmtrack.json",
-                                TrackItems.STRAIGHT,
-                                this.distanceToGo >= 10 ? 10 : this.distanceToGo,
-                                90,
-                                1,
-                                TrackPositionType.FIXED,
-                                TrackSmoothing.BOTH,
-                                TrackDirection.NONE,
-                                ItemStack.EMPTY,
-                                this.railBedFill,
-                                false,
-                                false
-                        );
-                        ItemStack trackBlueprintStack = new ItemStack(IRItems.ITEM_TRACK_BLUEPRINT, 0);
-                        settings.write(trackBlueprintStack);
-                        PlacementInfo placementInfo = new PlacementInfo(trackBlueprintStack, this.getRotationYaw(), new Vec3d(0.5, 0.5, 0.5));
-                        RailInfo railInfo = new RailInfo(trackBlueprintStack, placementInfo, null);
-                        World irWorld = getWorld();
-                        BuilderBase trackBuilder = railInfo.getBuilder(irWorld, new Vec3i(getPosition()));
-                        List<TrackBase> tracks = trackBuilder.getTracksForRender();
-                        trackBuilder.build();
-                        Simulation.forceQuickUpdates = true;
-                        this.states = new ArrayList<>();
-                        this.distanceToGo = this.distanceToGo >= 10 ? this.distanceToGo - 10 : 0;
-                    }
+                    this.placeTrack();
                 }
             }
         }
@@ -128,6 +102,66 @@ public class EntityTunnelBore extends Locomotive {
 
     public float getBorerAngle() {
         return borerAngle;
+    }
+
+    public void placeTrack() {
+        if(this.distanceToGo > 0) {
+            int placeableLength = this.distanceToGo > this.trackLength ? this.trackLength : this.distanceToGo;
+
+            RailSettings settings = null;
+
+            if(this.getRotationPitch() <= 0) settings = getSettingsStraight(placeableLength);
+            else settings = getSettingsSlope(placeableLength);
+
+            ItemStack trackBlueprintStack = new ItemStack(IRItems.ITEM_TRACK_BLUEPRINT, 0);
+            settings.write(trackBlueprintStack);
+            PlacementInfo placementInfo = new PlacementInfo(trackBlueprintStack, this.getRotationYaw(), new Vec3d(0.5, 0.5, 0.5));
+            RailInfo railInfo = new RailInfo(trackBlueprintStack, placementInfo, null);
+            World irWorld = getWorld();
+            BuilderBase trackBuilder = railInfo.getBuilder(irWorld, new Vec3i(getPosition()));
+            trackBuilder.build();
+
+            Simulation.forceQuickUpdates = true;
+            this.states = new ArrayList<>();
+            this.distanceToGo -= placeableLength;
+        }
+
+    }
+
+    public RailSettings getSettingsStraight (int length) {
+        return new RailSettings(
+                this.gauge,
+                "immersiverailroading:track/bmtrack.json",
+                TrackItems.STRAIGHT,
+                length,
+                90,
+                1,
+                TrackPositionType.FIXED,
+                TrackSmoothing.BOTH,
+                TrackDirection.NONE,
+                ItemStack.EMPTY,
+                this.railBedFill,
+                false,
+                false
+        );
+    }
+
+    public RailSettings getSettingsSlope (int length) {
+        return new RailSettings(
+                this.gauge,
+                "immersiverailroading:track/bmtrack.json",
+                TrackItems.SLOPE,
+                length,
+                90,
+                1,
+                TrackPositionType.FIXED,
+                TrackSmoothing.BOTH,
+                TrackDirection.NONE,
+                ItemStack.EMPTY,
+                this.railBedFill,
+                false,
+                false
+        );
     }
 }
 
