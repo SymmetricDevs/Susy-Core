a6dfee8321cb6c1ad34127f70a26a327017ec90d
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2024-03-12 00:02:20 +0800

    add top support for coagulation tank

--- src/main/java/supersymmetry/integration/theoneprobe/TheOneProbeModule.java
+++ src/main/java/supersymmetry/integration/theoneprobe/TheOneProbeModule.java
@@ -3,6 +3,7 @@ package supersymmetry.integration.theoneprobe;
 import mcjty.theoneprobe.TheOneProbe;
 import mcjty.theoneprobe.api.ITheOneProbe;
 import org.apache.logging.log4j.LogManager;
+import supersymmetry.integration.theoneprobe.provider.CoagulationTankParallelProvider;
 import supersymmetry.integration.theoneprobe.provider.VoidingMultiblockInfoProvider;
 
 public class TheOneProbeModule {
@@ -11,5 +12,6 @@ public class TheOneProbeModule {
         LogManager.getLogger("Supersymmetry TOP Integration").info("TheOneProbe found. Enabling integration...");
         ITheOneProbe oneProbe = TheOneProbe.theOneProbeImp;
         oneProbe.registerProvider(new VoidingMultiblockInfoProvider());
+        oneProbe.registerProvider(new CoagulationTankParallelProvider());
     }
 }
---
+++ src/main/java/supersymmetry/integration/theoneprobe/provider/CoagulationTankParallelProvider.java
@@ -0,0 +1,40 @@
+package supersymmetry.integration.theoneprobe.provider;
+
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.api.util.TextComponentUtil;
+import mcjty.theoneprobe.api.IProbeHitData;
+import mcjty.theoneprobe.api.IProbeInfo;
+import mcjty.theoneprobe.api.IProbeInfoProvider;
+import mcjty.theoneprobe.api.ProbeMode;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
+import supersymmetry.Supersymmetry;
+import supersymmetry.common.metatileentities.multi.VoidingMultiblockBase;
+import supersymmetry.common.metatileentities.multi.primitive.MetaTileEntityCoagulationTank;
+
+public class CoagulationTankParallelProvider implements IProbeInfoProvider {
+    @Override
+    public String getID() {
+        return Supersymmetry.MODID + ":coagulation_tank_parallel_provider";
+    }
+
+    public void addProbeInfo(@NotNull ProbeMode mode, @NotNull IProbeInfo probeInfo, @NotNull EntityPlayer player,
+                             @NotNull World world, @NotNull IBlockState blockState, @NotNull IProbeHitData data) {
+        if (blockState.getBlock().hasTileEntity(blockState)) {
+            TileEntity tileEntity = world.getTileEntity(data.getPos());
+            if (!(tileEntity instanceof IGregTechTileEntity)) return;
+
+            MetaTileEntity metaTileEntity = ((IGregTechTileEntity) tileEntity).getMetaTileEntity();
+            if (metaTileEntity instanceof MetaTileEntityCoagulationTank && metaTileEntity.isActive()) {
+                probeInfo.text(TextComponentUtil.translationWithColor(TextFormatting.AQUA,
+                        "susy.top.coagulation_tank_parallel",
+                        ((MetaTileEntityCoagulationTank) metaTileEntity).size).getFormattedText());
+            }
+        }
+    }
+}
