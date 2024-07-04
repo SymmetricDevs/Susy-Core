6bc8d70499fec31df392ccfa7495c5b288ffab80
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2024-07-04 14:29:53 +0800

    more cleanup for turbine fix

--- src/main/java/supersymmetry/api/blocks/VariantHorizontalRotatableBlock.java
+++ src/main/java/supersymmetry/api/blocks/VariantHorizontalRotatableBlock.java
@@ -1,6 +1,7 @@
 package supersymmetry.api.blocks;
 
 import gregtech.api.block.VariantBlock;
+import gregtech.common.items.tool.rotation.CustomBlockRotations;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.properties.PropertyEnum;
@@ -18,18 +19,25 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
 
-public class VariantRotatableBlock<T extends Enum<T> & IStringSerializable> extends VariantBlock<T> {
+import static gregtech.common.items.tool.rotation.CustomBlockRotations.BLOCK_HORIZONTAL_BEHAVIOR;
+
+public class VariantHorizontalRotatableBlock<T extends Enum<T> & IStringSerializable> extends VariantBlock<T> {
     public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
 
-    public VariantRotatableBlock(Material materialIn) {
+    public VariantHorizontalRotatableBlock(Material materialIn) {
         super(materialIn);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(this.VARIANT, this.VALUES[0]).withProperty(FACING, EnumFacing.NORTH));
+        this.setDefaultState(blockState.getBaseState().withProperty(VARIANT, VALUES[0]).withProperty(FACING, EnumFacing.NORTH));
+        CustomBlockRotations.registerCustomRotation(this, BLOCK_HORIZONTAL_BEHAVIOR);
     }
 
-    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+    @Nonnull
+    @Override
+    @SuppressWarnings("deprecation")
+    public IBlockState getStateForPlacement(@NotNull World worldIn, @NotNull BlockPos pos, @NotNull EnumFacing facing, float hitX, float hitY, float hitZ, int meta, @NotNull EntityLivingBase placer) {
         return super.getStateForPlacement(worldIn, pos, facing, hitX, hitY, hitZ, meta, placer).withProperty(FACING, placer.getHorizontalFacing().getOpposite());
     }
 
+    @Override
     public ItemStack getItemVariant(T variant, int amount) {
         return new ItemStack(this, amount, variant.ordinal() * 4);
     }
@@ -37,35 +45,37 @@ public class VariantRotatableBlock<T extends Enum<T> & IStringSerializable> exte
     @Nonnull
     @Override
     public BlockStateContainer createBlockState() {
-        Class<T> enumClass = getActualTypeParameter(this.getClass(), VariantRotatableBlock.class);
+        Class<T> enumClass = getActualTypeParameter(getClass(), VariantHorizontalRotatableBlock.class);
         this.VARIANT = PropertyEnum.create("variant", enumClass);
         this.VALUES = enumClass.getEnumConstants();
-        return new BlockStateContainer(this, this.VARIANT, this.FACING);
+        return new BlockStateContainer(this, VARIANT, FACING);
     }
 
     @Override
     public int damageDropped(@NotNull IBlockState state) {
-        return state.getValue(this.VARIANT).ordinal() * 4;
+        return state.getValue(VARIANT).ordinal() * 4;
     }
 
+    @Nonnull
     @Override
     public IBlockState getStateFromMeta(int meta) {
         int i = meta / 4;
         int j = meta % 4;
 
         EnumFacing enumfacing = EnumFacing.byHorizontalIndex(j);
-        return this.getDefaultState()
+        return getDefaultState()
                 .withProperty(FACING, enumfacing)
-                .withProperty(this.VARIANT, this.VALUES[i % this.VALUES.length]);
+                .withProperty(VARIANT, VALUES[i % VALUES.length]);
     }
 
     @Override
     public int getMetaFromState(IBlockState state) {
-        return state.getValue(this.VARIANT).ordinal() * 4 + state.getValue(this.FACING).getHorizontalIndex();
+        return state.getValue(VARIANT).ordinal() * 4 + state.getValue(FACING).getHorizontalIndex();
     }
 
+    @Nonnull
     @Override
-    public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player) {
-        return this.getItemVariant(state.getValue(this.VARIANT), 1);
+    public ItemStack getPickBlock(IBlockState state, @NotNull RayTraceResult target, @NotNull World world, @NotNull BlockPos pos, @NotNull EntityPlayer player) {
+        return getItemVariant(state.getValue(VARIANT), 1);
     }
 }
--- src/main/java/supersymmetry/common/blocks/BlockAlternatorCoil.java
+++ src/main/java/supersymmetry/common/blocks/BlockAlternatorCoil.java
@@ -3,12 +3,17 @@ package supersymmetry.common.blocks;
 import gregtech.api.block.IStateHarvestLevel;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.IStringSerializable;
-import supersymmetry.api.blocks.VariantRotatableBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import org.jetbrains.annotations.NotNull;
+import supersymmetry.api.blocks.VariantHorizontalRotatableBlock;
 
 import javax.annotation.Nonnull;
 
-public class BlockAlternatorCoil extends VariantRotatableBlock<BlockAlternatorCoil.AlternatorCoilType> {
+public class BlockAlternatorCoil extends VariantHorizontalRotatableBlock<BlockAlternatorCoil.AlternatorCoilType> {
+
     public BlockAlternatorCoil() {
         super(net.minecraft.block.material.Material.IRON);
         setTranslationKey("alternator_coil");
@@ -16,6 +21,12 @@ public class BlockAlternatorCoil extends VariantRotatableBlock<BlockAlternatorCo
         setResistance(10.0f);
         setSoundType(SoundType.METAL);
         setHarvestLevel("wrench", 2);
+    }
+
+    @Override
+    public boolean canCreatureSpawn(@NotNull IBlockState state, @NotNull IBlockAccess world, @NotNull BlockPos pos,
+                                    @NotNull EntityLiving.SpawnPlacementType type) {
+        return false;
     }
 
     public enum AlternatorCoilType implements IStringSerializable, IStateHarvestLevel {
@@ -30,14 +41,17 @@ public class BlockAlternatorCoil extends VariantRotatableBlock<BlockAlternatorCo
         }
 
         @Nonnull
+        @Override
         public String getName() {
             return this.name;
         }
 
+        @Override
         public int getHarvestLevel(IBlockState state) {
             return this.harvestLevel;
         }
 
+        @Override
         public String getHarvestTool(IBlockState state) {
             return "wrench";
         }
