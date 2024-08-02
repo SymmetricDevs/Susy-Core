ac3ed1f2198a66049ec85d1df8250fd66d082b47
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2024-08-02 11:41:47 +0800

    fix bds&m

--- src/main/java/supersymmetry/Supersymmetry.java
+++ src/main/java/supersymmetry/Supersymmetry.java
@@ -1,7 +1,6 @@
 package supersymmetry;
 
 import gregtech.GTInternalTags;
-import net.minecraft.entity.monster.EntityZombie;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLConstructionEvent;
@@ -9,7 +8,6 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import org.jetbrains.annotations.NotNull;
-import supersymmetry.api.event.MobHordeEvent;
 import supersymmetry.api.sound.SusySounds;
 import supersymmetry.common.CommonProxy;
 import supersymmetry.common.SusyMetaEntities;
@@ -22,6 +20,7 @@ import supersymmetry.common.command.CommandHordeStop;
 import supersymmetry.common.covers.SuSyCoverBehaviors;
 import supersymmetry.common.item.SuSyMetaItems;
 import supersymmetry.common.metatileentities.SuSyMetaTileEntities;
+import supersymmetry.integration.bdsandm.BDSAndMModule;
 import supersymmetry.loaders.SuSyIRLoader;
 
 @Mod(name = Supersymmetry.NAME, modid = Supersymmetry.MODID, version = Tags.VERSION, dependencies = GTInternalTags.DEP_VERSION_STRING + ";required-after:gcym;after:immersiverailroading")
@@ -63,6 +62,8 @@ public class Supersymmetry {
     public void onInit(@NotNull FMLInitializationEvent event) {
         proxy.load();
         SuSyCoverBehaviors.init();
+
+        BDSAndMModule.init();
     }
 
     @Mod.EventHandler
