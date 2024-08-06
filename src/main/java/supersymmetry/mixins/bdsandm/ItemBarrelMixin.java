feca049be49274ee802358ed636a401869c11272
The-Minecraft-Scientist <The-Minecraft-Scientist@users.noreply.github.com>
2024-08-05 21:23:24 -0700

    update to use @Inject instead of overriding the whole method

--- src/main/java/supersymmetry/mixins/bdsandm/ItemBarrelMixin.java
+++ src/main/java/supersymmetry/mixins/bdsandm/ItemBarrelMixin.java
@@ -1,6 +1,6 @@
 package supersymmetry.mixins.bdsandm;
 
-import funwayguy.bdsandm.inventory.capability.BdsmCapabilies;
+import com.llamalad7.mixinextras.sugar.Local;
 import funwayguy.bdsandm.inventory.capability.CapabilityBarrel;
 import funwayguy.bdsandm.items.ItemBarrel;
 import net.minecraft.block.Block;
@@ -8,6 +8,9 @@ import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemBarrel.class)
 public class ItemBarrelMixin extends ItemBlock {
@@ -17,20 +20,22 @@ public class ItemBarrelMixin extends ItemBlock {
     }
 
     /**
-     * @author The-Minecraft-Scientist (discord rsci.)
+     * @author The-Minecraft-Scientist (discord rsci.), MCTian-mi
      */
-    @Override
     // Hack that keeps a difficult to reproduce bug somewhere else in BDS&M from kicking players.
     // This hack omits adding the CapabilityBarrel NBT tag if CapabilityBarrel is not present on the given item stack.
     // The original BDS&M code had an assertion that assumed barrelCap was non-null, which would
     // fire from the network handler for a given player and kick them.
     // This hack reduces the scope of the bug to incorrect (not present) barrel metadata on the client.
-    public NBTTagCompound getNBTShareTag(ItemStack stack) {
-        CapabilityBarrel barrelCap = (CapabilityBarrel) stack.getCapability(BdsmCapabilies.BARREL_CAP, null);
-        if (barrelCap == null) {
-            return super.getNBTShareTag(stack);
+    @Inject(method = "getNBTShareTag",
+            remap = false,
+            at = @At(value = "INVOKE",
+                    target = "Lfunwayguy/bdsandm/inventory/capability/CapabilityBarrel;serializeNBT()Lnet/minecraft/nbt/NBTTagCompound;"),
+            cancellable = true)
+    public void returnIfNull(ItemStack stack, CallbackInfoReturnable<NBTTagCompound> cir, @Local CapabilityBarrel barrel) {
+        if (barrel == null) {
+            cir.setReturnValue(super.getNBTShareTag(stack));
+            cir.cancel();
         }
-        stack.setTagInfo("barrelCap", barrelCap.serializeNBT());
-        return super.getNBTShareTag(stack);
     }
 }
