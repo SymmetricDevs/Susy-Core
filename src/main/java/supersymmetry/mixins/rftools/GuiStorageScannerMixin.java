5989bc737b99399f14703a2548de09880d2eadbf
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2024-08-13 22:11:09 +0800

    bogosort Storage Scanners

--- src/main/java/supersymmetry/mixins/rftools/GuiStorageScannerMixin.java
+++ src/main/java/supersymmetry/mixins/rftools/GuiStorageScannerMixin.java
@@ -1,13 +1,18 @@
 package supersymmetry.mixins.rftools;
 
+import com.cleanroommc.bogosorter.common.sort.SortHandler;
 import com.llamalad7.mixinextras.sugar.Local;
 import mcjty.lib.gui.widgets.BlockRender;
 import mcjty.rftools.blocks.storagemonitor.GuiStorageScanner;
 import mcjty.rftools.blocks.storagemonitor.PacketReturnInventoryInfo;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import supersymmetry.api.bugfixes.IItemStackInfo;
+
+import java.util.Comparator;
+import java.util.function.Function;
 
 @Mixin(GuiStorageScanner.class)
 public class GuiStorageScannerMixin {
@@ -19,5 +24,14 @@ public class GuiStorageScannerMixin {
                     ordinal = 0))
     private BlockRender setRenderItemCorrectly(BlockRender renderer, Object object, @Local(argsOnly = true) PacketReturnInventoryInfo.InventoryInfo info) {
         return renderer.setRenderItem(((IItemStackInfo) info).getStack());
+    }
+
+    @Redirect(method = "updateContentsList",
+            remap = false,
+            at = @At(value = "INVOKE",
+                    target = "Ljava/util/Comparator;comparing(Ljava/util/function/Function;)Ljava/util/Comparator;",
+                    ordinal = 0))
+    public Comparator<ItemStack> redirectComparator(Function<ItemStack, String> no_one_likes_alphabetical_order) {
+        return SortHandler.getClientItemComparator();
     }
 }
