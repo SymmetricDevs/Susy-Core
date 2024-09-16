41625d7e9bb5bea05f9a8aa3a83ccb4e0170e369
trainvoi <voitrainelephant@gmail.com>
2024-09-16 21:16:07 +0700

    Reapply "fix tooltip"
    This reverts commit 46d21a6b0e8fd59b1e180071fe0323db07a48374.

--- src/main/java/supersymmetry/mixins/gregtech/MetaTileEntityLargeMinerMixin.java
+++ src/main/java/supersymmetry/mixins/gregtech/MetaTileEntityLargeMinerMixin.java
@@ -7,6 +7,9 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -17,13 +20,14 @@ public abstract class MetaTileEntityLargeMinerMixin extends MultiblockWithDispla
     public MetaTileEntityLargeMinerMixin(ResourceLocation metaTileEntityId) {
         super(metaTileEntityId);
     }
-    public void addInformation(ItemStack stack, @Nullable World player, @NotNull List<String> tooltip, boolean advanced) {
+
+    @Inject(method = "addInformation", at = @At("TAIL"))
+    public void addInformation(ItemStack stack, @Nullable World player, @NotNull List<String> tooltip, boolean advanced, CallbackInfo ci) {
         super.addInformation(stack, player, tooltip, advanced);
     }
+
     @Override
     public boolean getIsWeatherOrTerrainResistant() {
         return true;
     }
-
-
 }
