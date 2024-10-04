bb11ef260ef24f632c2a337b73a1ca879d0bb608
bruberu <80226372+bruberu@users.noreply.github.com>
2024-10-03 21:08:12 -0500

    feat: at least have a prototype

--- src/main/java/supersymmetry/common/metatileentities/SuSyMetaTileEntities.java
+++ src/main/java/supersymmetry/common/metatileentities/SuSyMetaTileEntities.java
@@ -153,6 +153,8 @@ public class SuSyMetaTileEntities {
     public static MetaTileEntityPrimitiveItemBus PRIMITIVE_ITEM_IMPORT;
     public static MetaTileEntityPrimitiveItemBus PRIMITIVE_ITEM_EXPORT;
 
+    public static MetaTileEntitySieveDistillationTower SIEVE_DISTILLATION_TOWER;
+
     public static void init() {
         MAGNETIC_REFRIGERATOR = registerMetaTileEntity(14500, new MetaTileEntityMagneticRefrigerator(susyId("magnetic_refrigerator")));
         COAGULATION_TANK = registerMetaTileEntity(14501, new MetaTileEntityCoagulationTank(susyId("coagulation_tank")));
@@ -165,6 +167,7 @@ public class SuSyMetaTileEntities {
         STEAM_LATEX_COLLECTOR[0] = registerMetaTileEntity(14510, new MetaTileEntitySteamLatexCollector(susyId("latex_collector.bronze"), false));
         STEAM_LATEX_COLLECTOR[1] = registerMetaTileEntity(14511, new MetaTileEntitySteamLatexCollector(susyId("latex_collector.steel"), true));
 
+        SIEVE_DISTILLATION_TOWER = registerMetaTileEntity(14520, new MetaTileEntitySieveDistillationTower(susyId("sieve_distillation_tower")));
         SINTERING_OVEN = registerMetaTileEntity(14521, new MetaTileEntitySinteringOven(susyId("sintering_oven")));
 
         registerSimpleSteamMTE(STEAM_VULCANIZING_PRESS, 14515, "vulcanizing_press", SuSyRecipeMaps.VULCANIZATION_RECIPES, SuSySteamProgressIndicators.COMPRESS, SusyTextures.VULCANIZING_PRESS_OVERLAY, true);
