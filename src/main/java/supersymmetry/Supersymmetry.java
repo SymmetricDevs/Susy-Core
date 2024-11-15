93df412cb93764032ca9821b27a1f32e4883364d
CaliforniaDemise <155631180+CaliforniaDemise@users.noreply.github.com>
2024-11-15 18:32:58 +0000

    Fix event registeration and rocket model. (#338)

--- src/main/java/supersymmetry/Supersymmetry.java
+++ src/main/java/supersymmetry/Supersymmetry.java
@@ -1,30 +1,17 @@
 package supersymmetry;
 
 import gregtech.GTInternalTags;
-import net.minecraft.client.renderer.block.model.IBakedModel;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.registry.IRegistry;
-import net.minecraftforge.client.event.ModelBakeEvent;
-import net.minecraftforge.client.event.TextureStitchEvent;
-import net.minecraftforge.client.model.IModel;
-import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.client.model.obj.OBJLoader;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLConstructionEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.FMLLaunchHandler;
 import net.minecraftforge.fml.relauncher.Side;
 import org.jetbrains.annotations.NotNull;
 import supersymmetry.api.sound.SusySounds;
-import supersymmetry.client.renderer.handler.RocketRenderer;
 import supersymmetry.common.CommonProxy;
 import supersymmetry.common.SusyMetaEntities;
 import supersymmetry.common.blocks.SuSyBlocks;
@@ -34,7 +21,6 @@ import supersymmetry.common.command.CommandHordeStart;
 import supersymmetry.common.command.CommandHordeStatus;
 import supersymmetry.common.command.CommandHordeStop;
 import supersymmetry.common.covers.SuSyCoverBehaviors;
-import supersymmetry.common.entities.EntityRocket;
 import supersymmetry.common.item.SuSyMetaItems;
 import supersymmetry.common.metatileentities.SuSyMetaTileEntities;
 import supersymmetry.loaders.SuSyIRLoader;
@@ -59,24 +45,6 @@ public class Supersymmetry {
         SuSyIRLoader.initDefinitions();
         SuSyIRLoader.initEntities();
     }
-
-    @SubscribeEvent
-    public void bakeModel(ModelBakeEvent event) {
-        IRegistry<ModelResourceLocation, IBakedModel> registry = event.getModelRegistry();
-        try {
-            IModel model = OBJLoader.INSTANCE.loadModel(new ResourceLocation(MODID, "models/entity/soyuz.obj"));
-            registry.putObject(SuSyValues.modelRocket, model.bake(model.getDefaultState(), DefaultVertexFormats.ITEM, ModelLoader.defaultTextureGetter()));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @SubscribeEvent
-    public void stitchTexture(TextureStitchEvent.Pre event) {
-        TextureMap map = event.getMap();
-        map.registerSprite(new ResourceLocation(MODID, "entities/soyuz"));
-    }
-
 
     @Mod.EventHandler
     public void onPreInit(@NotNull FMLPreInitializationEvent event) {
