ca16d06e72019755fe12c859cefbb693fe85f178
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2024-12-09 00:27:46 +0800

    feat: jet wingpack obj model rendering

---
+++ src/main/java/supersymmetry/client/renderer/handler/JetWingpackModel.java
@@ -0,0 +1,27 @@
+package supersymmetry.client.renderer.handler;
+
+import net.minecraft.client.model.ModelBiped;
+
+import static supersymmetry.api.util.SuSyUtility.susyId;
+
+public class JetWingpackModel extends ModelBiped {
+
+    public static final JetWingpackModel INSTANCE = new JetWingpackModel();
+    private final OBJModelRender objModel;
+
+    public JetWingpackModel() {
+        this.textureWidth = 128;
+        this.textureHeight = 128;
+
+        this.bipedHead.cubeList.clear();
+        this.bipedHeadwear.cubeList.clear();
+        this.bipedBody.cubeList.clear();
+        this.bipedRightArm.cubeList.clear();
+        this.bipedLeftArm.cubeList.clear();
+        this.bipedLeftLeg.cubeList.clear();
+        this.bipedRightLeg.cubeList.clear();
+
+        this.objModel = new OBJModelRender(this, susyId("models/armor/jet_wingpack.obj"));
+        this.bipedBody.addChild(objModel);
+    }
+}
