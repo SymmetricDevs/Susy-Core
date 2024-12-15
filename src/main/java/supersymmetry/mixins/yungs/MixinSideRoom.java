28372e78867311b10d98d4b275ec4a41b9e6583f
Oganesson897 <101081378+Oganesson897@users.noreply.github.com>
2024-12-15 17:16:45 +0900

    remove furnace generate

---
+++ src/main/java/supersymmetry/mixins/yungs/MixinSideRoom.java
@@ -0,0 +1,23 @@
+package supersymmetry.mixins.yungs;
+
+import com.yungnickyoung.minecraft.bettermineshafts.world.generator.pieces.MineshaftPiece;
+import com.yungnickyoung.minecraft.bettermineshafts.world.generator.pieces.SideRoom;
+import net.minecraft.block.BlockFurnace;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.structure.StructureBoundingBox;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(SideRoom.class)
+public abstract class MixinSideRoom extends MineshaftPiece {
+
+    @Redirect(method = "addComponentParts", at = @At(value = "INVOKE", target = "Lcom/yungnickyoung/minecraft/bettermineshafts/world/generator/pieces/SideRoom;setBlockState(Lnet/minecraft/world/World;Lnet/minecraft/block/state/IBlockState;IIILnet/minecraft/world/gen/structure/StructureBoundingBox;)V"))
+    private void susycore$removeFurnace(SideRoom instance, World world, IBlockState iBlockState, int i1, int i2, int i3, StructureBoundingBox structureBoundingBox) {
+        if (iBlockState.getBlock() instanceof BlockFurnace) {
+            setBlockState(world, AIR, i1, i2, i3, structureBoundingBox);
+        }
+    }
+
+}
