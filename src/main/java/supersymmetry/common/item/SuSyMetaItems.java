aa7d9a42384d42a4e07e30134b6fc54c746ed268
bruberu <80226372+bruberu@users.noreply.github.com>
2024-12-22 13:14:59 -0600

    feat: file rearranging

--- src/main/java/supersymmetry/common/item/SuSyMetaItems.java
+++ src/main/java/supersymmetry/common/item/SuSyMetaItems.java
@@ -85,22 +85,6 @@ public class SuSyMetaItems {
         }));
         SIMPLE_GAS_MASK = armorItem.addItem(0, "simple_gas_mask")
                 .setArmorLogic(new SimpleGasMask());
-        GAS_MASK = armorItem.addItem(1, "gas_mask").setArmorLogic(new BreathingApparatus(HEAD));
-        GAS_TANK = armorItem.addItem(2, "gas_tank").setArmorLogic(new BreathingApparatus(CHEST));
-        ASBESTOS_MASK = armorItem.addItem(3, "asbestos_mask").setArmorLogic(new AdvancedBreathingApparatus(HEAD, 1, "asbestos", 0, 0.3));
-        ASBESTOS_CHESTPLATE = armorItem.addItem(4, "asbestos_chestplate").setArmorLogic(new AdvancedBreathingApparatus(CHEST, 1, "asbestos", 0, 0.3));
-        ASBESTOS_LEGGINGS = armorItem.addItem(5, "asbestos_leggings").setArmorLogic(new AdvancedBreathingApparatus(LEGS, 1, "asbestos", 0, 0.3));
-        ASBESTOS_BOOTS = armorItem.addItem(6, "asbestos_boots").setArmorLogic(new AdvancedBreathingApparatus(FEET, 1, "asbestos", 0, 0.3));
-        REBREATHER_TANK = armorItem.addItem(7, "rebreather_tank").setArmorLogic(new AdvancedBreathingApparatus(CHEST, 1, "rebreather", 0, 0.3));
-        REFLECTIVE_MASK = armorItem.addItem(8, "reflective_mask").setArmorLogic(new AdvancedBreathingApparatus(HEAD, 5, "reflective", 0, 0.4));
-        REFLECTIVE_CHESTPLATE = armorItem.addItem(9, "reflective_chestplate").setArmorLogic(new AdvancedBreathingApparatus(CHEST, 5, "reflective", 0, 0.4));
-        REFLECTIVE_LEGGINGS = armorItem.addItem(10, "reflective_leggings").setArmorLogic(new AdvancedBreathingApparatus(LEGS, 5, "reflective", 0, 0.4));
-        REFLECTIVE_BOOTS = armorItem.addItem(11, "reflective_boots").setArmorLogic(new AdvancedBreathingApparatus(FEET, 5, "reflective", 0, 0.4));
-        FILTERED_TANK = armorItem.addItem(12, "filtered_tank").setArmorLogic(new AdvancedBreathingApparatus(CHEST, 5, "filtered", 0, 0.4));
-        NOMEX_MASK = armorItem.addItem(13, "nomex_mask").setArmorLogic(new AdvancedBreathingApparatus(HEAD, 0, "nomex", 1, 0.6));
-        NOMEX_CHESTPLATE = armorItem.addItem(14, "nomex_chestplate").setArmorLogic(new AdvancedBreathingApparatus(CHEST, 0, "nomex", 1, 0.6));
-        NOMEX_LEGGINGS = armorItem.addItem(15, "nomex_leggings").setArmorLogic(new AdvancedBreathingApparatus(LEGS, 0, "nomex", 1, 0.6));
-        NOMEX_BOOTS = armorItem.addItem(16, "nomex_boots").setArmorLogic(new AdvancedBreathingApparatus(FEET, 0, "nomex", 1, 0.6));
     }
 
     private static void addTieredOredictItem(OreDictValueItem[] items, int id, int RGB, OrePrefix prefix) {
