bc2bc48cfc6b1e7651d3b87477db0f09cc01e446
Loxo <loxo.Minecraft@gmail.com>
2025-01-07 20:32:19 +0100

    feat: basic filtering predicates

--- src/main/java/supersymmetry/api/stockinteraction/StockFilterPredicate.java
+++ src/main/java/supersymmetry/api/stockinteraction/StockFilterPredicate.java
@@ -1,4 +1,85 @@
 package supersymmetry.api.stockinteraction;
 
-public class StockFilterPredicate {
+import cam72cam.immersiverailroading.entity.EntityRollingStock;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
+
+public abstract class StockFilterPredicate {
+
+    public abstract boolean test(EntityRollingStock entity);
+
+    public CompoundPredicate or(StockFilterPredicate other) {
+        CompoundPredicate combined = new CompoundPredicate(other);
+        combined.merge(this);
+        return combined;
+    }
+
+    public static boolean any(EntityRollingStock rollingStock, Collection<StockFilterPredicate> predicates) {
+        return predicates.stream().anyMatch(predicate -> predicate.test(rollingStock));
+    }
+
+    public static boolean all(EntityRollingStock rollingStock, Collection<StockFilterPredicate> predicates) {
+        return predicates.stream().anyMatch(predicate -> predicate.test(rollingStock));
+    }
+
+    public static class CompoundPredicate extends StockFilterPredicate {
+        public List<StockFilterPredicate> subPredicates;
+
+        public CompoundPredicate () {
+            this.subPredicates = new ArrayList<>();
+        }
+
+        public CompoundPredicate (StockFilterPredicate other) {
+            if(other instanceof CompoundPredicate compound) {
+                this.subPredicates = compound.subPredicates;
+            }
+            else {
+                this.subPredicates = new ArrayList<>();
+                this.subPredicates.add(other);
+            }
+        }
+
+        public void merge (StockFilterPredicate other) {
+            if (other instanceof CompoundPredicate compound) {
+                this.subPredicates.addAll(compound.subPredicates);
+            }
+            else {
+                this.subPredicates.add(other);
+            }
+        }
+
+        @Override
+        public boolean test(EntityRollingStock entity) {
+            return any(entity, this.subPredicates);
+        }
+    }
+
+    public static class SimplePredicate extends StockFilterPredicate {
+        public Predicate<EntityRollingStock> predicate;
+
+        public SimplePredicate(Predicate<EntityRollingStock> predicate) {
+            this.predicate = predicate;
+        }
+
+        @Override
+        public boolean test(EntityRollingStock entity) {
+            return this.predicate.test(entity);
+        }
+    }
+
+    public static class EntityClassPredicate extends StockFilterPredicate {
+        public Class classToMatch;
+
+        public EntityClassPredicate (Class cls) {
+            this.classToMatch = cls;
+        }
+
+        @Override
+        public boolean test(EntityRollingStock entity) {
+            return entity.getClass() == classToMatch;
+        }
+    }
 }
--- src/main/java/supersymmetry/api/stockinteraction/StockFilterPredicates.java
+++ src/main/java/supersymmetry/api/stockinteraction/StockFilterPredicates.java
@@ -1,4 +1,18 @@
 package supersymmetry.api.stockinteraction;
 
+import cam72cam.immersiverailroading.entity.Freight;
+import cam72cam.immersiverailroading.entity.FreightTank;
+import cam72cam.immersiverailroading.entity.LocomotiveDiesel;
+import cam72cam.immersiverailroading.entity.LocomotiveSteam;
+import supersymmetry.common.entities.EntityTunnelBore;
+
 public class StockFilterPredicates {
+    // TODO: Transporter erector once that is merged.
+    public static StockFilterPredicate FREIGHT = new StockFilterPredicate.EntityClassPredicate(Freight.class);
+    public static StockFilterPredicate DIESEL_LOCOMOTIVE = new StockFilterPredicate.EntityClassPredicate(LocomotiveDiesel.class);
+    public static StockFilterPredicate STEAM_LOCOMOTIVE = new StockFilterPredicate.EntityClassPredicate(LocomotiveSteam.class);
+    public static StockFilterPredicate LOCOMOTIVE = DIESEL_LOCOMOTIVE.or(STEAM_LOCOMOTIVE);
+    public static StockFilterPredicate FREIGHT_TANK = new StockFilterPredicate.EntityClassPredicate(FreightTank.class);
+    public static StockFilterPredicate TUNNEL_BORE = new StockFilterPredicate.EntityClassPredicate(EntityTunnelBore.class);
+
 }
