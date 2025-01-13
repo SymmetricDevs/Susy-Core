1ca23462dd4e37889c4f98aefbb61ff02d2afd09
Loxo <loxo.Minecraft@gmail.com>
2025-01-13 16:44:26 +0100

    feat: allow any filter

--- src/main/java/supersymmetry/api/stockinteraction/StockFilterPredicates.java
+++ src/main/java/supersymmetry/api/stockinteraction/StockFilterPredicates.java
@@ -8,6 +8,8 @@ import supersymmetry.common.entities.EntityTunnelBore;
 
 public class StockFilterPredicates {
     // TODO: Transporter erector once that is merged.
+
+    public static StockFilterPredicate ANY = new StockFilterPredicate.SimplePredicate(entity -> true);
     public static StockFilterPredicate FREIGHT = new StockFilterPredicate.EntityClassPredicate(Freight.class);
     public static StockFilterPredicate DIESEL_LOCOMOTIVE = new StockFilterPredicate.EntityClassPredicate(LocomotiveDiesel.class);
     public static StockFilterPredicate STEAM_LOCOMOTIVE = new StockFilterPredicate.EntityClassPredicate(LocomotiveSteam.class);
