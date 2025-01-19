d701f259076f4b4ba41be2066b20a6a87d1baa63
Loxo <loxo.Minecraft@gmail.com>
2025-01-19 12:12:28 +0100

    fix: rendering and localisation

--- src/main/java/supersymmetry/common/blocks/BlockRustySteelFrame.java
+++ src/main/java/supersymmetry/common/blocks/BlockRustySteelFrame.java
@@ -3,13 +3,28 @@ package supersymmetry.common.blocks;
 import gregtech.api.block.IStateHarvestLevel;
 import gregtech.api.block.VariantBlock;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.material.EnumPushReaction;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
 
 public class BlockRustySteelFrame extends VariantBlock<BlockRustySteelFrame.RustySteelFrameVariant> {
+
+    public static final AxisAlignedBB COLLISION_BOX = new AxisAlignedBB(0.05, 0.0, 0.05, 0.95, 1.0, 0.95);
 
     public BlockRustySteelFrame() {
         super(Material.IRON);
@@ -20,6 +35,48 @@ public class BlockRustySteelFrame extends VariantBlock<BlockRustySteelFrame.Rust
         setDefaultState(getState(RustySteelFrameVariant.RUSTY_STEEL_VARIANT_1));
     }
 
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getRenderLayer()
+    {
+        return BlockRenderLayer.CUTOUT_MIPPED;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    public @NotNull BlockFaceShape getBlockFaceShape(@NotNull IBlockAccess worldIn, @NotNull IBlockState state, @NotNull BlockPos pos, @NotNull EnumFacing face) {
+        return BlockFaceShape.UNDEFINED;
+    }
+
+    // Taken from GT frame box code
+    @Override
+    public void onEntityCollision(@NotNull World worldIn, @NotNull BlockPos pos, @NotNull IBlockState state, Entity entityIn) {
+        entityIn.motionX = MathHelper.clamp(entityIn.motionX, -0.15, 0.15);
+        entityIn.motionZ = MathHelper.clamp(entityIn.motionZ, -0.15, 0.15);
+        entityIn.fallDistance = 0.0F;
+        if (entityIn.motionY < -0.15) {
+            entityIn.motionY = -0.15;
+        }
+
+        if (entityIn.isSneaking() && entityIn.motionY < 0.0) {
+            entityIn.motionY = 0.0;
+        }
+
+        if (entityIn.collidedHorizontally) {
+            entityIn.motionY = 0.3;
+        }
+
+    }
+
+    public @NotNull EnumPushReaction getPushReaction(@NotNull IBlockState state) {
+        return EnumPushReaction.DESTROY;
+    }
+
+    public AxisAlignedBB getCollisionBoundingBox(@NotNull IBlockState blockState, @NotNull IBlockAccess worldIn, @NotNull BlockPos pos) {
+        return COLLISION_BOX;
+    }
 
     public enum RustySteelFrameVariant implements IStringSerializable, IStateHarvestLevel {
         RUSTY_STEEL_VARIANT_1("variant_1", 1),
