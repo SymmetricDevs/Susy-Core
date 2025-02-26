34f7b00eeaca74f54252b712e881599dfff4546f
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2025-02-26 09:26:44 +0800

    fix: delegator TOP info broken on server

--- src/main/java/supersymmetry/integration/theoneprobe/provider/DelegatorInfoProvider.java
+++ src/main/java/supersymmetry/integration/theoneprobe/provider/DelegatorInfoProvider.java
@@ -2,10 +2,7 @@ package supersymmetry.integration.theoneprobe.provider;
 
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
-import mcjty.theoneprobe.api.IProbeHitData;
-import mcjty.theoneprobe.api.IProbeInfo;
-import mcjty.theoneprobe.api.IProbeInfoProvider;
-import mcjty.theoneprobe.api.ProbeMode;
+import mcjty.theoneprobe.api.*;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
@@ -29,8 +26,8 @@ public class DelegatorInfoProvider implements IProbeInfoProvider {
             if (te instanceof IGregTechTileEntity igtte) {
                 MetaTileEntity mte = igtte.getMetaTileEntity();
                 if (mte instanceof IDelegator delegator) {
-                    probeInfo.text(I18n.format("gregtech.top.delegator.delegating_face",
-                            delegator.getDelegatingFacing(data.getSideHit())));
+                    probeInfo.text(TextStyleClass.INFO + "{*gregtech.top.delegator.delegating_face*}" +
+                            delegator.getDelegatingFacing(data.getSideHit()));
                 }
             }
         }
