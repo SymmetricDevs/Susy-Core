1f1e2238545c5419b06b892367dd7892ed24ebf8
Tian mi <35869948+MCTian-mi@users.noreply.github.com>
2025-03-04 22:58:07 +0800

    Rail Interfaces (#396)
    * Move railinterfaces branch onto the main repo because this is pissing me off. (#366)
    
    * basic stock detector metatileentity
    
    basic stock detection metatileentity with saved data but no detection capability yet
    
    * added functions and interfaces for stock detection
    
    Added generic and untyped stock detection functions using AABB bounding boxes. Stock class is now stored as a string and StockHelperFunctions has functions to both check if an object is that class or get the class from string
    
    * fully functional stock detector
    
    stock detector fully functional
    
    * much changed. fluid exchanger working
    
    fluid exchanger functional, rework of stock detector and some changes to helper functions, item exchanger started (pulling works but not pulling or inventory transfer)
    
    * added more interfaces
    
    fluid and item exchangers working, reader working, started on controller
    
    * finished all basic blocks and added vertical slider gui component
    
    finished all basic blocks. item exchanger, fluid exchanger, item/fluid reader, stock detector, locomotive controller. also added vertical slider gui component for the locomotive controller gui
    
    * polished blocks and reconfiugred gui
    
    made a bunch of changes to gui and blocks, and optimised the packet sending and filter cycling.
    
    * Added separate on off states for stock controller
    
    stock controller now has on and off behaviours defined separately
    
    * First iteration of the item exchanger UI redo
    
    * Improve stock fluid exchanger UI and fix transfer sometimes deleting fluid
    
    * Progress Transfer (WIP): Abstract stock interactors, add config UI, improve fluid transfer code and improve filter code
    
    * TODOs
    
    * Render interaction bounding box
    
    * Sync interaction area
    
    * Commit progress
    
    * feat: start work on this again
    
    * Refactor StockFilter.java
    
    * Remove some entities for now.
    
    * Make stock IO interactors redstone interactable.
    
    * Feature: rightclicking io stock interfaces with screwdriver to cycle between pull and push
    
    * Fix: NPE because I am unable to read
    
    * Fix: Sync transfer state upon updating.
    
    ---------
    
    Co-authored-by: Dagan Hartmann <dagan.hartmann@gmail.com>
    Co-authored-by: Dagan Hartmann <110746303+ch3mbot@users.noreply.github.com>
    Co-authored-by: Gaming <112270586+swagXDragonSlayer46YT@users.noreply.github.com>
    Co-authored-by: bruberu <80226372+bruberu@users.noreply.github.com>
    
    * feat: maybe a modular entity filtering system
    
    * feat: I hate ceu making everything private
    
    * feat: basic filtering predicates
    
    * feat: allow any filter
    
    * feat: name matching
    
    * fix: properly query for the entity name because of course IR has to be extra
    
    * feat: make the item exchanger act like a delegate of the train inventory
    
    * feat: init mui2
    
    * fix: render overlay properly
    
    * fix: fix switch up in state rendering
    
    * fix: reset inventory
    
    * the giant refactor
    
    * feat: preparing trollage
    
    * feat: turn off track smoothing for sloped tunnelbore tracks
    
    * feat: filter pop-up panel & stockfilter rewrite
    
    * fix: make name filter work
    
    * feat: cleanup pattern code
    
    * feat: no longer caching stock lists & some cleanups
    
    * fix: check stock isn't dead
    
    * feat: controller ui
    
    * feat: cleaning up Locomotive Controller
    
    * feat: remove SliderWidgetAccessor
    
    * feat: lang & icons
    
    * feat: tiny cleanups
    
    * feat: cleanup and update to mui 2.5-rc4
    
    * feat: remove IR mixin
    
    ---------
    
    Co-authored-by: mtbo <111296252+loxoDev@users.noreply.github.com>
    Co-authored-by: Dagan Hartmann <dagan.hartmann@gmail.com>
    Co-authored-by: Dagan Hartmann <110746303+ch3mbot@users.noreply.github.com>
    Co-authored-by: Gaming <112270586+swagXDragonSlayer46YT@users.noreply.github.com>
    Co-authored-by: bruberu <80226372+bruberu@users.noreply.github.com>
    Co-authored-by: Loxo <loxo.Minecraft@gmail.com>

---
+++ src/main/java/supersymmetry/common/blocks/BlockStockDetector.java
@@ -0,0 +1,153 @@
+package supersymmetry.common.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+import java.util.Random;
+
+public class BlockStockDetector extends Block implements ITileEntityProvider
+{
+    public static final PropertyBool POWERED = PropertyBool.create("powered");
+    public BlockStockDetector()
+    {
+        super(Material.IRON);
+        setHarvestLevel("pickaxe", 0);
+        setHardness(2F);
+        setResistance(15F);
+        setDefaultState(getDefaultState().withProperty(POWERED, Boolean.valueOf(false)));
+        setCreativeTab(CreativeTabs.REDSTONE);
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(World worldIn, int meta)
+    {
+        return null;
+        //return new stock_detector_tile_entity(worldIn, meta);
+    }
+
+    @Override
+    public Item getItemDropped(IBlockState p_180660_1_, Random p_180660_2_, int p_180660_3_)
+    {
+        return null;
+        //return Item.getItemFromBlock(BRIBlocks.stock_detector);
+    }
+
+    @Override
+    public void breakBlock(World worldIn, BlockPos pos, IBlockState state)
+    {
+        //BetterRailInterfaces.logger.warn("broke block and is remote: " + worldIn.isRemote);
+        //BetterRailInterfaces.logger.warn("broke block that had powered: " + state.getValue(POWERED));
+
+        super.breakBlock(worldIn, pos, state);
+
+        /*
+        TileEntity tileentity = worldIn.getTileEntity(pos);
+
+        //worldIn.removeTileEntity(pos);
+
+        if (tileentity instanceof stock_detector_tile_entity)
+        {
+            super.breakBlock(worldIn, pos, state);
+        }
+*/
+
+
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, new IProperty[] {POWERED});
+    }
+
+    /*
+    @Override
+    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
+    {
+    }
+    */
+
+    //old and unused
+    public void updateBlockState(World worldIn, BlockPos pos, IBlockState state, boolean on)
+    {
+        if(worldIn.isRemote)
+        {
+            state = state.withProperty(POWERED, on);
+            worldIn.setBlockState(pos, state, 3);
+            worldIn.notifyNeighborsOfStateChange(pos, this, false);
+        }
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta)
+    {
+        return this.getDefaultState().withProperty(POWERED, meta == 1);
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state)
+    {
+        return state.getValue(POWERED)? 1 : 0;
+    }
+
+/*
+    @Override
+    public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos pos)
+    {
+        //BetterRailInterfaces.logger.warn("called getActualState, state had value: " + state.getValue(POWERED)); //seems to always match server
+        stock_detector_tile_entity te = (stock_detector_tile_entity)world.getTileEntity(pos);
+        return state.withProperty(POWERED, te.detected);
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return true;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return true;
+    }
+*/
+
+    @Override
+    public boolean hasTileEntity(IBlockState state) {
+        return true;
+    }
+
+    @Override
+    public int getWeakPower(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing facing) {
+        //BetterRailInterfaces.logger.warn("weak power from state: " + state.getValue(POWERED) + ", weak power from entity: " + ((stock_detector_tile_entity)world.getTileEntity(pos)).detected
+        //+ ", is remote: " + world.getTileEntity(pos).getWorld().isRemote
+        //);
+        return state.getValue(POWERED)? 15 : 0;
+        //return ((stock_detector_tile_entity)world.getTileEntity(pos)).detected ? 15 : 0;
+    }
+
+    @Override
+    public int getStrongPower(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing facing) {
+        return getWeakPower(state, world, pos, facing);
+        //return ((stock_detector_tile_entity)world.getTileEntity(pos)).detected ? 15 : 0;
+    }
+
+    @Override
+    public boolean canProvidePower(IBlockState state) {
+        return true;
+    }
+
+}
---
+++ src/main/java/supersymmetry/common/metatileentities/single/railinterfaces/MetaTileEntityStockInteractor.java
@@ -0,0 +1,355 @@
+package supersymmetry.common.metatileentities.single.railinterfaces;
+
+import cam72cam.immersiverailroading.entity.EntityRollingStock;
+import codechicken.lib.render.CCRenderState;
+import codechicken.lib.render.pipeline.IVertexOperation;
+import codechicken.lib.vec.Matrix4;
+import com.cleanroommc.modularui.api.drawable.IKey;
+import com.cleanroommc.modularui.drawable.GuiTextures;
+import com.cleanroommc.modularui.factory.PosGuiData;
+import com.cleanroommc.modularui.screen.ModularPanel;
+import com.cleanroommc.modularui.utils.Alignment;
+import com.cleanroommc.modularui.value.sync.BooleanSyncValue;
+import com.cleanroommc.modularui.value.sync.PanelSyncHandler;
+import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import com.cleanroommc.modularui.widgets.ButtonWidget;
+import com.cleanroommc.modularui.widgets.CycleButtonWidget;
+import com.cleanroommc.modularui.widgets.SlotGroupWidget;
+import com.cleanroommc.modularui.widgets.ToggleButton;
+import com.cleanroommc.modularui.widgets.layout.Flow;
+import gregtech.api.capability.GregtechDataCodes;
+import gregtech.api.capability.GregtechTileCapabilities;
+import gregtech.api.capability.IControllable;
+import gregtech.api.metatileentity.IFastRenderMetaTileEntity;
+import gregtech.client.renderer.ICubeRenderer;
+import gregtech.client.utils.RenderBufferHelper;
+import gregtech.client.utils.RenderUtil;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.apache.commons.lang3.tuple.Pair;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.lwjgl.opengl.GL11;
+import supersymmetry.api.SusyLog;
+import supersymmetry.api.gui.SusyGuiTextures;
+import supersymmetry.api.metatileentity.Mui2MetaTileEntity;
+import supersymmetry.api.stockinteraction.IStockInteractor;
+import supersymmetry.api.stockinteraction.StockFilter;
+import supersymmetry.api.stockinteraction.StockHelperFunctions;
+import supersymmetry.client.renderer.textures.SusyTextures;
+
+import java.io.IOException;
+
+public abstract class MetaTileEntityStockInteractor extends Mui2MetaTileEntity implements IStockInteractor, IFastRenderMetaTileEntity, IControllable {
+
+    AxisAlignedBB interactionBoundingBox;
+    private double interactionWidth = 11.;
+    private double interactionDepth = 5.;
+
+    // This defines which stock classes can be interacted with
+    protected StockFilter stockFilter;
+
+    @Nullable
+    protected EntityRollingStock stock;
+
+    protected boolean workingEnabled = true;
+    // Rendering
+    protected final ICubeRenderer renderer;
+    // If the current bounding box should be rendered
+    protected boolean renderBoundingBox = false;
+
+    public static final int TICK_RATE = 10;
+
+    public MetaTileEntityStockInteractor(ResourceLocation metaTileEntityId, ICubeRenderer renderer) {
+        super(metaTileEntityId);
+        this.renderer = renderer;
+        this.stockFilter = new StockFilter(9);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        updateStock();
+    }
+
+    public void updateStock() {
+        if (!getWorld().isRemote && this.isWorkingEnabled() && this.getOffsetTimer() % TICK_RATE == 0) {
+            this.stock = StockHelperFunctions.getStockFrom(getWorld(), getInteractionBoundingBox(), stockFilter);
+        }
+    }
+
+    @Override
+    public void setWorkingEnabled(boolean workingEnabled) {
+        this.workingEnabled = workingEnabled;
+        World world = this.getWorld();
+        if (world != null && !world.isRemote) {
+            this.writeCustomData(GregtechDataCodes.WORKING_ENABLED, (buf) -> {
+                buf.writeBoolean(workingEnabled);
+            });
+        }
+    }
+
+    @Override
+    public boolean isWorkingEnabled() {
+        return this.workingEnabled;
+    }
+
+    protected abstract <T> T getStockCapability(Capability<T> capability, EnumFacing side);
+
+    @Override
+    public <T> T getCapability(Capability<T> capability, EnumFacing side) {
+        if (capability == GregtechTileCapabilities.CAPABILITY_CONTROLLABLE) {
+            return GregtechTileCapabilities.CAPABILITY_CONTROLLABLE.cast(this);
+        }
+        if (stock != null && !stock.isDead()) {
+            T stockCapability = getStockCapability(capability, side);
+            if (stockCapability != null) {
+                return stockCapability;
+            }
+        }
+        return super.getCapability(capability, side);
+    }
+
+    @Override
+    public boolean needsSneakToRotate() {
+        return true;
+    }
+
+    // UI
+    @Override
+    public ModularPanel buildUI(PosGuiData guiData, PanelSyncManager syncManager) {
+
+        PanelSyncHandler panel = (PanelSyncHandler) syncManager.panel("filter_panel",
+                (panelSyncManager, syncHandler) -> stockFilter.createPopupPanel(panelSyncManager),
+                true);
+
+        BooleanSyncValue workingStateValue = new BooleanSyncValue(() -> workingEnabled, val -> workingEnabled = val);
+        BooleanSyncValue renderBoundingBoxValue = new BooleanSyncValue(() -> renderBoundingBox, val -> renderBoundingBox = val);
+
+        return defaultPanel(this)
+                .child(IKey.lang(getMetaFullName()).asWidget()
+                        .pos(5, 5))
+                .child(SlotGroupWidget.playerInventory()
+                        .left(7)
+                        .bottom(7))
+                .child(getLogo().asWidget()
+                        .size(17)
+                        .right(7)
+                        .bottom(88))
+                .child(new CycleButtonWidget()
+                        .left(7)
+                        .bottom(90)
+                        .background(GuiTextures.BUTTON_CLEAN)
+                        .hoverBackground(GuiTextures.BUTTON_CLEAN)
+                        .stateCount(2)
+                        .stateOverlay(SusyGuiTextures.BUTTON_POWER)
+                        .value(workingStateValue))
+                .child(Flow.column()
+                        .top(18)
+                        .margin(7, 0)
+                        .widthRel(1f)
+                        .coverChildrenHeight()
+                        .child(Flow.row()
+                                .coverChildrenHeight()
+                                .marginBottom(2)
+                                .widthRel(1f)
+                                .child(new ToggleButton()
+                                        .overlay(SusyGuiTextures.BUTTON_RENDER_AREA.asIcon().size(16))
+                                        .addTooltipLine(IKey.lang("susy.gui.stock_interactor.button.render_bounding_box.tooltip"))
+                                        .value(renderBoundingBoxValue))
+                                .child(IKey.lang("susy.gui.stock_interactor.title.render_bounding_box").asWidget()
+                                        .align(Alignment.CenterRight)
+                                        .height(18))
+                        )
+                        .child(Flow.row()
+                                .coverChildrenHeight()
+                                .marginBottom(2)
+                                .widthRel(1f)
+                                .child(new ButtonWidget<>()
+                                        .overlay(SusyGuiTextures.BUTTON_STOCK_FILTER.asIcon().size(16))
+                                        .addTooltipLine(IKey.lang("susy.gui.stock_interactor.button.stock_filter.tooltip"))
+                                        .onMousePressed(mouseButton -> {
+                                            if (!panel.isPanelOpen()) {
+                                                panel.openPanel();
+                                            } else {
+                                                panel.closePanel();
+                                            }
+                                            return true;
+                                        })
+                                )
+                                .child(IKey.lang("susy.gui.stock_interactor.title.stock_filter").asWidget()
+                                        .align(Alignment.CenterRight).height(18))
+                        )
+                );
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void renderMetaTileEntity(double x, double y, double z, float partialTicks) {
+        if(this.shouldRenderBoundingBox()) {
+            GlStateManager.pushMatrix();
+
+            RenderUtil.moveToFace(x,y,z,getFrontFacing());
+            GlStateManager.translate(0, -.5, 0);
+            RenderUtil.rotateToFace(getFrontFacing(), null);
+
+            GlStateManager.enableBlend();
+            GlStateManager.disableLighting();
+            GlStateManager.disableTexture2D();
+            GlStateManager.blendFunc(770, 771);
+            GlStateManager.glLineWidth(15);
+
+            Tessellator tessellator = Tessellator.getInstance();
+            BufferBuilder buffer = tessellator.getBuffer();
+            buffer.begin(GL11.GL_LINES, DefaultVertexFormats.POSITION_COLOR);
+
+            RenderBufferHelper.renderCubeFrame(buffer, -this.getInteractionWidth() / 2, 0, 0, this.getInteractionWidth() / 2,  (-this.getInteractionDepth() - this.getInteractionWidth()) / 2, this.getInteractionDepth(), 1, 0, 0, 0.6F);
+            tessellator.draw();
+
+            GlStateManager.disableBlend();
+            GlStateManager.enableLighting();
+            GlStateManager.enableTexture2D();
+            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+
+            GlStateManager.popMatrix();
+
+        }
+    }
+
+    @Override
+    public void renderMetaTileEntity(CCRenderState renderState, Matrix4 translation, IVertexOperation[] pipeline) {
+        SusyTextures.STOCK_MACHINE_CASING.render(renderState,translation,pipeline);
+        this.renderer.renderOrientedState(renderState, translation, pipeline, this.getFrontFacing(), true, this.isWorkingEnabled());
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public Pair<TextureAtlasSprite, Integer> getParticleTexture() {
+        return Pair.of(SusyTextures.STOCK_MACHINE_CASING.getParticleSprite(), getPaintingColorForRendering());
+    }
+
+    @Override
+    public AxisAlignedBB getRenderBoundingBox() {
+        return this.getInteractionBoundingBox();
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound data) {
+        super.writeToNBT(data);
+        data.setTag("stockFilter", this.stockFilter.serializeNBT());
+        data.setDouble("interactionWidth", this.interactionWidth);
+        data.setDouble("interactionDepth", this.interactionDepth);
+        data.setBoolean("renderBoundingBox", this.renderBoundingBox);
+        data.setBoolean("workingEnabled", this.workingEnabled);
+        return data;
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound data) {
+        super.readFromNBT(data);
+        this.stockFilter.deserializeNBT(data.getCompoundTag("stockFilter"));
+        this.setInteractionWidth(data.getDouble("interactionWidth"));
+        this.setInteractionDepth(data.getDouble("interactionDepth"));
+        this.renderBoundingBox = data.getBoolean("renderBoundingBox");
+        this.workingEnabled = data.getBoolean("workingEnabled");
+    }
+
+    @Override
+    public void writeInitialSyncData(@NotNull PacketBuffer buf) {
+        super.writeInitialSyncData(buf);
+        buf.writeCompoundTag(this.stockFilter.serializeNBT());
+        buf.writeDouble(this.getInteractionWidth());
+        buf.writeDouble(this.getInteractionDepth());
+        buf.writeBoolean(this.renderBoundingBox);
+        buf.writeBoolean(this.workingEnabled);
+    }
+
+    @Override
+    public void receiveInitialSyncData(@NotNull PacketBuffer buf) {
+        super.receiveInitialSyncData(buf);
+        try {
+            //noinspection DataFlowIssue
+            this.stockFilter.deserializeNBT(buf.readCompoundTag());
+        } catch (IOException e) {
+            SusyLog.logger.info("Could not deserialize stock stockFilter in stock interactor at {}", getPos());
+            SusyLog.logger.error(e);
+        }
+        this.setInteractionWidth(buf.readDouble());
+        this.setInteractionDepth(buf.readDouble());
+        this.renderBoundingBox = buf.readBoolean();
+        this.workingEnabled = buf.readBoolean();
+    }
+
+    @Override
+    public void receiveCustomData(int dataId, @NotNull PacketBuffer buf) {
+        super.receiveCustomData(dataId, buf);
+
+        if(dataId == 6500) {
+            this.renderBoundingBox = buf.readBoolean();
+        }
+
+        // Front facing changed
+        if(dataId == -2) {
+            this.recalculateBoundingBox();
+        }
+
+        if(dataId == GregtechDataCodes.WORKING_ENABLED) {
+            this.workingEnabled = buf.readBoolean();
+        }
+        this.scheduleRenderUpdate();
+
+    }
+
+    @Override
+    public AxisAlignedBB getInteractionBoundingBox() {
+        return interactionBoundingBox == null ? interactionBoundingBox = StockHelperFunctions.getBox(this.getPos(), this.getFrontFacing(), this.getInteractionWidth(), this.getInteractionDepth()) : interactionBoundingBox;
+    }
+
+    public void recalculateBoundingBox() {
+        this.interactionBoundingBox = StockHelperFunctions.getBox(this.getPos(), this.getFrontFacing(), this.getInteractionWidth(), this.getInteractionDepth());
+    }
+
+    public double getInteractionWidth() {
+        return interactionWidth;
+    }
+
+    public void setInteractionWidth(double interactionWidth) {
+        this.interactionWidth = interactionWidth;
+        this.recalculateBoundingBox();
+    }
+
+    public double getInteractionDepth() {
+        return interactionDepth;
+    }
+
+    public void setInteractionDepth(double interactionDepth) {
+        this.interactionDepth = interactionDepth;
+        this.recalculateBoundingBox();
+    }
+
+    public boolean shouldRenderBoundingBox() {
+        return this.renderBoundingBox;
+    }
+
+    public void setRenderBoundingBox(boolean renderBoundingBox) {
+        if(this.renderBoundingBox != renderBoundingBox) {
+            this.renderBoundingBox = renderBoundingBox;
+            if (!this.getWorld().isRemote) {
+                this.writeCustomData(6500, (buf -> buf.writeBoolean(this.renderBoundingBox)));
+                this.markDirty();
+            }
+        }
+    }
+}
