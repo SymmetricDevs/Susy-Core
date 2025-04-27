279ae6a3df40bc2fefffa0138e56fd150d9ce8e7
trainvoi <voitrainelephant@gmail.com>
2025-04-27 08:36:54 +0700

    line seperation

--- src/main/java/supersymmetry/api/capability/RecipeMapExtension.java
+++ src/main/java/supersymmetry/api/capability/RecipeMapExtension.java
@@ -2,7 +2,10 @@ package supersymmetry.api.capability;
 
 public interface RecipeMapExtension {
     void modifyMaxInputs(int maxInputs);
+
     void modifyMaxOutputs(int maxOutputs);
+
     void modifyMaxFluidInputs(int maxFluidInputs);
+
     void modifyMaxFluidOutputs(int maxFluidOutputs);
 }
