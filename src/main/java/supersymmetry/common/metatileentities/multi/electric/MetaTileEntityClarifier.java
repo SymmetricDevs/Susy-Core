5e7ed9fea4757a223d1318a817e8f1aa8bd9d8ec
h3tR <66027579+h3tR@users.noreply.github.com>
2025-06-05 19:35:56 +0200

    clarifier input fluid rendering

--- src/main/java/supersymmetry/common/metatileentities/multi/electric/MetaTileEntityClarifier.java
+++ src/main/java/supersymmetry/common/metatileentities/multi/electric/MetaTileEntityClarifier.java
@@ -1,5 +1,13 @@
 package supersymmetry.common.metatileentities.multi.electric;
 
+import codechicken.lib.render.BlockRenderer;
+import codechicken.lib.render.CCRenderState;
+import codechicken.lib.render.pipeline.ColourMultiplier;
+import codechicken.lib.render.pipeline.IVertexOperation;
+import codechicken.lib.vec.Cuboid6;
+import codechicken.lib.vec.Matrix4;
+import codechicken.lib.vec.Translation;
+import codechicken.lib.vec.uv.IconTransformation;
 import gregtech.api.capability.impl.MultiblockRecipeLogic;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
@@ -7,6 +15,7 @@ import gregtech.api.metatileentity.multiblock.IMultiblockPart;
 import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.recipes.Recipe;
 import gregtech.api.unification.material.Materials;
 import gregtech.client.renderer.ICubeRenderer;
 import gregtech.client.renderer.texture.Textures;
@@ -16,10 +25,17 @@ import gregtech.common.blocks.BlockMetalCasing.MetalCasingType;
 import gregtech.common.blocks.BlockTurbineCasing;
 import gregtech.common.blocks.MetaBlocks;
 import gregtech.common.blocks.StoneVariantBlock;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
+import net.minecraftforge.fluids.Fluid;
 import supersymmetry.api.recipes.SuSyRecipeMaps;
 import supersymmetry.client.renderer.textures.SusyTextures;
 import supersymmetry.common.blocks.BlockMultiblockTank;
@@ -28,8 +44,21 @@ import supersymmetry.common.blocks.SuSyBlocks;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class MetaTileEntityClarifier extends RecipeMapMultiblockController {
+
+    private Recipe previousRecipe;
+    private TextureAtlasSprite fluidTexture;
+    private int fluidColor;
+    private boolean hasFluidSelected = false;
+    private static final ThreadLocal<BlockRenderer.BlockFace> blockFaces = ThreadLocal
+            .withInitial(BlockRenderer.BlockFace::new);
+    private static final Cuboid6 FLUID_RENDER_CUBOID = new Cuboid6(0,0,0,1,3/16F,1);
+
+
+
+
     public MetaTileEntityClarifier(ResourceLocation metaTileEntityId) {
         super(metaTileEntityId, SuSyRecipeMaps.CLARIFIER);
         this.recipeMapWorkable = new MultiblockRecipeLogic(this, true);
@@ -96,5 +125,99 @@ public class MetaTileEntityClarifier extends RecipeMapMultiblockController {
     @Override
     public boolean allowsExtendedFacing() {
         return false;
+    }
+
+    @Override
+    public void renderMetaTileEntity(CCRenderState renderState, Matrix4 translation, IVertexOperation[] pipeline) {
+        super.renderMetaTileEntity(renderState, translation, pipeline);
+        if(this.isActive() && this.fluidTexture != null)
+            atEachTank(pos -> renderFluid(pos,renderState));
+
+    }
+
+
+    @Override
+    public void update() {
+        super.update();
+        if(this.isActive()) {
+            if (!getWorld().isRemote){
+                //Updates fluid texture and color when recipe is changed
+                Recipe currentRecipe = getRecipeMapWorkable().getPreviousRecipe();
+                if((currentRecipe != previousRecipe || getWorld().getWorldTime() % 100 == 0) && currentRecipe != null) { //update on recipe change and every 5 seconds
+                    //filters the input fluids for the consumed input fluid (not flotation agents)
+                    currentRecipe.getFluidInputs().stream().filter(fluidInput -> !fluidInput.isNonConsumable()).findFirst().ifPresent(fluidInput -> {
+                        System.out.println("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH I HAVE PARTICLE AIDS");
+                        Fluid fluid = fluidInput.getInputFluidStack().getFluid();
+                        this.writeCustomData(0x434C4152, buf -> {
+                            buf.writeInt(fluid.getColor());
+                            buf.writeResourceLocation(fluid.getStill());
+                        });
+                        previousRecipe = currentRecipe;
+                    });
+                }
+            }
+        }
+    }
+
+
+    private final static String[] fluidPattern = {
+            "     DDDD     ",
+            "   DDDDDDDD   ",
+            "  DDDDDDDDDD  ",
+            " DDDDDDDDDDDD ",
+            " DDDDDDDDDDDD ",
+            "DDDDDDDDDDDDDD",
+            "DDDDDD  DDDDDD",
+            "DDDDDD  DDDDDD",
+            "DDDDDDDDDDDDDD",
+            " DDDDDDDDDDDD ",
+            " DDDDDDDDDDDD ",
+            "  DDDDDDDDDD  ",
+            "   DDDDDDDD   ",
+            "     DDDD     "
+    };
+    //the coordinates where the pattern is relative to the multi controller
+    private final int patternXOffset = -11;
+    private final static int patternYOffset = 1;
+
+    private void atEachTank(Consumer<BlockPos> consumer) {
+        EnumFacing facing = this.getFrontFacing().getOpposite();
+        for (int y = 0; y < fluidPattern.length; y++) {
+            for (int x = 0; x < fluidPattern[y].length(); x++) {
+                if(fluidPattern[y].charAt(x) == ' ') continue;
+                int patternX = (x + patternXOffset);
+                int patternY = (y + patternYOffset);
+                if (this.isFlipped)
+                    patternX = -patternX;
+                consumer.accept(getPos().add(
+                        patternY * facing.getXOffset() + patternX * facing.getZOffset(),
+                        1,
+                        patternX * facing.getXOffset() + patternY * facing.getZOffset()
+                ));
+            }
+        }
+    }
+
+    @Override
+    public void receiveCustomData(int discriminator, PacketBuffer buf) {
+        super.receiveCustomData(discriminator, buf);
+        if (discriminator == 0x434C4152) { //Spells out CLAR in hex (for clarifier)
+            this.fluidColor = buf.readInt();
+            this.fluidTexture = Minecraft.getMinecraft().getTextureMapBlocks().getAtlasSprite(buf.readResourceLocation().toString());
+        }
+    }
+
+
+    private void renderFluid(Vec3i pos, CCRenderState renderState){
+        IVertexOperation[] fluid_render_pipeline = {
+                new Translation(pos),
+                new IconTransformation(fluidTexture),
+                new ColourMultiplier(fluidColor)
+        };
+
+        BlockRenderer.BlockFace blockFace = blockFaces.get();
+        blockFace.loadCuboidFace(FLUID_RENDER_CUBOID, EnumFacing.UP.getIndex());
+        renderState.setPipeline(blockFace, 0, blockFace.verts.length, fluid_render_pipeline);
+        renderState.render();
     }
 }
