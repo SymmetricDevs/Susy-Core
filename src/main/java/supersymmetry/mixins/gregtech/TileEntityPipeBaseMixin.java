5f74f061288d95534b52da0969fc68290baa5bc1
bruberu <80226372+bruberu@users.noreply.github.com>
2025-06-10 14:14:52 -0500

    feat: even more cleanup

--- src/main/java/supersymmetry/mixins/gregtech/TileEntityPipeBaseMixin.java
+++ src/main/java/supersymmetry/mixins/gregtech/TileEntityPipeBaseMixin.java
@@ -6,9 +6,12 @@ import gregtech.api.pipenet.block.IPipeType;
 import gregtech.api.pipenet.tile.IPipeTile;
 import gregtech.api.pipenet.tile.PipeCoverableImplementation;
 import gregtech.api.pipenet.tile.TileEntityPipeBase;
+
 import java.lang.reflect.Field;
+
 import org.jetbrains.annotations.ApiStatus.ScheduledForRemoval;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,49 +22,31 @@ import supersymmetry.api.SusyLog;
 @Mixin(TileEntityPipeBase.class)
 public abstract class TileEntityPipeBaseMixin<
         PipeType extends Enum<PipeType> & IPipeType<NodeDataType>, NodeDataType>
-    extends NeighborCacheTileEntityBase implements IPipeTile<PipeType, NodeDataType> {
+        extends NeighborCacheTileEntityBase implements IPipeTile<PipeType, NodeDataType> {
+    @Shadow
+    private PipeType pipeType;
+    @Shadow
+    private int paintingColor;
+    @Shadow
+    private int connections;
+    @Shadow
+    private PipeCoverableImplementation coverableImplementation;
 
-  @Inject(method = "transferDataFrom", at = @At("HEAD"), cancellable = true, remap = false)
-  private void transferDataFrom(IPipeTile<PipeType, NodeDataType> tileEntity, CallbackInfo ci) {
 
-    // "this"
-    TileEntityPipeBase<PipeType, NodeDataType> self =
-        (TileEntityPipeBase<PipeType, NodeDataType>) (Object) this;
-    try {
-      // self.pipeType = tileEntity.getPipeType();
-      Field pipeTypeField = TileEntityPipeBase.class.getDeclaredField("pipeType");
-      pipeTypeField.setAccessible(true);
-      PipeType pipeType = tileEntity.getPipeType();
-      pipeTypeField.set(self, pipeType);
-      // self.paintingColor = tileEntity.getPaintingColor();
-      Field paintingColorField = TileEntityPipeBase.class.getDeclaredField("paintingColor");
-      paintingColorField.setAccessible(true);
-      int paintingColor = tileEntity.getPaintingColor();
-      paintingColorField.set(self, paintingColor);
-      // self.connections = tileEntity.getConnections();
-      Field connectionsField = TileEntityPipeBase.class.getDeclaredField("connections");
-      connectionsField.setAccessible(true);
-      int connections = tileEntity.getConnections();
-      connectionsField.set(self, connections);
-      // coverableImplementation
-      Field coverableImplementationField =
-          TileEntityPipeBase.class.getDeclaredField("coverableImplementation");
+    @Inject(method = "transferDataFrom", at = @At("HEAD"), cancellable = true, remap = false)
+    private void transferDataFrom(IPipeTile<PipeType, NodeDataType> tileEntity, CallbackInfo ci) {
+        // "this"
+        TileEntityPipeBase<PipeType, NodeDataType> self =
+                (TileEntityPipeBase<PipeType, NodeDataType>) (Object) this;
+        this.pipeType = tileEntity.getPipeType();
+        this.paintingColor = tileEntity.getPaintingColor();
+        this.connections = tileEntity.getConnections();
+        if (tileEntity instanceof SyncedTileEntityBase pipeBase) {
+            addPacketsFrom(pipeBase);
+        }
+        tileEntity.getCoverableImplementation().transferDataTo(this.coverableImplementation);
+        self.setFrameMaterial(tileEntity.getFrameMaterial());
 
-      coverableImplementationField.setAccessible(true);
-      PipeCoverableImplementation coverableImplementation =
-          (PipeCoverableImplementation) coverableImplementationField.get(self);
-
-      if (tileEntity instanceof SyncedTileEntityBase pipeBase) {
-        addPacketsFrom(pipeBase);
-      }
-
-      tileEntity.getCoverableImplementation().transferDataTo(coverableImplementation);
-      self.setFrameMaterial(tileEntity.getFrameMaterial());
-
-    } catch (NoSuchFieldException | IllegalAccessException e) {
-      SusyLog.logger.error("something blew up in the TileEntityPipeBaseMixin");
-      e.printStackTrace();
+        ci.cancel();
     }
-    ci.cancel();
-  }
 }
