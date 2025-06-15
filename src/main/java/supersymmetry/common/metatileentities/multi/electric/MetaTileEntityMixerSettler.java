2eb2d3c84a9847b613110972714170877f0bd728
bruberu <80226372+bruberu@users.noreply.github.com>
2025-06-15 00:36:54 -0500

    feat: finish structure (by greg, don't force me to make this expandable in 2D)

--- src/main/java/supersymmetry/common/metatileentities/multi/electric/MetaTileEntityMixerSettler.java
+++ src/main/java/supersymmetry/common/metatileentities/multi/electric/MetaTileEntityMixerSettler.java
@@ -8,14 +8,16 @@ import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
 import gregtech.api.recipes.RecipeMap;
+import gregtech.api.unification.material.Materials;
 import gregtech.client.renderer.ICubeRenderer;
 import gregtech.client.renderer.texture.Textures;
-import gregtech.common.blocks.BlockMetalCasing;
-import gregtech.common.blocks.MetaBlocks;
+import gregtech.common.blocks.*;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 import supersymmetry.api.recipes.SuSyRecipeMaps;
+import supersymmetry.common.blocks.BlockSuSyMultiblockCasing;
+import supersymmetry.common.blocks.SuSyBlocks;
 
 public class MetaTileEntityMixerSettler extends RecipeMapMultiblockController {
     public MetaTileEntityMixerSettler(ResourceLocation metaTileEntityId) {
@@ -25,15 +27,37 @@ public class MetaTileEntityMixerSettler extends RecipeMapMultiblockController {
     @Override
     protected @NotNull BlockPattern createStructurePattern() {
         return FactoryBlockPattern.start()
-                .aisle("CSC")
+                .aisle("CCCCCCCCC", "GGGGGGGGG", "GGGGGGGGG", "GGGGGGGGG", "CCCCCCCCC")
+                .aisle("CCCCCCCCC", "G P G P G", "GFG GFG G", "IFG PFG O", "CMCCCMCCC")
+                .aisle("CCCCCCCCC", "G GTG GTG", "G GTG GTG", "G GTG GTG", "CCCCCCCCC")
+                .aisle("CCCCCCCCC", "GDG GDG G", "GDG GDG G", "G G G G G", "CCCCCCCCC")
+                .aisle("CCCCCCCCC", "G G G G G", "G G G G G", "G G G G G", "CCCCCCCCC").setRepeatable(0, 8)
+                .aisle("CCCCCCCCC", "G GDG GDG", "G GDG GDG", "G G G G G", "CCCCCCCCC")
+                .aisle("CCCCCCCCC", "GTG GTG G", "GTG GTG G", "GTG GTG G", "CCCCCCCCC")
+                .aisle("CCCCCCCCC", "O G P G I", "G GFG GFG", "G PFG PFG", "CCCMCCCMC")
+                .aisle("CCCCSCCCC", "GGGGGGGGG", "GGGGSGGGG", "GGGGGGGGG", "CCCCSCCCC")
                 .where('S', selfPredicate())
-                .where('F', abilities(MultiblockAbility.IMPORT_FLUIDS))
-                .where('O', abilities(MultiblockAbility.EXPORT_FLUIDS))
-                .where('I', abilities(MultiblockAbility.IMPORT_ITEMS, MultiblockAbility.EXPORT_ITEMS))
-                .where('A', autoAbilities(true, true, false, false, false, false, false))
-                .where('C', states(getCasingState()).setMinGlobalLimited(30))
-
+                .where('I', states(getGlassState()).or(abilities(MultiblockAbility.IMPORT_FLUIDS)))
+                .where('O', states(getGlassState()).or(abilities(MultiblockAbility.EXPORT_FLUIDS)))
+                .where('T', states(SuSyBlocks.MULTIBLOCK_CASING.getState(BlockSuSyMultiblockCasing.CasingType.SIEVE_TRAY)))
+                .where('P', states(getPipeCasingState()))
+                //.where('B', abilities(MultiblockAbility.IMPORT_ITEMS, MultiblockAbility.EXPORT_ITEMS))
+                .where('D', states(getCasingState()))
+                .where('C', states(getCasingState()).setMinGlobalLimited(110).or(autoAbilities(true, true, true, true, false, false, false)))
+                .where('G', states(getGlassState()))
+                .where('M', states(MetaBlocks.TURBINE_CASING.getState(BlockTurbineCasing.TurbineCasingType.STAINLESS_STEEL_GEARBOX)))
+                .where('F', frames(Materials.StainlessSteel))
+                .where(' ', air())
                 .build();
+    }
+
+
+    protected static IBlockState getGlassState() {
+        return MetaBlocks.TRANSPARENT_CASING.getState(BlockGlassCasing.CasingType.TEMPERED_GLASS);
+    }
+
+    protected static IBlockState getPipeCasingState() {
+        return MetaBlocks.BOILER_CASING.getState(BlockBoilerCasing.BoilerCasingType.POLYTETRAFLUOROETHYLENE_PIPE);
     }
 
     public IBlockState getCasingState() {
