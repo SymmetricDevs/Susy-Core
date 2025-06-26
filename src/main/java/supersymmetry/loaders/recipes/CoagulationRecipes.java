961e0d26a0c8ac043426540bc619f0448f85d7e9
trainvoi <voitrainelephant@gmail.com>
2025-06-26 20:15:18 +0700

    Update CoagulationRecipes.java

--- src/main/java/supersymmetry/loaders/recipes/CoagulationRecipes.java
+++ src/main/java/supersymmetry/loaders/recipes/CoagulationRecipes.java
@@ -2,16 +2,9 @@ package supersymmetry.loaders.recipes;
 
 import supersymmetry.api.recipes.SuSyRecipeMaps;
 
-import static gregtech.api.unification.material.Materials.Air;
 
 public class CoagulationRecipes {
     public static void init() {
-        SuSyRecipeMaps.CLARIFIER.recipeBuilder()
-                .fluidInputs(Air.getFluid(100))
-                .fluidOutputs(Air.getFluid(100))
-                .duration(200)
-                .EUt(30)
-                .buildAndRegister();
     }
 
 }
