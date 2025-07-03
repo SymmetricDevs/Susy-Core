a89432b50c970dea88cf1234814edba96dbe3513
planetme <planetmeksp@gmail.com>
2025-07-02 22:34:26 -0700

    change MS recipemap yet again

--- src/main/java/supersymmetry/api/recipes/SuSyRecipeMaps.java
+++ src/main/java/supersymmetry/api/recipes/SuSyRecipeMaps.java
@@ -369,7 +369,7 @@ public class SuSyRecipeMaps {
     public static final RecipeMap<SimpleRecipeBuilder> MILLING_RECIPES = new RecipeMap<>("milling", 2, 1, 0, 0, new SimpleRecipeBuilder().EUt(VA[LV]), false)
             .setSound(GTSoundEvents.CUT);
 
-    public static final RecipeMap<MixerSettlerRecipeBuilder> MIXER_SETTLER_RECIPES = new RecipeMap<>("mixer_settler", 1, 1, 3, 2, new MixerSettlerRecipeBuilder().EUt(VA[LV]), false)
+    public static final RecipeMap<MixerSettlerRecipeBuilder> MIXER_SETTLER_RECIPES = new RecipeMap<>("mixer_settler", 1, 1, 3, 3, new MixerSettlerRecipeBuilder().EUt(VA[LV]), false)
             .setSound(GTSoundEvents.MIXER);
 
     public static final RecipeMap<DimensionRecipeBuilder> QUARRY_RECIPES = new RecipeMap<>("quarry", 1, 9, 0, 0, new DimensionRecipeBuilder(), false)
