70ed481f8a53d2eef8d7055b3c766e432f7a4ebb
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2025-07-25 16:14:04 +0800

    ok less warning less unhappy

--- src/api/java/zone/rong/loliasm/config/LoliConfig.java
+++ src/api/java/zone/rong/loliasm/config/LoliConfig.java
@@ -3,6 +3,6 @@ package zone.rong.loliasm.config;
 /// Adapted and minimized from
 /// [CensoredASM](https://github.com/LoliKingdom/LoliASM/blob/master/src/main/java/zone/rong/loliasm/config/LoliConfig.java)
 public class LoliConfig {
-    public static final LoliConfig instance = null;
+    public static final LoliConfig instance = new LoliConfig();
     public boolean onDemandAnimatedTextures;
 }
