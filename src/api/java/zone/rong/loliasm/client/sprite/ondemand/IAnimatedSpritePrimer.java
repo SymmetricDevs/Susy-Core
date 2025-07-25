985ed1a8bbac564a3f44e5d2dbff0ce8193d8157
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2025-07-25 16:27:05 +0800

    even less unhappy

--- src/api/java/zone/rong/loliasm/client/sprite/ondemand/IAnimatedSpritePrimer.java
+++ src/api/java/zone/rong/loliasm/client/sprite/ondemand/IAnimatedSpritePrimer.java
@@ -5,5 +5,5 @@ import net.minecraft.client.renderer.chunk.CompiledChunk;
 /// Adapted and minimized from
 /// [CensoredASM](https://github.com/LoliKingdom/LoliASM/blob/master/src/main/java/zone/rong/loliasm/client/sprite/ondemand/IAnimatedSpritePrimer.java)
 public interface IAnimatedSpritePrimer {
-    ThreadLocal<CompiledChunk> CURRENT_COMPILED_CHUNK = null;
+    ThreadLocal<CompiledChunk> CURRENT_COMPILED_CHUNK = ThreadLocal.withInitial(() -> CompiledChunk.DUMMY);
 }
