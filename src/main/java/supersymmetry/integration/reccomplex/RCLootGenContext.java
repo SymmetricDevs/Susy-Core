a313065cce65a8830c0a208d7cbc62a8f16499e2
MCTian_mi <3431493799@qq.com>
2025-07-27 10:10:28 +0800

    refactor

--- src/main/java/supersymmetry/integration/reccomplex/RCLootGenContext.java
+++
@@ -1,5 +0,0 @@
-package supersymmetry.integration.reccomplex;
-
-public class RCLootGenContext {
-    public static final ThreadLocal<Boolean> STRUCTURE_GEN_RUNNING = ThreadLocal.withInitial(() -> false);
-}
