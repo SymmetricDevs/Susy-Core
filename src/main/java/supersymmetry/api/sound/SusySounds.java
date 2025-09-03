66a619586cb54147d4831cc4571ae399705abfb3
grema <gremail12345@gmail.com>
2025-09-03 15:19:52 +0200

    added SFX for models list may expand, don't merge until I give the go-ahead

--- src/main/java/supersymmetry/api/sound/SusySounds.java
+++ src/main/java/supersymmetry/api/sound/SusySounds.java
@@ -12,6 +12,9 @@ public class SusySounds {
     public static SoundEvent ROCKET_LAUNCH;
     public static SoundEvent JET_ENGINE_LOOP;
     public static SoundEvent LOCKED_CRATE;
+    public static SoundEvent COMPLEX_ALARM;
+    public static SoundEvent METAL_DOOR_CLOSE;
+    public static SoundEvent METAL_DRAWER_OPEN;
 
     public static void registerSounds() {
         ROCKET_LOOP = registerSound("entity.rocket_loop");
@@ -19,6 +22,9 @@ public class SusySounds {
         ROCKET_LAUNCH = registerSound("entity.rocket_launch");
         JET_ENGINE_LOOP = registerSound("item.jet_wingpack_engine_active");
         LOCKED_CRATE = registerSound("block.locked_crate");
+        COMPLEX_ALARM = registerSound("block.complex_alarm");
+        METAL_DOOR_CLOSE = registerSound("block.metal_door_close");
+        METAL_DRAWER_OPEN = registerSound("block.metal_drawer_open");
     }
 
     private static SoundEvent registerSound(String soundNameIn) {
