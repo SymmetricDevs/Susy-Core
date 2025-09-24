33b714e2f558d43dd90c35871587143700daeab7
bruberu <80226372+bruberu@users.noreply.github.com>
2025-09-24 13:59:35 -0500

    feat: more thought

--- src/main/java/supersymmetry/common/metatileentities/multi/rocket/MetaTileEntityLandingPad.java
+++ src/main/java/supersymmetry/common/metatileentities/multi/rocket/MetaTileEntityLandingPad.java
@@ -3,6 +3,7 @@ package supersymmetry.common.metatileentities.multi.rocket;
 import codechicken.lib.render.CCRenderState;
 import codechicken.lib.render.pipeline.IVertexOperation;
 import codechicken.lib.vec.Matrix4;
+import gregtech.api.capability.impl.ItemHandlerList;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.IMultiblockPart;
@@ -10,8 +11,11 @@ import gregtech.api.metatileentity.multiblock.MultiblockAbility;
 import gregtech.api.metatileentity.multiblock.MultiblockWithDisplayBase;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.pattern.PatternMatchContext;
 import gregtech.api.pattern.TraceabilityPredicate;
 import gregtech.api.recipes.Recipe;
+import gregtech.api.util.GTTransferUtils;
+import gregtech.api.util.GTUtility;
 import gregtech.client.renderer.ICubeRenderer;
 import gregtech.client.renderer.texture.Textures;
 import gregtech.common.blocks.BlockMetalCasing;
@@ -19,10 +23,16 @@ import gregtech.common.blocks.MetaBlocks;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.items.IItemHandlerModifiable;
 import org.jetbrains.annotations.NotNull;
 import supersymmetry.common.blocks.BlockSuSyMultiblockCasing;
 import supersymmetry.common.blocks.SuSyBlocks;
+import supersymmetry.common.entities.EntityDrone;
+import supersymmetry.common.entities.EntityDropPod;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayDeque;
@@ -33,6 +43,9 @@ public class MetaTileEntityLandingPad extends MultiblockWithDisplayBase {
 
     private Queue<LandingData> incoming;
     private LandingData current;
+    private EntityDropPod lander = null;
+    private AxisAlignedBB landingAreaBB;
+    protected IItemHandlerModifiable outputInventory;
 
     public MetaTileEntityLandingPad(ResourceLocation metaTileEntityId) {
         super(metaTileEntityId);
@@ -42,14 +55,31 @@ public class MetaTileEntityLandingPad extends MultiblockWithDisplayBase {
     public MetaTileEntity createMetaTileEntity(IGregTechTileEntity iGregTechTileEntity) {
         return new MetaTileEntityLandingPad(metaTileEntityId);
     }
+    @Override
+    protected void formStructure(PatternMatchContext context) {
+        super.formStructure(context);
+        initializeAbilities();
+    }
+
+    protected void initializeAbilities() {
+        this.outputInventory = new ItemHandlerList(getAbilities(MultiblockAbility.EXPORT_ITEMS));
+    }
 
     @Override
     protected void updateFormedValid() {
-        if (this.incoming.peek().scheduledTotalWorldTime > this.getWorld().getTotalWorldTime()) {
-
-        }
-        if (this.getWorld().isRemote) {
-
+        if (current == null) {
+            if (this.incoming.peek() == null) {
+                return;
+            }
+            if (this.incoming.peek().scheduledTotalWorldTime > this.getWorld().getTotalWorldTime()) {
+                this.current = this.incoming.poll();
+                this.lander = new EntityDropPod(this.getWorld(), this.getPos().offset(this.getFrontFacing(), 8).offset(EnumFacing.UP, 255));
+                this.getWorld().spawnEntity(this.lander);
+            }
+        } else if (this.hasLanderArrived()) {
+            this.lander.setDead();
+            GTTransferUtils.addItemsToItemHandler(this.outputInventory, false, this.current.recipe);
+            this.current = null;
         }
     }
 
@@ -162,6 +192,32 @@ public class MetaTileEntityLandingPad extends MultiblockWithDisplayBase {
             i++;
         }
     }
+
+    public EntityDropPod getLander() {
+        return this.lander;
+    }
+
+    public boolean hasLanderArrived() {
+        if (getLander() != null && !getLander().isDead) {
+            for (EntityDropPod entity : this.getWorld().getEntitiesWithinAABB(EntityDropPod.class, this.landingAreaBB)) {
+                if (entity == getLander()) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean allowsExtendedFacing() {
+        return false;
+    }
+
+    public void setStructureAABB() {
+        BlockPos spot = new BlockPos.MutableBlockPos(this.getPos()).move(this.getFrontFacing().getOpposite(), 8).move(EnumFacing.UP, 1);
+        this.landingAreaBB = new AxisAlignedBB(spot);
+    }
+
 
     private static class LandingData {
         private List<ItemStack> recipe;
---
+++ src/main/java/supersymmetry/common/rocketry/instruments/InstrumentRobotArm.java
@@ -0,0 +1,12 @@
+package supersymmetry.common.rocketry.instruments;
+
+import supersymmetry.api.rocketry.components.Instrument;
+import supersymmetry.common.entities.EntityRocket;
+
+public class InstrumentRobotArm implements Instrument {
+    @Override
+    public void act(int count, EntityRocket rocket) {
+        rocket.
+        rocket.getEntityData().getCompoundTag("rocket").getCompoundTag("instruments").setInteger("robotArm", count);
+    }
+}
