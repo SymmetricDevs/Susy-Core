9dc65f84fa82e8a6462a213d806f9308d1e8a039
aliu <andrew.yc.liu@gmail.com>
2026-01-30 20:36:39 -0800

    spotless happy

--- src/main/java/supersymmetry/common/metatileentities/multi/primitive/MetaTileEntityCupolaFurnace.java
+++ src/main/java/supersymmetry/common/metatileentities/multi/primitive/MetaTileEntityCupolaFurnace.java
@@ -1,27 +1,9 @@
 package supersymmetry.common.metatileentities.multi.primitive;
 
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.SoundEvents;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.*;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-import org.apache.commons.lang3.ArrayUtils;
 import org.jetbrains.annotations.NotNull;
 
-import com.codetaylor.mc.pyrotech.modules.core.ModuleCore;
-
-import codechicken.lib.render.CCRenderState;
-import codechicken.lib.render.pipeline.IVertexOperation;
-import codechicken.lib.vec.Matrix4;
-import gregtech.api.GTValues;
-import gregtech.api.capability.impl.ItemHandlerList;
-import gregtech.api.gui.ModularUI;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.IMultiblockPart;
@@ -29,21 +11,11 @@ import gregtech.api.metatileentity.multiblock.MultiblockAbility;
 import gregtech.api.metatileentity.multiblock.RecipeMapPrimitiveMultiblockController;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
-import gregtech.api.pattern.PatternMatchContext;
-import gregtech.api.pattern.TraceabilityPredicate;
-import gregtech.api.util.GTUtility;
-import gregtech.client.particle.VanillaParticleEffects;
-import gregtech.client.renderer.CubeRendererState;
 import gregtech.client.renderer.ICubeRenderer;
-import gregtech.client.renderer.cclop.ColourOperation;
-import gregtech.client.renderer.cclop.LightMapOperation;
 import gregtech.client.renderer.texture.Textures;
-import gregtech.common.ConfigHolder;
 import gregtech.common.blocks.BlockMetalCasing;
 import gregtech.common.blocks.MetaBlocks;
-import supersymmetry.api.metatileentity.multiblock.SuSyMultiblockAbilities;
 import supersymmetry.api.recipes.SuSyRecipeMaps;
-import supersymmetry.client.renderer.textures.SusyTextures;
 
 public class MetaTileEntityCupolaFurnace extends RecipeMapPrimitiveMultiblockController {
 
@@ -59,15 +31,15 @@ public class MetaTileEntityCupolaFurnace extends RecipeMapPrimitiveMultiblockCon
     @Override
     protected @NotNull BlockPattern createStructurePattern() {
         return FactoryBlockPattern.start()
-            .aisle("CCC", "CCC", "CCC", "CCC")
-            .aisle("CCC", "CSC", "CCC", "CCC")
-            .aisle("CCC", "CAC", "CAC", "CAC")
-            .aisle("CCC", "CAC", "CAC", "CAC")
-            .where('C', states(MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.BRONZE_BRICKS))
-            .or(abilities(MultiblockAbility.EXPORT_ITEMS, MultiblockAbility.IMPORT_ITEMS)))
-            .where('A', air())
-            .where('S', selfPredicate())
-            .build();
+                .aisle("CCC", "CCC", "CCC", "CCC")
+                .aisle("CCC", "CSC", "CCC", "CCC")
+                .aisle("CCC", "CAC", "CAC", "CAC")
+                .aisle("CCC", "CAC", "CAC", "CAC")
+                .where('C', states(MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.BRONZE_BRICKS))
+                        .or(abilities(MultiblockAbility.EXPORT_ITEMS, MultiblockAbility.IMPORT_ITEMS)))
+                .where('A', air())
+                .where('S', selfPredicate())
+                .build();
     }
 
     @Override
