e41b1701be52dab4b9d0d8bd68b6ba3a288203f9
aliu <andrew.yc.liu@gmail.com>
2026-02-04 17:46:55 -0800

    hopefully have parallel by height

--- src/main/java/supersymmetry/common/metatileentities/multi/primitive/MetaTileEntityCupolaFurnace.java
+++ src/main/java/supersymmetry/common/metatileentities/multi/primitive/MetaTileEntityCupolaFurnace.java
@@ -1,5 +1,7 @@
 package supersymmetry.common.metatileentities.multi.primitive;
 
+import static gregtech.api.util.RelativeDirection.*;
+
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.*;
 import net.minecraftforge.fml.relauncher.Side;
@@ -7,16 +9,23 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 import org.jetbrains.annotations.NotNull;
 
+import codechicken.lib.render.CCRenderState;
+import codechicken.lib.render.pipeline.IVertexOperation;
+import codechicken.lib.vec.Matrix4;
 import gregtech.api.capability.impl.ItemHandlerList;
+import gregtech.api.capability.impl.PrimitiveRecipeLogic;
 import gregtech.api.gui.ModularUI;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.IMultiblockPart;
 import gregtech.api.metatileentity.multiblock.MultiblockAbility;
+import gregtech.api.metatileentity.multiblock.ParallelLogicType;
 import gregtech.api.metatileentity.multiblock.RecipeMapPrimitiveMultiblockController;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
 import gregtech.api.pattern.PatternMatchContext;
+import gregtech.api.pattern.TraceabilityPredicate;
+import gregtech.api.recipes.RecipeMap;
 import gregtech.api.unification.material.Materials;
 import gregtech.client.renderer.ICubeRenderer;
 import gregtech.client.renderer.texture.Textures;
@@ -27,6 +36,8 @@ import supersymmetry.client.renderer.textures.SusyTextures;
 
 public class MetaTileEntityCupolaFurnace extends RecipeMapPrimitiveMultiblockController {
 
+    public int size;
+
     public MetaTileEntityCupolaFurnace(ResourceLocation metaTileEntityId) {
         super(metaTileEntityId, SuSyRecipeMaps.CUPOLA_FURNACE);
     }
@@ -36,17 +47,28 @@ public class MetaTileEntityCupolaFurnace extends RecipeMapPrimitiveMultiblockCon
         return new MetaTileEntityCupolaFurnace(this.metaTileEntityId);
     }
 
+    public static TraceabilityPredicate isIndicatorPredicate() {
+        return new TraceabilityPredicate((blockWorldState) -> {
+            if (air().test(blockWorldState)) {
+                blockWorldState.getMatchContext().increment("tankLength", 1);
+                return true;
+            } else
+                return false;
+        });
+    }
+
     @Override
     protected @NotNull BlockPattern createStructurePattern() {
-        return FactoryBlockPattern.start()
-                .aisle("F F", "CCC", "CCC", "CCC")
-                .aisle(" O ", "CAC", "CAC", "CAC")
-                .aisle("F F", "CSC", "CCC", "CCC")
+        return FactoryBlockPattern.start(RIGHT, BACK, UP)
+                .aisle("FAF", "AOA", "FAF")
+                .aisle("CCC", "CHC", "CSC")
+                .aisle("CCC", "CHC", "CCC").setRepeatable(2, 8)
                 .where('C',
                         states(MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.PRIMITIVE_BRICKS))
                                 .or(abilities(MultiblockAbility.IMPORT_ITEMS).setMinGlobalLimited(1)
                                         .setMaxGlobalLimited(4)))
                 .where('A', air())
+                .where('H', isIndicatorPredicate())
                 .where('S', selfPredicate())
                 .where(' ', air())
                 .where('O', abilities(MultiblockAbility.EXPORT_ITEMS))
@@ -55,15 +77,15 @@ public class MetaTileEntityCupolaFurnace extends RecipeMapPrimitiveMultiblockCon
     }
 
     @Override
-    protected void initializeAbilities() {
-        this.importItems = new ItemHandlerList(getAbilities(MultiblockAbility.IMPORT_ITEMS));
-        this.exportItems = new ItemHandlerList(getAbilities(MultiblockAbility.EXPORT_ITEMS));
-    }
-
-    @Override
     protected void formStructure(PatternMatchContext context) {
         super.formStructure(context);
         this.initializeAbilities();
+    }
+
+    @Override
+    protected void initializeAbilities() {
+        this.importItems = new ItemHandlerList(getAbilities(MultiblockAbility.IMPORT_ITEMS));
+        this.exportItems = new ItemHandlerList(getAbilities(MultiblockAbility.EXPORT_ITEMS));
     }
 
     @Override
@@ -74,6 +96,13 @@ public class MetaTileEntityCupolaFurnace extends RecipeMapPrimitiveMultiblockCon
     @SideOnly(Side.CLIENT)
     protected @NotNull ICubeRenderer getFrontOverlay() {
         return SusyTextures.CUPOLA_FURNACE_OVERLAY;
+    }
+
+    @Override
+    public void renderMetaTileEntity(CCRenderState renderState, Matrix4 translation, IVertexOperation[] pipeline) {
+        super.renderMetaTileEntity(renderState, translation, pipeline);
+        this.getFrontOverlay().renderOrientedState(renderState, translation, pipeline, getFrontFacing(),
+                recipeMapWorkable.isActive(), recipeMapWorkable.isWorkingEnabled());
     }
 
     @Override
@@ -94,5 +123,25 @@ public class MetaTileEntityCupolaFurnace extends RecipeMapPrimitiveMultiblockCon
     @Override
     public void update() {
         super.update();
+    }
+
+    public class CupolaFurnaceLogic extends PrimitiveRecipeLogic {
+
+        public CupolaFurnaceLogic(RecipeMapPrimitiveMultiblockController tileEntity,
+                                  RecipeMap<?> recipeMap) {
+            super(tileEntity, recipeMap);
+        }
+
+        public int getParallelLimit() {
+            return ((MetaTileEntityCupolaFurnace) this.getMetaTileEntity()).size;
+        }
+
+        protected long getMaxParallelVoltage() {
+            return 2147432767L;
+        }
+
+        public @NotNull ParallelLogicType getParallelLogicType() {
+            return ParallelLogicType.MULTIPLY;
+        }
     }
 }
