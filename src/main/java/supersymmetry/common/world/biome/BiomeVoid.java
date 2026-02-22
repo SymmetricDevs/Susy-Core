6e367a7439cd5979b1911a97593466201dff726b
Martin <tsdrppy@gmail.com>
2026-02-21 22:52:33 -0500

    add void biome (for in space)

---
+++ src/main/java/supersymmetry/common/world/biome/BiomeVoid.java
@@ -0,0 +1,35 @@
+package supersymmetry.common.world.biome;
+
+import gregtech.common.blocks.MetaBlocks;
+import gregtech.common.blocks.StoneVariantBlock;
+import net.minecraft.entity.EnumCreatureType;
+import supersymmetry.api.space.dimension.biome.SpaceBiome;
+import supersymmetry.common.blocks.BlockRegolith;
+import supersymmetry.common.blocks.SuSyBlocks;
+
+import javax.annotation.Nonnull;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BiomeVoid extends SpaceBiome {
+
+        public BiomeVoid(BiomeProperties properties) {
+            super(properties);
+
+            this.topBlock = SuSyBlocks.REGOLITH.getState(BlockRegolith.BlockRegolithType.LOWLAND);
+            this.fillerBlock = MetaBlocks.STONE_BLOCKS.get(StoneVariantBlock.StoneVariant.SMOOTH)
+                    .getState(StoneVariantBlock.StoneType.BASALT);
+        }
+
+        @Override
+        @Nonnull
+        public List<SpawnListEntry> getSpawnableList(EnumCreatureType type) {
+            return new LinkedList<>();
+        }
+
+        @Override
+        public float getSpawningChance() {
+            return 0f; // Nothing spawns
+        }
+
+}
