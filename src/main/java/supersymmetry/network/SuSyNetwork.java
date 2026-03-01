c010e994e6bee0ace027dc4f74ac9b47ecf550c2
Martin <tsdrppy@gmail.com>
2026-03-01 09:05:02 -0500

    Move missing-mod checks to server/network

---
+++ src/main/java/supersymmetry/network/SuSyNetwork.java
@@ -0,0 +1,15 @@
+package supersymmetry.network;
+
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.relauncher.Side;
+
+import supersymmetry.Supersymmetry;
+
+public class SuSyNetwork {
+    public static final SimpleNetworkWrapper CHANNEL = NetworkRegistry.INSTANCE.newSimpleChannel(Supersymmetry.MODID + ":warnings");
+
+    public static void init() {
+        CHANNEL.registerMessage(MissingModsMessage.Handler.class, MissingModsMessage.class, 0, Side.CLIENT);
+    }
+}
