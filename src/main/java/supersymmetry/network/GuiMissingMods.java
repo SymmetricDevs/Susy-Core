f7c4335a40d1900547056b6ece3189847d7a6e7e
Martin <tsdrppy@gmail.com>
2026-03-01 09:05:02 -0500

    Move missing-mod checks to server/network

---
+++ src/main/java/supersymmetry/network/GuiMissingMods.java
@@ -0,0 +1,60 @@
+    package supersymmetry.network;
+
+    import net.minecraft.client.gui.GuiButton;
+    import net.minecraft.client.gui.GuiScreen;
+    import net.minecraft.util.text.TextFormatting;
+
+    import java.io.IOException;
+    import java.util.List;
+
+    public class GuiMissingMods extends GuiScreen {
+
+        private final List<String> missingModNames;
+
+        public GuiMissingMods(List<String> missingModNames) {
+            this.missingModNames = missingModNames;
+        }
+
+        @Override
+        public void initGui() {
+            this.buttonList.add(new GuiButton(0, this.width / 2 - 100, this.height - 38, "I Understand"));
+        }
+
+        @Override
+        public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+            this.drawDefaultBackground();
+
+            this.drawCenteredString(fontRenderer, TextFormatting.RED + "⚠ Server Missing Required Mods ⚠",
+                    this.width / 2, 20, 0xFFFFFF);
+
+            this.drawCenteredString(fontRenderer, "This SUSY server is missing mods. Expect broken or missing content:",
+                    this.width / 2, 36, 0xAAAAAA);
+
+            drawRect(this.width / 2 - 120, 46, this.width / 2 + 120, 47, 0xFF555555);
+
+            int y = 54;
+            for (String name : missingModNames) {
+                this.drawCenteredString(fontRenderer, TextFormatting.YELLOW + "• " + TextFormatting.WHITE + name,
+                        this.width / 2, y, 0xFFFFFF);
+                y += 12;
+            }
+
+            this.drawCenteredString(fontRenderer,
+                    TextFormatting.GRAY + "You may continue playing, but some features will not work correctly.",
+                    this.width / 2, this.height - 52, 0xFFFFFF);
+
+            super.drawScreen(mouseX, mouseY, partialTicks);
+        }
+
+        @Override
+        protected void actionPerformed(GuiButton button) throws IOException {
+            if (button.id == 0) {
+                this.mc.displayGuiScreen(null); // dismiss, return to game
+            }
+        }
+
+        @Override
+        public boolean doesGuiPauseGame() {
+            return true;
+        }
+    }
---
+++ src/main/java/supersymmetry/network/ServerModHandler.java
@@ -0,0 +1,39 @@
+package supersymmetry.network;
+
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent;
+
+import supersymmetry.Supersymmetry;
+
+@Mod.EventBusSubscriber(modid = Supersymmetry.MODID)
+public class ServerModHandler {
+
+    private static final String[][] REQUIRED_MODS = {
+            { "srparasites",   "Scape and Run Parasites"     },
+            { "openglasses",   "OpenGlasses"                 },
+            { "rendertoolkit", "Commons0815 (renderToolkit)"  },
+            { "guitoolkit",    "Commons0815 (guiToolkit)"     }
+    };
+
+    @SubscribeEvent
+    public static void onPlayerConnecting(FMLNetworkEvent.ServerConnectionFromClientEvent event) {
+        StringBuilder missing = new StringBuilder();
+        for (String[] mod : REQUIRED_MODS) {
+            if (!Loader.isModLoaded(mod[0])) {
+                if (missing.length() > 0) missing.append(", ");
+                missing.append(mod[1]);
+            }
+        }
+
+        if (missing.length() > 0) {
+            event.getManager().closeChannel(new TextComponentString(
+                    TextFormatting.RED + "[Supersymmetry] This server is missing required mods and cannot accept connections.\n" +
+                            TextFormatting.YELLOW + "Missing: " + missing
+            ));
+        }
+    }
+}
