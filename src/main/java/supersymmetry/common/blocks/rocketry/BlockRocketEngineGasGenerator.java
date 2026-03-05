5e6e63347bfe07605a9c3476e0c290359ff8c748
Engineerboy2998 <immanueltomczuk@gmail.com>
2026-03-05 22:46:57 +0000

    Fixed gas generator block issues

--- src/main/java/supersymmetry/common/blocks/rocketry/BlockRocketEngineGasGenerator.java
+++ src/main/java/supersymmetry/common/blocks/rocketry/BlockRocketEngineGasGenerator.java
@@ -1,13 +1,15 @@
 package supersymmetry.common.blocks.rocketry;
 
-import gregtech.api.block.IStateHarvestLevel;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.IStringSerializable;
+
+import gregtech.api.block.IStateHarvestLevel;
 import supersymmetry.api.blocks.VariantHorizontalRotatableBlock;
 
-public class BlockRocketEngineGasGenerator extends VariantHorizontalRotatableBlock<BlockRocketEngineGasGenerator.GasGeneratorType> {
+public class BlockRocketEngineGasGenerator extends
+                                           VariantHorizontalRotatableBlock<BlockRocketEngineGasGenerator.GasGeneratorType> {
 
     public BlockRocketEngineGasGenerator() {
         super(Material.IRON);
