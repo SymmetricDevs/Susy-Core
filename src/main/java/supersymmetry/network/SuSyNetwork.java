af8e7b9136a867f9d5930ca95e854dc012a0d90d
Martin <tsdrppy@gmail.com>
2026-03-05 17:15:48 -0500

    fix: cant connect to server at all

--- src/main/java/supersymmetry/network/SuSyNetwork.java
+++ src/main/java/supersymmetry/network/SuSyNetwork.java
@@ -2,7 +2,6 @@ package supersymmetry.network;
 
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
-import net.minecraftforge.fml.relauncher.Side;
 
 import supersymmetry.Supersymmetry;
 
@@ -11,7 +10,5 @@ public class SuSyNetwork {
     public static final SimpleNetworkWrapper CHANNEL = NetworkRegistry.INSTANCE
             .newSimpleChannel(Supersymmetry.MODID + ":warnings");
 
-    public static void init() {
-        CHANNEL.registerMessage(MissingModsMessage.Handler.class, MissingModsMessage.class, 0, Side.CLIENT);
-    }
+    public static void init() {}
 }
