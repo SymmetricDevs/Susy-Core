f8909652574628abf8eb3d9321a8e0c3aee8039a
aliu <andrew.yc.liu@gmail.com>
2026-03-07 22:08:46 -0800

    error was with order of loading

--- src/main/java/supersymmetry/mixins/dimstack/DimensionalPipeMixin.java
+++ src/main/java/supersymmetry/mixins/dimstack/DimensionalPipeMixin.java
@@ -1,14 +1,11 @@
 package supersymmetry.mixins.dimstack;
 
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumFacing;
-import net.minecraftforge.common.capabilities.Capability;
+import net.minecraft.nbt.NBTTagCompound;
 
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 
 import cd4017be.dimstack.tileentity.DimensionalPipe;
 import cd4017be.lib.TickRegistry.IUpdatable;
@@ -19,33 +16,24 @@ import cd4017be.lib.tileentity.BaseTileEntity;
 @Mixin(value = DimensionalPipe.class, remap = false)
 interface DimensionalPipeMixinAccessor {
 
-    @Accessor
-    EnumFacing getSide();
-
-    @Accessor
-    DimensionalPipe getLinkTile();
-
-    @Final
     @Invoker
-    TileEntity callGetCon();
+    void callLink(DimensionalPipe tile);
+
+    @Invoker
+    void callProcess();
 }
 
 @Mixin(value = DimensionalPipe.class, remap = false)
 public abstract class DimensionalPipeMixin extends BaseTileEntity
                                            implements INeighborAwareTile, IUpdatable, IInteractiveTile {
 
-    /**
-     * @Author aliu-here
-     * @reason testing
-     */
-    @Overwrite
-    public boolean hasCapability(Capability<?> cap, EnumFacing facing) {
-        if (facing != ((DimensionalPipeMixinAccessor) this).getSide()) return false;
-        DimensionalPipe linkTile = ((DimensionalPipeMixinAccessor) this).getLinkTile();
-        if (linkTile != null && !linkTile.invalid()) {
-            return ((DimensionalPipeMixinAccessor) linkTile).callGetCon().hasCapability(cap,
-                    ((DimensionalPipeMixinAccessor) this).getSide());
-        }
-        return false;
+    @Inject(method = "link", at = @At("HEAD"))
+    void linkOther(DimensionalPipe tile) {
+        ((DimensionalPipeMixinAccessor) tile).callLink(((DimensionalPipe) (Object) this));
+    }
+
+    @Inject(method = "loadState", at = @At("TAIL"))
+    void tryLink(NBTTagCompound nbt, int mode) {
+        ((DimensionalPipeMixinAccessor) this).callProcess();
     }
 }
