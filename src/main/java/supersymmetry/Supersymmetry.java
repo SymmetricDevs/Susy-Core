6c01ea904664ce720471fae578309fc982932357
Martin <196868237+ThisMightBeNoah@users.noreply.github.com>
2026-03-14 19:05:55 -0400

    use said shaders

--- src/main/java/supersymmetry/Supersymmetry.java
+++ src/main/java/supersymmetry/Supersymmetry.java
@@ -18,6 +18,7 @@ import li.cil.oc.api.Items;
 import supercritical.common.SCConfigHolder;
 import supersymmetry.api.capability.SuSyCapabilities;
 import supersymmetry.api.sound.SusySounds;
+import supersymmetry.client.shaders.ShaderManager;
 import supersymmetry.common.CommonProxy;
 import supersymmetry.common.SusyMetaEntities;
 import supersymmetry.common.blocks.SuSyBlocks;
@@ -90,6 +91,9 @@ public class Supersymmetry {
     @Mod.EventHandler
     public void onInit(@NotNull FMLInitializationEvent event) {
         proxy.load();
+        if (event.getSide().isClient()) {
+            ShaderManager.initShaders();
+        }
         SuSyCoverBehaviors.init();
 
         Driver.add(new supersymmetry.integration.opencomputers.DriverSpeaker());
