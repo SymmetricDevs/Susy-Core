4e932da9f6d2e6de0cdb409cf95a53ae219b0291
aliu <andrew.yc.liu@gmail.com>
2026-03-15 03:15:33 -0700

    what

--- src/main/java/supersymmetry/mixins/gregtech/WorldItemPipeNetMixin.java
+++ src/main/java/supersymmetry/mixins/gregtech/WorldItemPipeNetMixin.java
@@ -1,7 +1,5 @@
 package supersymmetry.mixins.gregtech;
 
-import java.util.List;
-
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,29 +9,14 @@ import gregtech.api.pipenet.WorldPipeNet;
 import gregtech.common.pipelike.itempipe.net.ItemPipeNet;
 import gregtech.common.pipelike.itempipe.net.WorldItemPipeNet;
 
-class WorldItemPipeNetAccessor extends WorldItemPipeNet {
-
-    WorldItemPipeNetAccessor(String name) { // here because java forces me to put it
-        super(name);
-    }
-
-    List<ItemPipeNet> getPipeNets() {
-        return this.pipeNets;
-    }
-
-    void callAddPipeNet(ItemPipeNet net) {
-        this.addPipeNet(net);
-    }
-}
-
 @Mixin(value = WorldItemPipeNet.class, remap = false)
 public abstract class WorldItemPipeNetMixin {
 
-    @Overwrite
     /**
      * @author aliu-here
      * @reason couldn't figure out a way to fix the original through a regular mixin; i just copied source w/ added line
      */
+    @Overwrite
     public static WorldItemPipeNet getWorldPipeNet(World world) {
         String DATA_ID = WorldPipeNet.getDataID("gregtech.item_pipe_net", world);
         WorldItemPipeNet netWorldData = (WorldItemPipeNet) world.loadData(WorldItemPipeNet.class, DATA_ID);
@@ -41,15 +24,15 @@ public abstract class WorldItemPipeNetMixin {
             // try loading from the old one; going to save to the new one later
             netWorldData = (WorldItemPipeNet) world.loadData(WorldItemPipeNet.class, "gregtech.item_pipe_net");
 
-            WorldItemPipeNet newNetWorldData = new WorldItemPipeNet(DATA_ID);
-
-            for (ItemPipeNet net : ((WorldItemPipeNetAccessor) netWorldData).getPipeNets()) {
-                ((WorldItemPipeNetAccessor) newNetWorldData).callAddPipeNet(net);
-            }
-
             if (netWorldData == null) {
                 netWorldData = new WorldItemPipeNet(DATA_ID);
                 world.setData(DATA_ID, netWorldData);
+            } else {
+                WorldItemPipeNet newNetWorldData = new WorldItemPipeNet(DATA_ID); // copy stuff over
+                for (ItemPipeNet net : ((WorldPipeNetAccessor) netWorldData).getPipeNets()) {
+                    ((WorldPipeNetAccessor) newNetWorldData).callAddPipeNet(net);
+                }
+                netWorldData = newNetWorldData;
             }
         }
         ((WorldPipeNetAccessor) netWorldData).callSetWorldAndInit(world);
--- src/main/java/supersymmetry/mixins/gregtech/WorldPipeNetAccessor.java
+++ src/main/java/supersymmetry/mixins/gregtech/WorldPipeNetAccessor.java
@@ -1,15 +1,26 @@
 package supersymmetry.mixins.gregtech;
 
+import java.util.List;
+
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
+import gregtech.api.pipenet.PipeNet;
 import gregtech.api.pipenet.WorldPipeNet;
+import gregtech.common.pipelike.itempipe.net.ItemPipeNet;
 
 @Mixin(value = WorldPipeNet.class, remap = false)
 public interface WorldPipeNetAccessor {
 
     @Invoker
     void callSetWorldAndInit(World world);
+
+    @Accessor
+    List<ItemPipeNet> getPipeNets();
+
+    @Invoker("addPipeNetSilently")
+    void callAddPipeNet(PipeNet pipeNet);
 }
