64a4f79838c8608b70b111718208c99c2a3da0ef
aliu <andrew.yc.liu@gmail.com>
2026-03-14 18:48:19 -0700

    extract accessor bc apparently i have to do that

--- src/main/java/supersymmetry/mixins/dimstack/DimensionalPipeMixin.java
+++ src/main/java/supersymmetry/mixins/dimstack/DimensionalPipeMixin.java
@@ -1,5 +1,7 @@
 package supersymmetry.mixins.dimstack;
 
+import supersymmetry.mixins.dimstack.DimensionalPipeMixinAccessor;
+
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 
@@ -7,6 +9,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import cd4017be.dimstack.tileentity.DimensionalPipe;
 import cd4017be.lib.TickRegistry.IUpdatable;
@@ -15,19 +18,12 @@ import cd4017be.lib.block.AdvancedBlock.INeighborAwareTile;
 import cd4017be.lib.tileentity.BaseTileEntity;
 
 @Mixin(value = DimensionalPipe.class, remap = false)
-interface DimensionalPipeMixinAccessor {
-
-    @Accessor
-    EnumFacing getSide();
-}
-
-@Mixin(value = DimensionalPipe.class, remap = false)
 public abstract class DimensionalPipeMixin extends BaseTileEntity
                                            implements INeighborAwareTile, IUpdatable, IInteractiveTile {
 
     @Inject(method = "link", at = @At("TAIL"))
-    void linkOther(DimensionalPipe tile) {
-        BlockPos adjTEPos = pos.offset(((DimensionalPipeMixinAccessor) this).getSide());
-        world.getTileEntity(adjTEPos).getBlockType().onNeighborChange(world, adjTEPos, pos);
+    void link(DimensionalPipe tile, CallbackInfo ci) {
+        BlockPos neighbor = pos.offset(((DimensionalPipeMixinAccessor) this).getSide());
+        world.getTileEntity(neighbor).getBlockType().onNeighborChange(world, pos, neighbor);
     }
 }
