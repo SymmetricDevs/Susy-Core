7218b299c46bc22201642b043afe54397a4034ba
aliu <andrew.yc.liu@gmail.com>
2026-04-07 19:01:38 -0700

    add stuff

--- src/main/java/supersymmetry/common/metatileentities/single/electric/MetaTileEntityScreenPrinter.java
+++ src/main/java/supersymmetry/common/metatileentities/single/electric/MetaTileEntityScreenPrinter.java
@@ -1,43 +1,39 @@
 package supersymmetry.common.metatileentities.single.electric;
 
-import static gregtech.api.recipes.logic.OverclockingLogic.standardOverclockingLogic;
-
-import java.util.List;
 import java.util.function.Supplier;
 
-import net.minecraft.client.resources.I18n;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.World;
-
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import gregtech.api.GTValues;
 import gregtech.api.capability.IEnergyContainer;
-import gregtech.api.capability.impl.EnergyContainerHandler;
 import gregtech.api.capability.impl.RecipeLogicEnergy;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.SimpleMachineMetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.recipes.RecipeMap;
-import gregtech.api.recipes.recipeproperties.IRecipePropertyStorage;
-import gregtech.api.util.GTUtility;
 import supersymmetry.api.recipes.SuSyRecipeMaps;
 import supersymmetry.client.renderer.textures.SusyTextures;
 
 public class MetaTileEntityScreenPrinter extends SimpleMachineMetaTileEntity {
+
     public MetaTileEntityScreenPrinter(ResourceLocation metaTileEntityId) {
-        super(metaTileEntityId, SuSyRecipeMaps.SCREEN_PRINTER, SusyTextures.SCREEN_PRINTER, 1, true);
+        super(metaTileEntityId, SuSyRecipeMaps.SCREEN_PRINTER, SusyTextures.SCREEN_PRINTER_OVERLAY, 1, true);
     }
 
     @Override
     public MetaTileEntity createMetaTileEntity(IGregTechTileEntity tileEntity) {
-        return new MetaTileEntityBathCondenser(metaTileEntityId);
+        return new MetaTileEntityScreenPrinter(metaTileEntityId);
+    }
+
+    @Override
+    protected RecipeLogicEnergy createWorkable(RecipeMap<?> recipeMap) {
+        return new ScreenPrinterRecipeLogic(this, recipeMap, () -> energyContainer);
     }
 
     public class ScreenPrinterRecipeLogic extends RecipeLogicEnergy {
-        public ScreenPrinterRecipeLogic(MetaTileEntity tileEntity, RecipeMap<?> recipeMap, Supplier<IEnergyContainer> energyContainer) {
+
+        public ScreenPrinterRecipeLogic(MetaTileEntity tileEntity, RecipeMap<?> recipeMap,
+                                        Supplier<IEnergyContainer> energyContainer) {
             super(tileEntity, recipeMap, energyContainer);
         }
 
