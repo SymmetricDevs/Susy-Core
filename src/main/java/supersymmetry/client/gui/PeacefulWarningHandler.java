4695edb9f44b056d545167634818c6e5db473f3e
grema <gremail12345@gmail.com>
2026-04-14 17:03:34 +0200

    fix: unfucking the peaceful warning - the warning emoji is in the right place now - restarting the game no longer resets the difficulty

--- src/main/java/supersymmetry/client/gui/PeacefulWarningHandler.java
+++ src/main/java/supersymmetry/client/gui/PeacefulWarningHandler.java
@@ -14,32 +14,31 @@ import supersymmetry.Supersymmetry;
 public class PeacefulWarningHandler {
 
     @SubscribeEvent
-    public static void onButtonPressed(GuiScreenEvent.ActionPerformedEvent.Pre event) {
+    public static void onButtonPressed(GuiScreenEvent.ActionPerformedEvent.Post event) {
         GuiScreen gui = event.getGui();
 
         if (!(gui instanceof GuiOptions)) return;
-        if (event.getButton().id == 108) {
-            Minecraft mc = Minecraft.getMinecraft();
-            EnumDifficulty current = mc.world.getDifficulty();
-            EnumDifficulty next = EnumDifficulty.byId((current.getId() + 1) % 4);
-            if (next == EnumDifficulty.PEACEFUL) {
+        if (event.getButton().id != 108) return;
 
-                // cancel the original button press
-                event.setCanceled(true);
-                mc.displayGuiScreen(new GuiYesNo(
-                        (result, id) -> {
-                            if (result) {
-                                mc.world.getWorldInfo().setDifficulty(EnumDifficulty.PEACEFUL);
-                            }
-                            mc.displayGuiScreen(gui);
-                        },
-                        "Change to Peaceful?",
-                        "Supersymmetry is intended to be played at difficulty Easy or higher.\n" +
-                                "If you do not like mobs attacking your base, use /gamerule doInvasions false instead.\n" +
-                                "Only change this setting if you know what you're doing, as doing so may break parts of the game.",
-                        0
-                ));
-            }
+        Minecraft mc = Minecraft.getMinecraft();
+
+        if (mc.world == null) return;
+
+        if (mc.world.getDifficulty() == EnumDifficulty.PEACEFUL) {
+
+            mc.displayGuiScreen(new GuiYesNo(
+                    (result, id) -> {
+                        if (!result) {
+                            mc.world.getWorldInfo().setDifficulty(EnumDifficulty.EASY);
+                        }
+                        mc.displayGuiScreen(gui);
+                    },
+                    "Change to Peaceful?",
+                    "Supersymmetry is intended to be played at difficulty Easy or higher.\n" +
+                            "If you do not like mobs attacking your base, use /gamerule doInvasions false instead.\n" +
+                            "Only change this setting if you know what you're doing, as doing so may break parts of the game.",
+                    0
+            ));
         }
     }
     @SubscribeEvent
@@ -52,9 +51,8 @@ public class PeacefulWarningHandler {
 
         if (mc.world.getDifficulty() == net.minecraft.world.EnumDifficulty.PEACEFUL) {
 
-            // unfuck
-            int x = event.getGui().width / 2 + 128;
-            int y = event.getGui().height / 6 - 10;
+            int x = event.getGui().width / 2 + 122;
+            int y = event.getGui().height / 6 - 6;
 
             mc.fontRenderer.drawString("⚠", x, y, 0xFF5555);
         }
