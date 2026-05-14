a1ca5e95799c67b908fccdcd03fba6e16db9a222
oliwier509 <oliwier5009.2003@wp.pl>
2026-05-14 14:32:18 +0200

    added to-do list

--- src/main/java/supersymmetry/common/metatileentities/SuSyMetaTileEntities.java
+++ src/main/java/supersymmetry/common/metatileentities/SuSyMetaTileEntities.java
@@ -741,8 +741,13 @@ public class SuSyMetaTileEntities {
         registerSimpleMTE(RESISTANCE_FURNACE, 12, 18503, "resistance_furnace", SuSyRecipeMaps.RESISTANCE_FURNACE,
                 Textures.ELECTRIC_FURNACE_OVERLAY, true, GTUtility.defaultTankSizeFunction);
 
-        // Defoliators: 18600-18700
+        // Siege elements: 18600-18700
         DEFOLIATOR[0] = registerMetaTileEntity(18600, new MetaTileEntityDefoliator(susyId("defoliator.lv"), 1));
+        /* todo
+        interference dynamo
+        toxic spewer
+        atmospheric oxidizer
+         */
     }
 
     private static void registerSimpleSteamMTE(SuSySimpleSteamMetaTileEntity[] machines, int startId, String name,
