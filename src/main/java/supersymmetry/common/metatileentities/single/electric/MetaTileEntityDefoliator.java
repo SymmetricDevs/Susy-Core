ef40f431029b14747f620eb6214a2a9a960b1f1e
oliwier509 <oliwier5009.2003@wp.pl>
2026-05-14 13:07:17 +0200

    change: refactored everything, made the defoliator work on other modded plants in susy.
    so far it works on vanilla, BoP and GTFO plants, will add more if I figure out more are present.
    task for later:
    it should stop the greenhouse from working when within range.

--- src/main/java/supersymmetry/common/metatileentities/single/electric/MetaTileEntityDefoliator.java
+++ src/main/java/supersymmetry/common/metatileentities/single/electric/MetaTileEntityDefoliator.java
@@ -1,13 +1,11 @@
 package supersymmetry.common.metatileentities.single.electric;
 
-import com.google.common.collect.ImmutableMap;
 import gregtech.api.GTValues;
 import gregtech.api.gui.ModularUI;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.TieredMetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockDirt;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
@@ -17,9 +15,11 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -37,19 +37,10 @@ public class MetaTileEntityDefoliator extends TieredMetaTileEntity {
     }
 
     @Override
-    protected ModularUI createUI(EntityPlayer entityPlayer) {
-        return null;
-    }
+    protected ModularUI createUI(EntityPlayer entityPlayer) { return null; }
 
     @Override
-    protected boolean openGUIOnRightClick() {
-        return false;
-    }
-
-    @Override
-    protected void reinitializeEnergyContainer() {
-        super.reinitializeEnergyContainer();
-    }
+    protected boolean openGUIOnRightClick() { return false; }
 
     @Override
     public NBTTagCompound writeToNBT(NBTTagCompound data) {
@@ -74,32 +65,32 @@ public class MetaTileEntityDefoliator extends TieredMetaTileEntity {
         if (getWorld().isRemote) return;
 
         this.energyContainer.removeEnergy(this.energyContainer.getEnergyCapacity());
-
         processShell(getWorld(), getPos(), currentRadius);
 
-        if (currentRadius > 32) return;
-        currentRadius++;
+        if (currentRadius <= 32) currentRadius++;
     }
 
-
     private void processShell(World world, BlockPos center, int radius) {
-        if (radius == 0) {
-            return;
-        }
+        if (radius == 0) return;
 
-        Map<Block, IBlockState> replacements = DefoliatorReplacements.REPLACEMENTS;
+        Map<ResourceLocation, IBlockState> replacements     = DefoliatorReplacements.getReplacements();
+        Map<String, IBlockState>           metaReplacements = DefoliatorReplacements.getMetaReplacements();
 
         for (int dx = -radius; dx <= radius; dx++) {
             for (int dy = -radius; dy <= radius; dy++) {
                 for (int dz = -radius; dz <= radius; dz++) {
                     BlockPos target = center.add(dx, dy, dz);
-
                     if (!world.isValid(target)) continue;
 
-                    IBlockState state = world.getBlockState(target);
-                    Block block = state.getBlock();
+                    IBlockState state     = world.getBlockState(target);
+                    ResourceLocation blockName = ForgeRegistries.BLOCKS.getKey(state.getBlock());
+                    if (blockName == null) continue;
 
-                    IBlockState replacement = replacements.get(block);
+                    String metaKey = blockName + ":" + state.getBlock().getMetaFromState(state);
+                    IBlockState replacement = metaReplacements.get(metaKey);
+
+                    if (replacement == null) replacement = replacements.get(blockName);
+
                     if (replacement != null) {
                         world.setBlockState(target, replacement, 2);
                     }
@@ -108,40 +99,111 @@ public class MetaTileEntityDefoliator extends TieredMetaTileEntity {
         }
     }
 
-
     @Override
-    public void addInformation(ItemStack stack, @Nullable World player, @NotNull List<String> tooltip,
-                               boolean advanced) {
+    public void addInformation(ItemStack stack, @Nullable World player, @NotNull List<String> tooltip, boolean advanced) {
         tooltip.add(I18n.format("susy.machine.defoliator.tooltip.info"));
         tooltip.add(I18n.format("susy.machine.defoliator.tooltip.description"));
     }
 
     @Override
-    public boolean getIsWeatherOrTerrainResistant() {
-        return true;
-    }
+    public boolean getIsWeatherOrTerrainResistant() { return true; }
 
-    public class DefoliatorReplacements {
+    // -------------------------------------------------------------------------
 
-        public static final Map<Block, IBlockState> REPLACEMENTS = ImmutableMap.<Block, IBlockState>builder()
-                .put(Blocks.TALLGRASS,      Blocks.AIR.getDefaultState())
-                .put(Blocks.YELLOW_FLOWER,  Blocks.AIR.getDefaultState())
-                .put(Blocks.RED_FLOWER,     Blocks.AIR.getDefaultState())
-                .put(Blocks.DOUBLE_PLANT,   Blocks.AIR.getDefaultState())
-                .put(Blocks.DEADBUSH,       Blocks.AIR.getDefaultState())
-                .put(Blocks.BROWN_MUSHROOM, Blocks.AIR.getDefaultState())
-                .put(Blocks.RED_MUSHROOM,   Blocks.AIR.getDefaultState())
-                .put(Blocks.VINE,           Blocks.AIR.getDefaultState())
-                .put(Blocks.WATERLILY,      Blocks.AIR.getDefaultState())
-                .put(Blocks.CACTUS,         Blocks.AIR.getDefaultState())
-                .put(Blocks.REEDS,          Blocks.AIR.getDefaultState())
-                .put(Blocks.LEAVES,         Blocks.AIR.getDefaultState())
-                .put(Blocks.LEAVES2,        Blocks.AIR.getDefaultState())
+    public static class DefoliatorReplacements {
 
-                .put(Blocks.GRASS, Blocks.DIRT.getDefaultState()
-                        .withProperty(BlockDirt.VARIANT, BlockDirt.DirtType.COARSE_DIRT))
+        private static Map<ResourceLocation, IBlockState> replacements     = null;
+        private static Map<String, IBlockState>           metaReplacements = null;
 
-                .build();
+        public static Map<ResourceLocation, IBlockState> getReplacements() {
+            if (replacements == null) buildReplacements();
+            return replacements;
+        }
+
+        public static Map<String, IBlockState> getMetaReplacements() {
+            if (metaReplacements == null) buildReplacements();
+            return metaReplacements;
+        }
+
+        private static IBlockState stateOf(String domain, String path, int meta) {
+            Block block = Block.REGISTRY.getObject(new ResourceLocation(domain, path));
+            return block != null ? block.getStateFromMeta(meta) : Blocks.AIR.getDefaultState();
+        }
+
+        private static void buildReplacements() {
+
+            //no metadata
+            IBlockState AIR         = stateOf("minecraft", "air",  0);
+            IBlockState WATER       = stateOf("minecraft", "water", 0);
+            IBlockState COARSE_DIRT = stateOf("minecraft", "dirt",  1);
+            IBlockState LOAMY_DIRT  = stateOf("biomesoplenty", "dirt", 8);
+            IBlockState SANDY_DIRT  = stateOf("biomesoplenty", "dirt", 9);
+            IBlockState SILTY_DIRT  = stateOf("biomesoplenty", "dirt", 10);
+
+            Map<ResourceLocation, IBlockState> map = new HashMap<>();
+
+            // Minecraft
+            map.put(new ResourceLocation("minecraft", "tallgrass"),      AIR);
+            map.put(new ResourceLocation("minecraft", "yellow_flower"),  AIR);
+            map.put(new ResourceLocation("minecraft", "red_flower"),     AIR);
+            map.put(new ResourceLocation("minecraft", "double_plant"),   AIR);
+            map.put(new ResourceLocation("minecraft", "deadbush"),       AIR);
+            map.put(new ResourceLocation("minecraft", "brown_mushroom"), AIR);
+            map.put(new ResourceLocation("minecraft", "red_mushroom"),   AIR);
+            map.put(new ResourceLocation("minecraft", "vine"),           AIR);
+            map.put(new ResourceLocation("minecraft", "waterlily"),      AIR);
+            map.put(new ResourceLocation("minecraft", "cactus"),         AIR);
+            map.put(new ResourceLocation("minecraft", "reeds"),          AIR);
+            map.put(new ResourceLocation("minecraft", "leaves"),         AIR);
+            map.put(new ResourceLocation("minecraft", "leaves2"),        AIR);
+            map.put(new ResourceLocation("minecraft", "sapling"),        AIR);
+            map.put(new ResourceLocation("minecraft", "grass"),          COARSE_DIRT);
+            map.put(new ResourceLocation("minecraft", "mycelium"),       COARSE_DIRT);
+
+            // Biomes O' Plenty
+            map.put(new ResourceLocation("biomesoplenty", "bamboo"),          AIR);
+            map.put(new ResourceLocation("biomesoplenty", "mushroom"),        AIR);
+            map.put(new ResourceLocation("biomesoplenty", "waterlily"),       AIR);
+            map.put(new ResourceLocation("biomesoplenty", "double_plant"),    AIR);
+            map.put(new ResourceLocation("biomesoplenty", "willow_wine"),     AIR);
+            map.put(new ResourceLocation("biomesoplenty", "bramble_plant"),   AIR);
+            map.put(new ResourceLocation("biomesoplenty", "ivy"),             AIR);
+            map.put(new ResourceLocation("biomesoplenty", "bamboo_tatching"), AIR);
+
+            for (int i = 0; i <= 5; i++)
+                map.put(new ResourceLocation("biomesoplenty", "leaves_"  + i), AIR);
+            for (int i = 0; i <= 2; i++)
+                map.put(new ResourceLocation("biomesoplenty", "sapling_" + i), AIR);
+            for (int i = 0; i <= 1; i++)
+                map.put(new ResourceLocation("biomesoplenty", "plant_"   + i), AIR);
+            for (int i = 0; i <= 1; i++)
+                map.put(new ResourceLocation("biomesoplenty", "flower_"   + i), AIR);
+
+            // GregTech Food Option
+            for (int i = 0; i <= 1; i++)
+                map.put(new ResourceLocation("gregtechfoodoption", "gtfo_sapling_" + i), AIR);
+            for (int i = 0; i <= 2; i++)
+                map.put(new ResourceLocation("gregtechfoodoption", "gtfo_leaves_"  + i), AIR);
+
+            for (ResourceLocation rl : Block.REGISTRY.getKeys()) {
+                if (rl.getNamespace().equals("gregtechfoodoption") && rl.getPath().startsWith("crop_"))
+                    map.put(rl, AIR);
+            }
+
+            replacements = map;
+
+            //metadata
+            Map<String, IBlockState> metaMap = new HashMap<>();
+
+            metaMap.put("biomesoplenty:coral:0",  WATER);
+            metaMap.put("biomesoplenty:grass:2",  LOAMY_DIRT);
+            metaMap.put("biomesoplenty:grass:3",  SANDY_DIRT);
+            metaMap.put("biomesoplenty:grass:4",  SILTY_DIRT);
+            metaMap.put("biomesoplenty:grass:5",  COARSE_DIRT);
+            metaMap.put("biomesoplenty:grass:7",  COARSE_DIRT);
+
+            metaReplacements = metaMap;
+        }
 
         private DefoliatorReplacements() {}
     }
