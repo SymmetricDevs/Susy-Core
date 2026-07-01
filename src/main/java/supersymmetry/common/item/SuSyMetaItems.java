f236305fddea24897a24eab2bbb0a2941da8bfa3
Engineerboy2998 <immanueltomczuk@gmail.com>
2026-07-01 06:32:07 +0100

    changed data_card.master_blueprint to use BlueprintBehavior instead of DataCardBehavior

--- src/main/java/supersymmetry/common/item/SuSyMetaItems.java
+++ src/main/java/supersymmetry/common/item/SuSyMetaItems.java
@@ -154,7 +154,7 @@ public class SuSyMetaItems {
                         lines -> lines.add(I18n.format("metaitem.data_card.tooltip.1")), Arrays.asList("type")));
 
         DATA_CARD_MASTER_BLUEPRINT = metaItem.addItem(13, "data_card.master_blueprint").setMaxStackSize(1)
-                .addComponents(new DataCardBehavior(_ -> {}, Arrays.asList("rocketType")));
+                .addComponents(new BlueprintBehavior(_ -> {}, Arrays.asList("name")));
 
         TUNGSTEN_ELECTRODE = metaItem.addItem(14, "tungsten_electrode").setMaxStackSize(1)
                 .addComponents(ElectrodeDurabilityManager.INSTANCE);
