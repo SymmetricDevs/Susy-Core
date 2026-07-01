15591c5093e1c1ad544ac39cc774f04c01155550
Engineerboy2998 <immanueltomczuk@gmail.com>
2026-07-01 06:32:38 +0100

    Added BlueprintBehavior.java to seperate the behaviour between the blueprints and the rest of the data cards

---
+++ src/main/java/supersymmetry/common/item/behavior/BlueprintBehavior.java
@@ -0,0 +1,82 @@
+package supersymmetry.common.item.behavior;
+
+import gregtech.api.items.metaitem.stats.IItemBehaviour;
+import gregtech.api.items.metaitem.stats.ISubItemHandler;
+import gregtech.api.util.GTUtility;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.common.util.Constants;
+import org.jetbrains.annotations.NotNull;
+import supersymmetry.api.rocketry.components.AbstractComponent;
+import supersymmetry.common.item.SuSyMetaItems;
+import supersymmetry.common.rocketry.SusyRocketComponents;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class BlueprintBehavior implements IItemBehaviour, ISubItemHandler {
+
+    private final Consumer<List<String>> lines;
+    private final List<String> keys;
+public BlueprintBehavior(@NotNull Consumer<List<String>> lines, List<String> keys) {
+        this.lines = lines;
+        this.keys = keys;
+    }
+
+    @Override
+    public String getItemSubType(ItemStack itemStack) {
+        var tag = GTUtility.getOrCreateNbtCompound(itemStack);
+        return tag.getString("name");
+    }
+
+    @Override
+    public void getSubItems(ItemStack itemStack, CreativeTabs creativeTab, NonNullList<ItemStack> subItems) {
+        subItems.add(itemStack.copy());
+        if (itemStack.getMetadata() == SuSyMetaItems.DATA_CARD_MASTER_BLUEPRINT.metaValue &&
+                SusyRocketComponents.ROCKET_SOYUZ_BLUEPRINT_DEFAULT != null) {
+            ItemStack configured = itemStack.copy();
+            NBTTagCompound tag = SusyRocketComponents.ROCKET_SOYUZ_BLUEPRINT_DEFAULT.writeToNBT();
+            configured.setTagCompound(tag);
+            subItems.add(configured);
+        }
+    }
+
+    @Override
+    public void addInformation(ItemStack itemStack, List<String> lines) {
+        this.lines.accept(lines);
+        NBTTagCompound tag = itemStack.getTagCompound();
+        if (tag == null) return;
+
+        for (String key : this.keys) {
+            if (tag.hasKey(key, Constants.NBT.TAG_STRING)) {
+                if (tag.hasKey("stages")) {
+                    lines.add(
+                            I18n.format(itemStack.getTranslationKey() + ".tag." + tag.getString(key)) + " ID: " +
+                                    getID(tag));
+                } else {
+                    lines.add(
+                            I18n.format(itemStack.getTranslationKey() + ".tag." + tag.getString(key)));
+                }
+            }
+        }
+
+        if (tag.hasKey("name", Constants.NBT.TAG_STRING)) {
+            String targetName = tag.getString("name");
+            for (AbstractComponent<?> component : AbstractComponent.getRegistry()) {
+                if (component.getName().equals(targetName)) {
+                    lines.addAll(component.getTooltipLines(tag));
+                    break;
+                }
+            }
+        }
+    }
+
+    private String getID(NBTTagCompound key) {
+        // Left pad
+        String fullID = String.format("%08x", key.hashCode());
+        return fullID.toUpperCase();
+    }
+}
