66d550e8d53ba90d6a8858f65d39f17af533fb48
superintendent2521 <67196752+superintendent2521@users.noreply.github.com>
2026-07-08 22:29:44 -0400

    i cannot read code

--- src/main/java/supersymmetry/common/blocks/SuSyBlocks.java
+++ src/main/java/supersymmetry/common/blocks/SuSyBlocks.java
@@ -186,8 +186,7 @@ public class SuSyBlocks {
         }
 
         for (IBlockState state : ASPHALT.getBlockState().getValidStates()) {
-            BlockUtility.setWalkingSpeedBonus(state, 1); // Buff from 0.6F, Controls all forms of Concrete and Asphalt
-                                                         // speed boosts.
+            BlockUtility.setWalkingSpeedBonus(state, 1); // Buff from 0.6F
         }
         for (IBlockState state : LUNAR_CONCRETE.getBlockState().getValidStates()) {
             BlockUtility.setWalkingSpeedBonus(state, BlockUtility.ASPHALT_WALKING_SPEED_BONUS);
