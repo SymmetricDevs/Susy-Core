cb7406b96d183b25a876c898ab6d5966b693c8de
jethro3000 <giraffeman05@verizon.net>
2026-07-15 05:06:26 -0500

    remodel/rename steel drill bit block

--- src/main/java/supersymmetry/common/blocks/BlockDrillBit.java
+++ src/main/java/supersymmetry/common/blocks/BlockDrillBit.java
@@ -1,14 +1,24 @@
 package supersymmetry.common.blocks;
 
+import static net.minecraft.block.BlockDirectional.FACING;
+
 import javax.annotation.Nonnull;
 
 import net.minecraft.block.SoundType;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -24,44 +34,92 @@ public class BlockDrillBit extends VariantBlock<BlockDrillBit.DrillBitType> {
         setResistance(10.0f);
         setSoundType(SoundType.METAL);
         setHarvestLevel("wrench", 2);
-        setDefaultState(getState(DrillBitType.STEEL));
+        setDefaultState(getState(DrillBitType.STEEL).withProperty(FACING, EnumFacing.NORTH));
+    }
+
+    @Nonnull
+    @Override
+    public IBlockState getStateForPlacement(@NotNull World worldIn, @NotNull BlockPos pos,
+                                            @NotNull EnumFacing facing, float hitX, float hitY,
+                                            float hitZ, int meta, @NotNull EntityLivingBase placer) {
+        return super.getStateForPlacement(worldIn, pos, facing, hitX, hitY, hitZ, meta, placer)
+                .withProperty(FACING, EnumFacing.getDirectionFromEntityLiving(pos, placer));
+    }
+
+    @Nonnull
+    @Override
+    public BlockStateContainer createBlockState() {
+        Class<DrillBitType> enumClass = DrillBitType.class;
+        this.VARIANT = PropertyEnum.create("variant", enumClass);
+        this.VALUES = enumClass.getEnumConstants();
+        return new BlockStateContainer(this, VARIANT, FACING);
     }
 
     @Override
-    public boolean canCreatureSpawn(@NotNull IBlockState state, @NotNull IBlockAccess world, @NotNull BlockPos pos,
-                                    @NotNull EntityLiving.SpawnPlacementType type) {
-        return false;
+    public int damageDropped(@NotNull IBlockState state) {
+        return state.getValue(VARIANT).ordinal();
     }
 
-    @NotNull
+    @Nonnull
     @Override
-    public BlockRenderLayer getRenderLayer() {
-        return BlockRenderLayer.SOLID;
+    public IBlockState getStateFromMeta(int meta) {
+        EnumFacing enumfacing = EnumFacing.byIndex(meta);
+        return getDefaultState().withProperty(FACING, enumfacing);
     }
 
-    public enum DrillBitType implements IStringSerializable, IStateHarvestLevel {
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(FACING).ordinal();
+    }
 
-        STEEL("steel", 3);
+    @Nonnull
+    @Override
+    public ItemStack getPickBlock(@NotNull IBlockState state, @NotNull RayTraceResult target,
+                                  @NotNull World world, @NotNull BlockPos pos,
+                                  @NotNull EntityPlayer player) {
+        return this.getItemVariant(state.getValue(VARIANT), 1);
+    }
 
-        private final String name;
-        private final int harvestLevel;
-
-        DrillBitType(String name, int harvestLevel) {
-            this.name = name;
-            this.harvestLevel = harvestLevel;
+        @Override
+        public boolean canCreatureSpawn (@NotNull IBlockState state, @NotNull IBlockAccess world, @NotNull BlockPos pos,
+                @NotNull EntityLiving.SpawnPlacementType type){
+            return false;
         }
 
-        @Nonnull
-        public String getName() {
-            return this.name;
+        @Override
+        public boolean isOpaqueCube (@NotNull IBlockState state){
+            return false;
         }
 
-        public int getHarvestLevel(IBlockState state) {
-            return this.harvestLevel;
+        @NotNull
+        @Override
+        public BlockRenderLayer getRenderLayer () {
+            return BlockRenderLayer.CUTOUT;
         }
 
-        public String getHarvestTool(IBlockState state) {
-            return "wrench";
+        public enum DrillBitType implements IStringSerializable, IStateHarvestLevel {
+
+            STEEL("steel", 3);
+
+            private final String name;
+            private final int harvestLevel;
+
+            DrillBitType(String name, int harvestLevel) {
+                this.name = name;
+                this.harvestLevel = harvestLevel;
+            }
+
+            @Nonnull
+            public String getName() {
+                return this.name;
+            }
+
+            public int getHarvestLevel(IBlockState state) {
+                return this.harvestLevel;
+            }
+
+            public String getHarvestTool(IBlockState state) {
+                return "wrench";
+            }
         }
     }
-}
