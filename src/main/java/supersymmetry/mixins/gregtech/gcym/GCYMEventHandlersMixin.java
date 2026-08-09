6a1f5264e4c89f0ede3740d6c527e7bcf3458ff5
jethro3000 <giraffeman05@verizon.net>
2026-08-09 03:12:02 -0500

    force declare molten

--- src/main/java/supersymmetry/mixins/gregtech/gcym/GCYMEventHandlersMixin.java
+++ src/main/java/supersymmetry/mixins/gregtech/gcym/GCYMEventHandlersMixin.java
@@ -5,8 +5,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import gregicality.multiblocks.api.unification.properties.AlloyBlastProperty;
+import gregicality.multiblocks.api.unification.properties.GCYMPropertyKey;
 import gregicality.multiblocks.common.GCYMEventHandlers;
+import gregtech.api.GregTechAPI;
+import gregtech.api.unification.material.Material;
 import gregtech.api.unification.material.event.MaterialEvent;
+import gregtech.api.unification.material.event.PostMaterialEvent;
 
 @Mixin(value = GCYMEventHandlers.class, remap = false)
 public abstract class GCYMEventHandlersMixin {
@@ -14,5 +19,32 @@ public abstract class GCYMEventHandlersMixin {
     @Inject(method = "registerMaterials", at = @At("HEAD"), cancellable = true)
     private static void susy$disableGCYMMaterials(MaterialEvent event, CallbackInfo ci) {
         ci.cancel();
+    }
+
+    @Inject(
+            method = "registerMaterialsPost",
+            at = @At(
+                     value = "INVOKE",
+                     target = "Lgregicality/multiblocks/api/fluids/GeneratedFluidHandler;init()V"))
+    private static void susy$forceMoltenMaterials(PostMaterialEvent event, CallbackInfo ci) {
+        susy$forceMolten("monel_500");
+        susy$forceMolten("hsla_980_x");
+        susy$forceMolten("food_grade_stainless_steel");
+        susy$forceMolten("zircaloy_4");
+        susy$forceMolten("reactor_steel");
+        susy$forceMolten("alnico");
+    }
+
+    private static void susy$forceMolten(String name) {
+        Material material = GregTechAPI.materialManager.getMaterial("susy:" + name);
+        if (material == null) {
+            throw new IllegalStateException("Unable to find SuSy material: " + name);
+        }
+        AlloyBlastProperty property = material.getProperty(GCYMPropertyKey.ALLOY_BLAST);
+        if (property == null) {
+            throw new IllegalStateException(
+                    "SuSy material has no GCYM AlloyBlastProperty: " + name);
+        }
+        property.setForceGenerateMolten(true);
     }
 }
