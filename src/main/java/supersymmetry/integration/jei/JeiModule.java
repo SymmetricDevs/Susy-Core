80f78f983e3ed45895479a9f2592d0ce0471293a
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2026-08-15 14:04:46 +0800

    compat(jei): add collapsible groups for tankless fluid pipes

--- src/main/java/supersymmetry/integration/jei/JeiModule.java
+++ src/main/java/supersymmetry/integration/jei/JeiModule.java
@@ -1,10 +1,19 @@
 package supersymmetry.integration.jei;
 
 import static supersymmetry.api.fluids.SusyGeneratedFluidHandler.CAST_MATERIALS;
+import static supersymmetry.common.blocks.SuSyMetaBlocks.TANKLESS_FLUID_PIPES;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import lombok.val;
+import mezz.jei.api.*;
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import org.jetbrains.annotations.NotNull;
 
 import cam72cam.immersiverailroading.IRItems;
@@ -13,12 +22,10 @@ import gregtech.api.modules.GregTechModule;
 import gregtech.api.recipes.RecipeMaps;
 import gregtech.api.util.Mods;
 import gregtech.integration.IntegrationSubmodule;
-import mezz.jei.api.IModPlugin;
-import mezz.jei.api.IModRegistry;
-import mezz.jei.api.ISubtypeRegistry;
-import mezz.jei.api.JEIPlugin;
 import mezz.jei.api.ingredients.IModIngredientRegistration;
 import mezz.jei.api.recipe.IRecipeCategoryRegistration;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.NullMarked;
 import supersymmetry.Supersymmetry;
 import supersymmetry.api.particle.ParticleBeam;
 import supersymmetry.api.recipes.SuSyRecipeMaps;
@@ -41,7 +48,7 @@ import supersymmetry.modules.SuSyModules;
 public class JeiModule extends IntegrationSubmodule implements IModPlugin {
 
     @Override
-    public void registerItemSubtypes(@NotNull ISubtypeRegistry subtypeRegistry) {
+    public void registerSubtypes(@NotNull ISubtypeRegistry subtypeRegistry) {
         subtypeRegistry.registerSubtypeInterpreter(IRItems.ITEM_ROLLING_STOCK.internal,
                 new RollingStockSubtypeHandler());
     }
@@ -77,7 +84,7 @@ public class JeiModule extends IntegrationSubmodule implements IModPlugin {
         registry.addRecipeCatalyst(SuSyMetaTileEntities.LARGE_RES.getStackForm(), largeRESMapId);
 
         String strandCastingId = GTValues.MODID + ":strand_casting";
-        registry.addRecipes(CAST_MATERIALS.stream().map(StrandInfo::new).collect(Collectors.toList()), strandCastingId);
+        registry.addRecipes(CAST_MATERIALS.stream().map(StrandInfo::new).toList(), strandCastingId);
         registry.addRecipeCatalyst(SuSyMetaTileEntities.TURNING_ZONE.getStackForm(), strandCastingId);
         registry.addRecipeCatalyst(SuSyMetaTileEntities.ROLLING_MILL.getStackForm(), strandCastingId);
         registry.addRecipeCatalyst(SuSyMetaTileEntities.CLUSTER_MILL.getStackForm(), strandCastingId);
@@ -91,6 +98,37 @@ public class JeiModule extends IntegrationSubmodule implements IModPlugin {
     public void registerCategories(IRecipeCategoryRegistration registry) {
         registry.addRecipeCategories(new RocketFuelCategory(registry.getJeiHelpers().getGuiHelper()));
         registry.addRecipeCategories(new StrandCategory(registry.getJeiHelpers().getGuiHelper()));
-        registry.addRecipeCategories(new RocketFuelCategory(registry.getJeiHelpers().getGuiHelper()));
+    }
+
+    @Override
+    public void registerCollapsibleGroups(@NonNull ICollapsibleGroupRegistry registry) {
+        buildTanklessFluidPipeGroup(registry);
+    }
+
+    private static void buildTanklessFluidPipeGroup(@NonNull ICollapsibleGroupRegistry registry) {
+        List<ItemStack> stacks = new ArrayList<>();
+        for (val blocks : TANKLESS_FLUID_PIPES.values()) {
+            for (val block : blocks) {
+                addSubBlocks(stacks, block);
+            }
+        }
+        addGroup(registry, "tankless_fluid_pipes", stacks);
+    }
+
+
+    @NullMarked
+    private static void addSubBlocks(List<ItemStack> out, Block block) {
+        NonNullList<ItemStack> sub = NonNullList.create();
+        block.getSubBlocks(CreativeTabs.SEARCH, sub);
+        out.addAll(sub);
+    }
+
+    @NullMarked
+    private static void addGroup(ICollapsibleGroupRegistry registry,
+                                 String id,
+                                 Collection<ItemStack> stacks) {
+        registry.newGroup(Supersymmetry.MODID + ":" + id, Supersymmetry.MODID + ".jei.group." + id)
+                .add(stacks.toArray())
+                .build();
     }
 }
