7ac270017cb10e2f2eca765aa5de90b8a364058f
bruberu <80226372+bruberu@users.noreply.github.com>
2026-08-17 16:21:04 -0500

    reduce inputs

--- src/main/java/supersymmetry/api/recipes/SuSyRecipeMaps.java
+++ src/main/java/supersymmetry/api/recipes/SuSyRecipeMaps.java
@@ -488,7 +488,7 @@ public class SuSyRecipeMaps {
             new PrimitiveRecipeBuilder(), false).setSound(GTSoundEvents.FURNACE);
 
     public static final RecipeMap<SimpleRecipeBuilder> LARGE_RES_RECIPES = new RecipeMap<>(
-            "large_railroad_engineering_station", 25, 1, 3, 0, new SimpleRecipeBuilder(), false)
+            "large_railroad_engineering_station", 16, 1, 3, 0, new SimpleRecipeBuilder(), false)
             .setSlotOverlay(false, false, GuiTextures.SLOT).setSlotOverlay(true, false, GuiTextures.SLOT)
             .setSound(GTSoundEvents.ASSEMBLER)
             .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW_MULTIPLE, ProgressWidget.MoveType.HORIZONTAL);
