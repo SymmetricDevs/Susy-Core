b4c1fb31715142cbb1a4d14c6651a75ad9e86899
MCTian-mi <35869948+MCTian-mi@users.noreply.github.com>
2026-08-25 00:02:47 +0800

    fix: rewrite dimstack mixins

--- src/main/java/supersymmetry/mixins/dimstack/DimensionalPipeMixin.java
+++ src/main/java/supersymmetry/mixins/dimstack/DimensionalPipeMixin.java
@@ -1,35 +1,44 @@
 package supersymmetry.mixins.dimstack;
 
-import net.minecraft.tileentity.TileEntity;
-
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.items.CapabilityItemHandler;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 
 import cd4017be.dimstack.tileentity.DimensionalPipe;
-import cd4017be.lib.TickRegistry.IUpdatable;
-import cd4017be.lib.block.AdvancedBlock.IInteractiveTile;
-import cd4017be.lib.block.AdvancedBlock.INeighborAwareTile;
-import cd4017be.lib.tileentity.BaseTileEntity;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(value = DimensionalPipe.class, remap = false)
-public abstract class DimensionalPipeMixin extends BaseTileEntity
-                                           implements IUpdatable, IInteractiveTile, INeighborAwareTile {
+public abstract class DimensionalPipeMixin {
 
-    /**
-     * @author aliu-here
-     * @reason idk how else to fix the problem where a dimensionalpipe tileentity (that was already loaded) would have
-     *         its linkTile get set to null when it was already a non-null value
-     */
-    @Overwrite
-    void link(DimensionalPipe tile) {
-        if (tile != ((DimensionalPipeAccessor) this).getLinkTile() && tile != null) {
-            ((DimensionalPipeAccessor) this).setLinkTile(tile);
-        }
-        ((DimensionalPipeAccessor) this).setUpdateLink(false);
-        TileEntity connected = ((DimensionalPipeAccessor) this).callGetCon();
-        if (connected != null) {
-            connected.getBlockType().observedNeighborChange(((DimensionalPipeAccessor) this).getConBlock(), world,
-                    connected.getPos(), this.getBlockType(), this.getPos()); // definitely force a pipenet reload
-        }
+    @Shadow
+    @Final
+    private static Object[] NULL_CAPS;
+
+    @ModifyReturnValue(method = "hasCapability",
+            at = {
+                    @At(value = "RETURN", ordinal = 2),
+                    @At(value = "RETURN", ordinal = 3),
+            })
+    private boolean alwaysHasItemAndFluidCapability(boolean original, @Local(argsOnly = true) Capability<?> cap) {
+        return original || cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY || cap == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY;
+    }
+
+    @SuppressWarnings("unchecked")
+    @ModifyReturnValue(method = "getCapability",
+            at = {
+                    @At(value = "RETURN", ordinal = 1),
+                    @At(value = "RETURN", ordinal = 3),
+            })
+    private <T> T returnEmptyCapabilities(T original, @Local(argsOnly = true) Capability<?> cap) {
+        if (original != null) return original;
+        if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) return (T) NULL_CAPS[1];
+        if (cap == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY) return (T) NULL_CAPS[2];
+        return null;
     }
 }
