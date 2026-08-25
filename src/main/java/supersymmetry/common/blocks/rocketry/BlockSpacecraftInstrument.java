efa5c1bb92b42ef5af4a1e36ebab36a05cb0e4ec
lanicerine <285244510+lanicerine@users.noreply.github.com>
2026-08-25 05:20:05 +0300

    fix all blocks being usable in the spacecraft hull and make hollowness checking only count air

--- src/main/java/supersymmetry/common/blocks/rocketry/BlockSpacecraftInstrument.java
+++ src/main/java/supersymmetry/common/blocks/rocketry/BlockSpacecraftInstrument.java
@@ -38,7 +38,7 @@ public class BlockSpacecraftInstrument extends VariantBlock<BlockSpacecraftInstr
             case SOLAR_PANEL -> 100;
             case BATTERY -> 100;
             case ARM -> 800;
-            case LANDER -> 800;
+            case LANDER -> 15200; // Apollo lander mass from wikipedia
         };
     }
 
@@ -47,15 +47,10 @@ public class BlockSpacecraftInstrument extends VariantBlock<BlockSpacecraftInstr
         SENSOR_ARRAY("sensors", 4),
         COLLECTOR("collector", 4),
         CAMERA("camera", 4),
-        SOLAR_PANEL("solar_panel",
-                4),
-        BATTERY("battery",
-                4),
+        SOLAR_PANEL("solar_panel", 4),
+        BATTERY("battery", 4),
         ARM("arm", 4, new InstrumentRobotArm()),
-        LANDER("lander", 4, new InstrumentLander()); // will
-                                                     // have
-                                                     // variable
-                                                     // purposes
+        LANDER("lander", 4, new InstrumentLander()); // will have variable purposes
 
         public String name;
         public int h;
