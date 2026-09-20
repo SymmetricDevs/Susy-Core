96265a63e9451eff9402e26ec507e0fb5b567126
lanicerine <285244510+lanicerine@users.noreply.github.com>
2026-09-20 22:43:46 +0300

    cvd input slots

--- src/main/java/supersymmetry/api/recipes/SuSyRecipeMaps.java
+++ src/main/java/supersymmetry/api/recipes/SuSyRecipeMaps.java
@@ -207,7 +207,7 @@ public class SuSyRecipeMaps {
             1, 2, 0, new SimpleRecipeBuilder(), false)
                     .setSound(GTSoundEvents.ELECTROLYZER);
 
-    public static final RecipeMap<SimpleRecipeBuilder> CVD_RECIPES = new RecipeMap<>("cvd", 3, 1, 4, 2,
+    public static final RecipeMap<SimpleRecipeBuilder> CVD_RECIPES = new RecipeMap<>("cvd", 3, 1, 5, 2,
             new SimpleRecipeBuilder(), false)
                     .setSound(GTSoundEvents.ARC);
 
